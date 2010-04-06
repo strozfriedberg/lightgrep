633861a954a5f4b2a1252bb14f08c382ff54ab17
Jon Stewart <jonathan.l.stewart@acm.org>
2010-04-06 13:28:33 -0400

    Added EitherState

--- include/states.h
+++ include/states.h
@@ -9,3 +9,13 @@ public:
 private:
   char Lit;
 };
+
+class EitherState {
+public:
+  EitherState(char one, char two): Lit1(one), Lit2(two) {}
+
+  const char* allowed(const char* beg, const char*) const { return *beg == Lit1 || *beg == Lit2 ? beg+1: beg; }
+
+private:
+  char Lit1, Lit2;
+};
