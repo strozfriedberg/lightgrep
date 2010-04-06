dd074838d4e8a6a269986b42ad65a570922442ae
Jon Stewart <jonathan.l.stewart@acm.org>
2010-04-06 13:23:50 -0400

    Added LitState

--- include/states.h
+++ include/states.h
@@ -1,2 +1,11 @@
 #pragma once
 
+class LitState {
+public:
+  LitState(char lit): Lit(lit) {}
+  
+  const char* allowed(const char* beg, const char*) const { return *beg == Lit ? beg+1: beg; }
+
+private:
+  char Lit;
+};
--- test/test_states.cpp
+++ test/test_states.cpp
@@ -3,4 +3,9 @@
 #include "states.h"
 
 SCOPE_TEST(litAccept) {
+  LitState lit('a');
+  char ch = 'a';
+  SCOPE_ASSERT_EQUAL(&ch+1, lit.allowed(&ch, &ch+1));
+  ch = 'b';
+  SCOPE_ASSERT_EQUAL(&ch, lit.allowed(&ch, &ch+1));
 }
