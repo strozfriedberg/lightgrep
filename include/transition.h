65124fb6569dd14eec17281ea73b9bd268d6aa23
Jon Stewart <jonathan.l.stewart@acm.org>
2010-04-07 12:57:55 -0400

    Added ability to clone a Transition onto a buffer

--- include/transition.h
+++ include/transition.h
@@ -9,4 +9,5 @@ public:
   virtual const byte* allowed(const byte* beg, const byte* end) const = 0;
   virtual void getBits(ByteSet& bitvec) const = 0;
   virtual size_t objSize() const = 0;
+  virtual Transition* clone(void* buffer) const = 0;
 };
