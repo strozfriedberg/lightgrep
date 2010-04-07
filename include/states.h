b57c24b17b6069270d41118af2719c24eaf71c11
Jon Stewart <jonathan.l.stewart@acm.org>
2010-04-06 20:30:42 -0400

    Change everything over to using unsigned chars.

--- include/states.h
+++ include/states.h
@@ -1,37 +1,40 @@
 #pragma once
 
 #include <bitset>
+#include "basic.h"
 
 class LitState {
 public:
-  LitState(char lit): Lit(lit) {}
+  LitState(byte lit): Lit(lit) {}
   
-  const char* allowed(const char* beg, const char*) const { return *beg == Lit ? beg+1: beg; }
+  const byte* allowed(const byte* beg, const byte*) const { return *beg == Lit ? beg+1: beg; }
 
   void getBits(std::bitset<256>& bits) const { bits.set(Lit); }
 
 private:
-  char Lit;
+  byte Lit;
 };
 
 class EitherState {
 public:
-  EitherState(char one, char two): Lit1(one), Lit2(two) {}
+  EitherState(byte one, byte two): Lit1(one), Lit2(two) {}
 
-  const char* allowed(const char* beg, const char*) const { return *beg == Lit1 || *beg == Lit2 ? beg+1: beg; }
+  const byte* allowed(const byte* beg, const byte*) const { return *beg == Lit1 || *beg == Lit2 ? beg+1: beg; }
 
   void getBits(std::bitset<256>& bits) const { bits.set(Lit1); bits.set(Lit2); }
 
 private:
-  char Lit1, Lit2;
+  byte Lit1, Lit2;
 };
 
 class RangeState {
 public:
-  RangeState(char first, char last): First(first), Last(last) {}
+  RangeState(byte first, byte last): First(first), Last(last) {}
 
-  const char* allowed(const char* beg, const char*) const { return First <= *beg && *beg <= Last ? beg+1: beg; }
+  const byte* allowed(const byte* beg, const byte*) const { return First <= *beg && *beg <= Last ? beg+1: beg; }
+
+  void getBits(std::bitset<256>& bits) const { for (byte i = First; i <= Last; ++i) { bits.set(i); }; }
 
 private:
-  char First, Last;
+  byte First, Last;
 };
