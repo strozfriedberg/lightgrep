e755a12ae1e13717b0059fd99f7a7f642ab540c6
Jon Stewart <jonathan.l.stewart@acm.org>
2010-04-08 17:26:42 -0400

    ghastly...

--- src/staticFSM.cpp
+++ src/staticFSM.cpp
@@ -1,7 +1,8 @@
 #include "staticFSM.h"
 
+#include <iostream>
+
 static const uint32 STATE_SIZE = 4;
-static const uint32 EDGE_SIZE = 4;
 
 StaticFSM::StaticFSM():
   NumStates(0), NumEdges(0) {}
@@ -14,8 +15,24 @@ uint32 StaticFSM::numEdges() const {
   return NumEdges;
 }
 
-void StaticFSM::allocate(uint32 transitionSize, uint32 numStates, uint32 numEdges) {
-  Buffer.reset(new byte[transitionSize + (numStates * STATE_SIZE) + (numEdges * EDGE_SIZE)]);
+StaticFSM::StateT StaticFSM::getFirstState() const {
+  return 0;
+}
+
+StaticFSM::EdgeRange StaticFSM::getEdges(StaticFSM::StateT curState) const {
+  std::pair<EdgeIt, EdgeIt> ret;
+  uint32 numEdges = *((uint32*)(getRawBuffer() + curState));
+  ret.first = (EdgeIt)(getRawBuffer() + curState + sizeof(uint32));
+  ret.second = ret.first + numEdges;
+  return ret;
+}
+
+uint32 StaticFSM::allocate(uint32 transitionSize, uint32 numStates, uint32 numEdges) {
+  // std::cout << "allocate) transitionSize = " << transitionSize << "; numStates = " << numStates << "; numEdges = " << numEdges << std::endl;
+
+  uint32 size = transitionSize + (numStates * STATE_SIZE) + (numEdges * sizeof(StaticEdge));
+  Buffer.reset(new byte[size]);
   NumStates = numStates;
   NumEdges = numEdges;
+  return size;
 }
