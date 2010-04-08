d20c44eef7fba7d524f043b0567c594601e485f0
Jon Stewart <jonathan.l.stewart@acm.org>
2010-04-08 12:07:09 -0400

    Starting to flesh out things a bit with DynamicFSM and StaticFSM and converting between them.

--- include/dynamicFSM.h
+++ include/dynamicFSM.h
@@ -1,5 +1,10 @@
 #pragma once
 
 #include <boost/graph/adjacency_list.hpp>
+#include "transition.h"
 
-typedef boost::adjacency_list<boost::listS, boost::listS, boost::directedS> DynamicFSM;
+typedef boost::shared_ptr<Transition> TransitionPtr;
+
+typedef boost::adjacency_list<boost::listS, boost::vecS, boost::directedS, boost::no_property, TransitionPtr> DynamicFSM;
+
+typedef DynamicFSM::edge_descriptor EdgeIdx;
--- src/utility.cpp
+++ src/utility.cpp
@@ -1,5 +1,7 @@
 #include "utility.h"
 
 boost::shared_ptr<StaticFSM> convert_to_static(const DynamicFSM& graph) {
-  return boost::shared_ptr<StaticFSM>();
+  boost::shared_ptr<StaticFSM> ret(new StaticFSM());
+  ret->allocate(0, num_vertices(graph), 0);
+  return ret;
 }
