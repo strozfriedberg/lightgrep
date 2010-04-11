6a81390e9b64058a58f5fc8bb415bb88d9f18cde
Jon Stewart <jonathan.l.stewart@acm.org>
2010-04-11 10:49:49 -0400

    Some tests on StaticFSM::allocate()

--- test/test_staticfsm.cpp
+++ test/test_staticfsm.cpp
@@ -8,3 +8,26 @@ SCOPE_TEST(testGetEdges) {
   SCOPE_ASSERT_EQUAL(((uint64)&num)+4, (uint64)edges.first);
   SCOPE_ASSERT_EQUAL(((uint64)&num) + 4 + (5 * sizeof(StaticEdge)), (uint64)edges.second);
 }
+
+SCOPE_TEST(testAllocate) {
+  StaticFSM fsm;
+  SCOPE_ASSERT_EQUAL(sizeof(uint32), fsm.allocate(0, 1, 0));
+  SCOPE_ASSERT_EQUAL(1u, fsm.numStates());
+  SCOPE_ASSERT_EQUAL(0u, fsm.numEdges());
+
+  SCOPE_ASSERT_EQUAL(0u, fsm.allocate(0, 0, 1));
+  SCOPE_ASSERT_EQUAL(0u, fsm.numStates());
+  SCOPE_ASSERT_EQUAL(0u, fsm.numEdges());
+
+  SCOPE_ASSERT_EQUAL(0u, fsm.allocate(0, 1, 1));
+  SCOPE_ASSERT_EQUAL(0u, fsm.numStates());
+  SCOPE_ASSERT_EQUAL(0u, fsm.numEdges());
+
+  SCOPE_ASSERT_EQUAL((2 * sizeof(uint32)) + sizeof(StaticEdge), fsm.allocate(0, 2, 1));
+  SCOPE_ASSERT_EQUAL(2u, fsm.numStates());
+  SCOPE_ASSERT_EQUAL(1u, fsm.numEdges());
+
+  SCOPE_ASSERT_EQUAL(57 + (2 * sizeof(uint32)) + sizeof(StaticEdge), fsm.allocate(57, 2, 1));
+  SCOPE_ASSERT_EQUAL(2u, fsm.numStates());
+  SCOPE_ASSERT_EQUAL(1u, fsm.numEdges());
+}
