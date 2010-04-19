a1839fb3449662e1c1367e786de3ee88dee0f859
Jon Stewart <jonathan.l.stewart@acm.org>
2010-04-19 15:33:59 -0400

    Specify 'test' as command to the tester for running unit tests

--- test/main.cpp
+++ test/main.cpp
@@ -3,5 +3,11 @@
 #include <scope/testrunner.h>
 
 int main(int argc, char** argv) {
-  return scope::DefaultRun(std::cout, argc, argv) ? 0: 1;
+  if (argc > 1) {
+    std::string first(argv[1]);
+    if (first == "test") {
+      return scope::DefaultRun(std::cout, argc, argv) ? 0: 1;
+    }
+  }
+  return 0;
 }
