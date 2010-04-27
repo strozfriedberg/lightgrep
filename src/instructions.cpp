2b08769aaab4df4aa1cbebff662c039bcc5b61ab
Jon Stewart <jonathan.l.stewart@acm.org>
2010-04-27 14:40:54 -0400

    instructions can be converted to human-readable strings

--- src/instructions.cpp
+++ src/instructions.cpp
@@ -1,6 +1,28 @@
 #include "instructions.h"
 
 #include <stdexcept>
+#include <iomanip>
+
+std::string Instruction::toString() const {
+  std::stringstream buf;
+  switch (OpCode) {
+    case LIT_OP:
+      buf << "Literal 0x" << std::hex << (unsigned short)Op.Literal << "/'" << Op.Literal << '\'';
+      break;
+    case JUMP_OP:
+      buf << "Jump 0x" << std::hex << std::setfill('0') << std::setw(8) << Op.Offset << '/' << std::dec << Op.Offset;
+      break;
+    case MATCH_OP:
+      buf << "Match";
+      break;
+    case SAVE_LABEL_OP:
+      buf << "SaveLabel " << Op.Offset;
+      break;
+    default:
+      buf << "* UNRECOGNIZED *";
+  };
+  return buf.str();
+}
 
 Instruction Instruction::makeLit(byte b) {
   Instruction i;
--- test/test_instructions.cpp
+++ test/test_instructions.cpp
@@ -28,13 +28,15 @@ SCOPE_TEST(makeLit) {
   SCOPE_ASSERT_EQUAL(LIT_OP, i.OpCode);
   SCOPE_ASSERT_EQUAL(1u, i.wordSize());
   SCOPE_ASSERT_EQUAL('a', i.Op.Literal);
+  SCOPE_ASSERT_EQUAL("Literal 0x61/'a'", i.toString());
 }
 
 SCOPE_TEST(makeJump) {
-  Instruction i = Instruction::makeJump(5);
+  Instruction i = Instruction::makeJump(11);
   SCOPE_ASSERT_EQUAL(JUMP_OP, i.OpCode);
   SCOPE_ASSERT_EQUAL(1u, i.wordSize());
-  SCOPE_ASSERT_EQUAL(5u, i.Op.Offset);
+  SCOPE_ASSERT_EQUAL(11u, i.Op.Offset);
+  SCOPE_ASSERT_EQUAL("Jump 0x0000000b/11", i.toString());
   SCOPE_EXPECT(Instruction::makeJump(1 << 24), std::overflow_error);
 }
 
@@ -42,6 +44,7 @@ SCOPE_TEST(makeMatch) {
   Instruction i = Instruction::makeMatch();
   SCOPE_ASSERT_EQUAL(MATCH_OP, i.OpCode);
   SCOPE_ASSERT_EQUAL(1u, i.wordSize());
+  SCOPE_ASSERT_EQUAL("Match", i.toString());
 }
 
 SCOPE_TEST(makeSaveLabel) {
@@ -49,5 +52,6 @@ SCOPE_TEST(makeSaveLabel) {
   SCOPE_ASSERT_EQUAL(SAVE_LABEL_OP, i.OpCode);
   SCOPE_ASSERT_EQUAL(1u, i.wordSize());
   SCOPE_ASSERT_EQUAL(3u, i.Op.Offset);
+  SCOPE_ASSERT_EQUAL("SaveLabel 3", i.toString());
   SCOPE_EXPECT(Instruction::makeSaveLabel(1 << 24), std::overflow_error);
 }
