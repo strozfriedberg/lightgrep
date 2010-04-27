6521e8ba0059469756adfe5aa08fe6c3bb152c12
Jon Stewart <jonathan.l.stewart@acm.org>
2010-04-26 21:27:06 -0400

    Execute Jump instructions

--- test/test_vm.cpp
+++ test/test_vm.cpp
@@ -5,13 +5,24 @@
 SCOPE_TEST(executeLit) {
   byte b = 'a';
   Instruction i = Instruction::makeLit('a');
+  Thread      cur(&i, 0, 0, 0);
   ThreadList  next;
-  SCOPE_ASSERT(Vm::execute(&i, next, &b));
+  SCOPE_ASSERT(Vm::execute(&i, &i, cur, next, &b));
   SCOPE_ASSERT_EQUAL(1u, next.size());
   SCOPE_ASSERT_EQUAL(Thread(&i+1, 0, 0, 0), next[0]);
   
   next.clear();
   b = 'c';
-  SCOPE_ASSERT(!Vm::execute(&i, next, &b));
+  SCOPE_ASSERT(!Vm::execute(&i, &i, cur, next, &b));
   SCOPE_ASSERT_EQUAL(0u, next.size());
+}
+
+SCOPE_TEST(executeJump) {
+  byte b;
+  Instruction i = Instruction::makeJump(18);
+  Thread      cur(&i, 0, 0, 0);
+  ThreadList  next;
+  SCOPE_ASSERT(Vm::execute(&i, &i, cur, next, &b));
+  SCOPE_ASSERT_EQUAL(0u, next.size());
+  SCOPE_ASSERT_EQUAL(&i+18, cur.PC);
 }
