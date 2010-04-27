c6591122f06ab4a745c6b84c1643c87427e34f47
Jon Stewart <jonathan.l.stewart@acm.org>
2010-04-26 22:54:56 -0400

    Execute a save label instruction, and then run a program... also, execute() should return true if the current thread should keep executing, false otherwise

--- include/vm.h
+++ include/vm.h
@@ -8,4 +8,5 @@ class Vm {
 public:
   
   static bool execute(const Instruction* base, const Instruction* pc, Thread& t, ThreadList& next, const byte* cur, uint64 offset);
+  static bool run(const Instruction* base, uint32 num, const byte* beg, const byte* end, uint64 startOffset);
 };
--- test/test_vm.cpp
+++ test/test_vm.cpp
@@ -7,7 +7,7 @@ SCOPE_TEST(executeLit) {
   Instruction i = Instruction::makeLit('a');
   Thread      cur(&i, 0, 0, 0);
   ThreadList  next;
-  SCOPE_ASSERT(Vm::execute(&i, &i, cur, next, &b, 0));
+  SCOPE_ASSERT(!Vm::execute(&i, &i, cur, next, &b, 0));
   SCOPE_ASSERT_EQUAL(1u, next.size());
   SCOPE_ASSERT_EQUAL(Thread(&i+1, 0, 0, 0), next[0]);
   
@@ -35,4 +35,25 @@ SCOPE_TEST(executeMatch) {
   SCOPE_ASSERT(!Vm::execute(&i, &i, cur, next, &b, 57));
   SCOPE_ASSERT_EQUAL(0u, next.size());
   SCOPE_ASSERT_EQUAL(Thread(&i, 0, 0, 57), cur);
+}
+
+SCOPE_TEST(executeSaveLabel) {
+  byte b;
+  Instruction i = Instruction::makeSaveLabel(31);
+  Thread      cur(&i, 0, 0, 0);
+  ThreadList  next;
+  SCOPE_ASSERT(Vm::execute(&i, &i, cur, next, &b, 47));
+  SCOPE_ASSERT_EQUAL(0u, next.size());
+  SCOPE_ASSERT_EQUAL(Thread(&i+1, 31, 47, 0), cur);
+}
+
+SCOPE_TEST(simpleLitMatch) {
+  Instruction prog[4] = { Instruction::makeSaveLabel(3),
+                          Instruction::makeLit('a'),
+                          Instruction::makeLit('b'),
+                          Instruction::makeMatch()      };
+  byte text[] = {'a', 'b', 'c'};
+  SCOPE_ASSERT(Vm::run(prog, 4, text, &text[3], 35));
+  text[1] = 'c';
+  SCOPE_ASSERT(!Vm::run(prog, 4, text, &text[3], 35));
 }
