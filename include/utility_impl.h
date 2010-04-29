26069aeb57290d8649628643e27c081cae203d22
Jon Stewart <jonathan.l.stewart@acm.org>
2010-04-28 21:47:54 -0400

    Groping my way towards an algorithm for generating the program. I think this will calculate where each state should put its instructions.

--- include/utility_impl.h
+++ include/utility_impl.h
@@ -6,10 +6,35 @@
 #include <iostream>
 #include <cstring>
 #include <exception>
+#include <algorithm>
 
 #include <boost/graph/breadth_first_search.hpp>
 
 static const uint32 UNALLOCATED = 0xffffffff;
+
+struct CodeGenHelper {
+  CodeGenHelper(uint32 numStates): Snippets(numStates), Guard(0) {}
+
+  void addSnippet(uint32 state, uint32 num) {
+    Snippets[state] = std::make_pair(Guard, num);
+    Guard += num;
+  }
+
+  std::vector< std::pair< uint32, uint32 > > Snippets;
+  uint32 Guard;
+};
+
+class CodeGenVisitor: public boost::default_bfs_visitor {
+public:
+  CodeGenVisitor(boost::shared_ptr<CodeGenHelper> helper): Helper(helper) {}
+
+  void examine_vertex(DynamicFSM::vertex_descriptor v, const DynamicFSM& graph) {
+    Helper->addSnippet(v, std::max(out_degree(v, graph), 1ul) + (v == 0 ? 0: 1));
+  }
+
+private:
+  boost::shared_ptr<CodeGenHelper> Helper;
+};
 
 struct LayoutHelper {
   LayoutHelper(uint32 numStates, byte* buf): SizeVec(numStates), Offsets(numStates), LastState(UNALLOCATED), Buffer(buf), Guard(buf) {}
