04b1656bfe8208ee08ba064da7d64a9cff93afbe
Jon Stewart <jonathan.l.stewart@acm.org>
2010-05-03 18:45:30 -0400

    Added a label to a Transition

--- include/transition.h
+++ include/transition.h
@@ -4,8 +4,11 @@
 
 class Instruction;
 
+#pragma pack(1)
 class Transition {
 public:
+  Transition(): Label(0xffffffff) {}
+  Transition(uint32 lbl): Label(lbl) {}
   virtual ~Transition() {}
 
   virtual const byte* allowed(const byte* beg, const byte* end) const = 0;
@@ -14,4 +17,7 @@ public:
   virtual Transition* clone(void* buffer) const = 0;
   virtual size_t numInstructions() const = 0;
   virtual bool   toInstruction(Instruction* addr) const = 0;
+
+  uint32 Label;
 };
+#pragma pack(pop)
