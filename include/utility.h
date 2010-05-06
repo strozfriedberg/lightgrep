ef4df72f9fe2bc3fd0ab0547bb62d51b76b91830
Jon Stewart <jonathan.l.stewart@acm.org>
2010-05-06 08:21:00 -0400

    Removed all the code related to StaticFSM and DFASearch

--- include/utility.h
+++ include/utility.h
@@ -2,15 +2,10 @@
 
 #include "basic.h"
 
-#include "dynamicFSM.h"
-#include "staticFSM.h"
 #include "instructions.h"
+#include "dynamicFSM.h"
 
 DynamicFSMPtr createDynamicFSM(const std::vector<std::string>& keywords);
-
-boost::shared_ptr<StaticFSM> convert_to_static(const DynamicFSM& graph);
-
-uint32 staticStateSize(DynamicFSM::vertex_descriptor state, const DynamicFSM& graph);
 
 boost::shared_ptr< std::vector<Instruction> > createProgram(const DynamicFSM& graph);
 
--- src/utility.cpp
+++ src/utility.cpp
@@ -44,36 +44,6 @@ DynamicFSMPtr createDynamicFSM(const std::vector<std::string>& keywords) {
   return g;
 }
 
-boost::shared_ptr<StaticFSM> convert_to_static(const DynamicFSM& graph) {
-  boost::shared_ptr<StaticFSM> ret(new StaticFSM());
-  std::pair<EdgeIt, EdgeIt> edges = boost::edges(graph);
-  uint32 tSize = 0;
-  for (EdgeIt cur = edges.first; cur != edges.second; ++cur) {
-    tSize += graph[*cur]->objSize();
-  }
-  uint32 bufSize = ret->allocate(tSize, num_vertices(graph), num_edges(graph));
-  if (bufSize > 0) {
-    boost::shared_ptr<LayoutHelper> helper(new LayoutHelper(num_vertices(graph), ret->getRawBuffer()));
-    LayoutVisitor vis(helper);
-    // std::cout << " *** starting ***" << std::endl;
-    boost::breadth_first_search(graph, 0, visitor(vis));
-    // std::cout << " *** finishing ***" << std::endl;
-    ret->setLastState(helper->LastState);
-  }
-  return ret;
-}
-
-uint32 staticStateSize(DynamicFSM::vertex_descriptor state, const DynamicFSM& graph) {
-  uint32 numEdges = 0,
-         tSize = 0;
-  std::pair<DynamicFSM::out_edge_iterator, DynamicFSM::out_edge_iterator> adj(boost::out_edges(state, graph));
-  for (DynamicFSM::out_edge_iterator cur = adj.first; cur != adj.second; ++cur) {
-    ++numEdges;
-    tSize += graph[*cur]->objSize();
-  }
-  return sizeof(uint32) + (boost::out_degree(state, graph) * sizeof(StaticEdge)) + tSize;
-}
-
 boost::shared_ptr< std::vector<Instruction> > createProgram(const DynamicFSM& graph) {
   boost::shared_ptr< std::vector<Instruction> > ret(new std::vector<Instruction>());
   boost::shared_ptr<CodeGenHelper> cg(new CodeGenHelper(boost::num_vertices(graph)));
