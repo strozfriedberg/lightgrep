a5fc1bd9131e42c129e15515764b283c7459394e
Jon Stewart <jonathan.l.stewart@acm.org>
2010-05-28 10:40:21 +0200

    Test that alternation has precedence over concatenation

--- test/test_parser.cpp
+++ test/test_parser.cpp
@@ -27,6 +27,7 @@ std::ostream& operator<<(std::ostream& out, const VList& list) {
     out << *it;
   }
   out << '}';
+  return out;
 }
 
 struct Fragment {
@@ -129,7 +130,7 @@ private:
   std::stack< Fragment > Stack;
 };
 
-void Parser::callback(const std::string& type, Node n) {
+void Parser::callback(const std::string&, Node n) {
   // std::cout << type << std::endl;
   switch (n.Type) {
     case REGEXP:
@@ -181,6 +182,18 @@ SCOPE_TEST(parseAB) {
   SCOPE_ASSERT_EQUAL(1u, boost::out_degree(0, fsm));
   SCOPE_ASSERT_EQUAL(1u, boost::out_degree(1, fsm));
   SCOPE_ASSERT_EQUAL(0u, boost::out_degree(2, fsm));
+}
+
+SCOPE_TEST(parseAlternationAndConcatenation) {
+  Parser      p;
+  SyntaxTree  tree;
+  DynamicFSM& fsm(*p.getFsm());
+  SCOPE_ASSERT(parse("a|bc", tree, boost::bind(&Parser::callback, &p, _1, _2)));
+  SCOPE_ASSERT_EQUAL(4u, boost::num_vertices(fsm));
+  SCOPE_ASSERT_EQUAL(2u, boost::out_degree(0, fsm));
+  SCOPE_ASSERT_EQUAL(0u, boost::out_degree(1, fsm));
+  SCOPE_ASSERT_EQUAL(1u, boost::out_degree(2, fsm));
+  SCOPE_ASSERT_EQUAL(0u, boost::out_degree(3, fsm));
 }
 
 // 
