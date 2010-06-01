a858364900d0eb80160adb8690441e8e2d996920
Jon Stewart <jonathan.l.stewart@acm.org>
2010-06-01 08:14:49 -0400

    Fixed a retarded bug in my parsing of hex codes

--- test/test_parser.cpp
+++ test/test_parser.cpp
@@ -217,3 +217,18 @@ SCOPE_TEST(parseHexCode) {
   SCOPE_ASSERT_EQUAL(1u, set.count());
   SCOPE_ASSERT(set[' ']);
 }
+
+SCOPE_TEST(parseHexDotPlus) {
+  Parser      p;
+  SyntaxTree  tree;
+  DynamicFSM& fsm(*p.getFsm());
+  SCOPE_ASSERT(parse("\\x20\\xFF.+\\x20", tree, boost::bind(&Parser::callback, &p, _1, _2)));
+  SCOPE_ASSERT_EQUAL(5u, boost::num_vertices(fsm));
+  SCOPE_ASSERT_EQUAL(1u, boost::out_degree(0, fsm));
+  SCOPE_ASSERT_EQUAL(1u, boost::out_degree(1, fsm));
+  SCOPE_ASSERT_EQUAL(1u, boost::out_degree(2, fsm));
+  SCOPE_ASSERT_EQUAL(2u, boost::out_degree(3, fsm));
+  SCOPE_ASSERT_EQUAL(2u, boost::in_degree(3, fsm));
+  SCOPE_ASSERT_EQUAL(0u, boost::out_degree(4, fsm));
+  SCOPE_ASSERT_EQUAL(1u, boost::in_degree(4, fsm));
+}
