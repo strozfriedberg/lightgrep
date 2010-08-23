e934e570f9a5a4c5b9133285ee7f813cf98969fb
Jon Stewart <jonathan.l.stewart@acm.org>
2010-08-23 07:21:44 -0400

    Converted debugs to std::cout from std::cerr and increased the buffer size. This was able to take in 32GB of data from two different threads.

--- test/server.cpp
+++ test/server.cpp
@@ -9,12 +9,13 @@
 
 using boost::asio::ip::tcp;
 
-static const uint64 BUF_SIZE = 16384;
+static const uint64 BUF_SIZE = 1024 * 1024;
 
 void processConn(tcp::socket* socketPtr) {
-  byte  data[BUF_SIZE];
+  boost::scoped_array<byte> data(new byte[BUF_SIZE]);
   boost::shared_ptr<tcp::socket> sock(socketPtr);
   std::size_t len = 0;
+  uint64 totalRead = 0;
   try {
     while (true) {
       uint64 number = 0;
@@ -22,37 +23,36 @@ void processConn(tcp::socket* socketPtr) {
       if (len == sizeof(number)) {
         uint64 toRead = number;
         while (toRead > 0) {
-          len = sock->read_some(boost::asio::buffer(data, std::min(BUF_SIZE - 1, toRead)));
-          std::cerr << "read " << len << " bytes" << std::endl;
-          data[len] = 0;
-          std::cerr << data << std::endl;
+          len = sock->read_some(boost::asio::buffer(data.get(), std::min(BUF_SIZE - 1, toRead)));
+          std::cout << "read " << len << " bytes\n";
           toRead -= len;
+          totalRead += len;
         }
       }
       // uint32 i = ntohl(*(uint32*)data);
     }
   }
   catch (std::exception& e) {
-    std::cerr << "broke out of reading socket " << sock->remote_endpoint() << std::endl;
+    std::cout << "broke out of reading socket " << sock->remote_endpoint() << '\n';
   }
-  std::cerr << "thread dying" << std::endl;
+  std::cout << "thread dying\n";
 }
 
 void startup() {
   try {
     boost::asio::io_service srv;
-    std::cerr << "Created service" << std::endl;
-    tcp::acceptor acceptor(srv, tcp::endpoint(tcp::v4(), 31778));
-    std::cerr << "Created acceptor" << std::endl;
+    std::cout << "Created service" << std::endl;
+    tcp::acceptor acceptor(srv, tcp::endpoint(tcp::v4(), 12777));
+    std::cout << "Created acceptor" << std::endl;
     while (true) {
       std::auto_ptr<tcp::socket> socket(new tcp::socket(srv));
-      std::cerr << "Created socket" << std::endl;
+      std::cout << "Created socket" << std::endl;
       acceptor.accept(*socket);
-      std::cerr << "Accepted socket from " << socket->remote_endpoint() << " on " << socket->local_endpoint() << std::endl;
+      std::cout << "Accepted socket from " << socket->remote_endpoint() << " on " << socket->local_endpoint() << std::endl;
       boost::thread spawned(boost::bind(processConn, socket.release())); // launches the thread, then detaches
     }
   }
   catch (std::exception& e) {
-    std::cerr << e.what() << std::endl;
+    std::cout << e.what() << std::endl;
   }
 }
