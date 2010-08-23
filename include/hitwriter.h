e0b9269758a9af64a5c4a77f9240b4dc36a016c9
Jon Stewart <jonathan.l.stewart@acm.org>
2010-08-23 12:53:30 -0400

    Changed the output to print the keyword and the encoding

--- include/hitwriter.h
+++ include/hitwriter.h
@@ -8,10 +8,17 @@ class HitWriter: public HitCallback {
 public:
   uint64  NumHits;
 
-  HitWriter(std::ostream& outStream): NumHits(0), Out(outStream) {}
+  HitWriter(std::ostream& outStream,
+            const std::vector< std::pair<uint32, uint32> >& tbl,
+            const std::vector<std::string>& keys,
+            const std::vector<std::string>& encodings):
+            NumHits(0), Out(outStream), Table(tbl), Keys(keys), Encodings(encodings) {}
 
   virtual void collect(const SearchHit& hit);
 
 private:
   std::ostream& Out;
+  const std::vector< std::pair<uint32, uint32> >& Table;
+  const std::vector< std::string >& Keys;
+  const std::vector< std::string >& Encodings;
 };
