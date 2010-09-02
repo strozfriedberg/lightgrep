c2055e5a76394b9cae019df79494a79b0042e087
Jon Stewart <jonathan.l.stewart@acm.org>
2010-09-02 17:57:17 -0400

    Wired up the option to enable case-insensitivity. Also, extracted a class for holding options set from the command-line.

--- test/main.cpp
+++ test/main.cpp
@@ -21,6 +21,32 @@ namespace { // I am so bad
                 DebugEnd   = std::numeric_limits<uint64>::max();
 }
 
+struct Options {
+  uint64  DebugBegin,
+          DebugEnd;
+          
+  std::string KeyFile,
+              Command,
+              Input,
+              Encoding;
+  bool    CaseSensitive;
+
+  uint32 getEncoding() const {
+    uint32 value = 0;
+    if (Encoding == "ucs16") {
+      value |= CP_UCS16;
+    }
+    else if (Encoding == "both") {
+      value |= CP_UCS16;
+      value |= CP_ASCII;
+    }
+    else if (Encoding == "ascii") {
+      value |= CP_ASCII;
+    }
+    return value;
+  }
+};
+
 void startup(ProgramPtr p, const KwInfo& keyInfo);
 
 bool readKeyFile(const std::string& keyFilePath, std::vector<std::string>& keys) {
@@ -44,29 +70,29 @@ bool readKeyFile(const std::string& keyFilePath, std::vector<std::string>& keys)
   }
 }
 
-void writeGraphviz(const std::string& keyFilePath, uint32 enc) {
+void writeGraphviz(const Options& opts) {
   std::vector<std::string> keys;
-  if (readKeyFile(keyFilePath, keys)) {
-    DynamicFSMPtr fsm = createDynamicFSM(keys, enc);
+  if (readKeyFile(opts.KeyFile, keys)) {
+    DynamicFSMPtr fsm = createDynamicFSM(keys, opts.getEncoding());
     writeGraphviz(std::cout, *fsm);
   }
 }
 
-void writeProgram(const std::string& keyFilePath, uint32 enc) {
+void writeProgram(const Options& opts) {
   std::vector<std::string> keys;
-  if (readKeyFile(keyFilePath, keys)) {
-    DynamicFSMPtr fsm = createDynamicFSM(keys, enc);
+  if (readKeyFile(opts.KeyFile, keys)) {
+    DynamicFSMPtr fsm = createDynamicFSM(keys, opts.getEncoding());
     ProgramPtr p = createProgram(*fsm);
     std::cout << p->size() << " instructions" << std::endl;
     std::cout << *p;
   }
 }
 
-ProgramPtr initProgram(const std::string& keyFilePath, KwInfo& keyInfo, uint32 enc) {
-  readKeyFile(keyFilePath, keyInfo.Keywords);
+ProgramPtr initProgram(const Options& opts, KwInfo& keyInfo) {
+  readKeyFile(opts.KeyFile, keyInfo.Keywords);
   std::cerr << keyInfo.Keywords.size() << " keywords"<< std::endl;
 
-  DynamicFSMPtr fsm = createDynamicFSM(keyInfo, enc);
+  DynamicFSMPtr fsm = createDynamicFSM(keyInfo, opts.getEncoding(), opts.CaseSensitive);
   std::cerr << boost::num_vertices(*fsm) << " vertices" << '\n';
 
   ProgramPtr p = createProgram(*fsm);
@@ -91,8 +117,8 @@ ProgramPtr initProgram(const std::string& keyFilePath, KwInfo& keyInfo, uint32 e
   return p;
 }
 
-boost::shared_ptr<Vm> initSearch(const std::string& keyFilePath, KwInfo& keyInfo, uint32 enc) {
-  ProgramPtr p = initProgram(keyFilePath, keyInfo, enc);
+boost::shared_ptr<Vm> initSearch(const Options& opts, KwInfo& keyInfo) {
+  ProgramPtr p = initProgram(opts, keyInfo);
 
   boost::shared_ptr<Vm> ret(new Vm);
   ret->init(p);
@@ -113,12 +139,12 @@ void printHelp(const po::options_description& desc) {
     << desc << std::endl;
 }
 
-void search(const string& keyfile, const string& input, uint32 enc) {
-  std::ifstream file(input.c_str(), ios::in | ios::binary | ios::ate);
+void search(const Options& opts) {
+  std::ifstream file(opts.Input.c_str(), ios::in | ios::binary | ios::ate);
   if (file) {
     file.rdbuf()->pubsetbuf(0, 0);
     KwInfo keyInfo;
-    boost::shared_ptr<Vm> search = initSearch(keyfile, keyInfo, enc);
+    boost::shared_ptr<Vm> search = initSearch(opts, keyInfo);
 
     HitWriter cb(std::cout, keyInfo.PatternsTable, keyInfo.Keywords, keyInfo.Encodings);
 
@@ -167,10 +193,7 @@ void search(const string& keyfile, const string& input, uint32 enc) {
 }
 
 int main(int argc, char** argv) {
-  string cmd,
-         encoding,
-         keyfile,
-         input;
+  Options opts;
 
   po::options_description desc("Allowed Options");
   po::positional_options_description posOpts;
@@ -179,45 +202,40 @@ int main(int argc, char** argv) {
   desc.add_options()
     ("help", "produce help message")
     ("test", "run unit tests (same as test command)")
-    ("encoding,e", po::value< std::string >(&encoding)->default_value("ascii"), "encodings to use [ascii|ucs16|both]")
-    ("command,c", po::value< std::string >(&cmd)->default_value("search"), "command to perform [search|graph|prog|test|server]")
-    ("keywords,k", po::value< std::string >(&keyfile), "path to file containing keywords")
-    ("input,i", po::value< std::string >(&input)->default_value("-"), "file to search")
+    ("encoding,e", po::value< std::string >(&opts.Encoding)->default_value("ascii"), "encodings to use [ascii|ucs16|both]")
+    ("command,c", po::value< std::string >(&opts.Command)->default_value("search"), "command to perform [search|graph|prog|test|server]")
+    ("keywords,k", po::value< std::string >(&opts.KeyFile), "path to file containing keywords")
+    ("input", po::value< std::string >(&opts.Input)->default_value("-"), "file to search")
+    ("ignore-case,i", "file to search")
     #ifdef LBT_TRACE_ENABLED
-    ("begin-debug", po::value< uint64 >(&DebugBegin)->default_value(std::numeric_limits<uint64>::max()), "offset for beginning of debug logging")
-    ("end-debug", po::value< uint64 >(&DebugEnd)->default_value(std::numeric_limits<uint64>::max()), "offset for end of debug logging")
+    ("begin-debug", po::value< uint64 >(&opts.DebugBegin)->default_value(std::numeric_limits<uint64>::max()), "offset for beginning of debug logging")
+    ("end-debug", po::value< uint64 >(&opts.DebugEnd)->default_value(std::numeric_limits<uint64>::max()), "offset for end of debug logging")
     #endif
     ;
   
-  po::variables_map opts;
+  po::variables_map optsMap;
   try {
-    po::store(po::command_line_parser(argc, argv).options(desc).positional(posOpts).run(), opts);
-    po::notify(opts);
-    if (opts.count("help")) {
+    po::store(po::command_line_parser(argc, argv).options(desc).positional(posOpts).run(), optsMap);
+    po::notify(optsMap);
+    opts.CaseSensitive = optsMap.count("ignore-case") == 0;
+    if (optsMap.count("help")) {
       printHelp(desc);
     }
-    else if (cmd == "test" || opts.count("test")) {
+    else if (opts.Command == "test" || optsMap.count("test")) {
       return scope::DefaultRun(std::cout, argc, argv) ? 0: 1;
     }
-    uint32 enc = 0;
-    if (encoding == "ascii" || encoding == "both") {
-      enc |= CP_ASCII;
+    if (opts.Command == "search") {
+      search(opts);
     }
-    if (encoding == "ucs16" || encoding == "both") {
-      enc |= CP_UCS16;
+    else if (opts.Command == "graph") {
+      writeGraphviz(opts);
     }
-    if (cmd == "search") {
-      search(keyfile, input, enc);
+    else if (opts.Command == "prog") {
+      writeProgram(opts);
     }
-    else if (cmd == "graph") {
-      writeGraphviz(keyfile, enc);
-    }
-    else if (cmd == "prog") {
-      writeProgram(keyfile, enc);
-    }
-    else if (cmd == "server") {
+    else if (opts.Command == "server") {
       KwInfo keyInfo;
-      ProgramPtr p = initProgram(keyfile, keyInfo, enc);
+      ProgramPtr p = initProgram(opts, keyInfo);
       startup(p, keyInfo);
     }
   }
