dfdc58ad5e00bac65e3981f733c2e69ae3a9c813
Joel Uckelman <uckelman@nomic.net>
2010-11-08 14:33:51 +0100

    Added test for a.a+

--- test/test_searches.cpp
+++ test/test_searches.cpp
@@ -165,9 +165,19 @@ SCOPE_FIXTURE_CTOR(zeroDotStarZeroSearch, STest, STest("0.*0")) {
   SCOPE_ASSERT_EQUAL(SearchHit(0, 5, 0), fixture.Hits[0]);
 }
 
+SCOPE_FIXTURE_CTOR(aDotaPlusSearch, STest, STest("a.a+")) {
+  const byte* text = (const byte*)"aaabaaa";
+  fixture.Grep.search(text, text + 7, 0, fixture);
+  SCOPE_ASSERT_EQUAL(2u, fixture.Hits.size());
+  SCOPE_ASSERT_EQUAL(SearchHit(0, 3, 0), fixture.Hits[0]);
+  SCOPE_ASSERT_EQUAL(SearchHit(4, 3, 0), fixture.Hits[1]);
+}
+
+// FIXME: this one should actually be rejected; how to fix the test?
 SCOPE_FIXTURE_CTOR(aStarSearch, STest, STest("a*")) {
   const byte* text = (const byte*)"a";
   fixture.Grep.search(text, text + 1, 0, fixture);
   SCOPE_ASSERT_EQUAL(1u, fixture.Hits.size());
   SCOPE_ASSERT_EQUAL(SearchHit(0, 1, 0), fixture.Hits[0]);
 }
+
