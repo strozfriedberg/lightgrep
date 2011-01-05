19409cd1061bcfe543775dfa3a7e9920f9b2120a
Joel Uckelman <uckelman@nomic.net>
2011-01-05 05:58:47 +0100

    Use a (correct) label walk-back which is O(n) instead of O(n^2).

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -1,8 +1,10 @@
 #include "compiler.h"
-
 #include "utility.h"
-#include <stack>
+
 #include <iostream>
+#include <set>
+#include <stack>
+#include <vector>
 
 #include <boost/bind.hpp>
 
@@ -85,11 +87,10 @@ void Compiler::mergeIntoFSM(DynamicFSM& fsm, const DynamicFSM& addend, uint32 ke
 void Compiler::labelGuardStates(DynamicFSM& fsm) {
   using namespace boost;
 
-  std::vector<bool> visited(fsm.numVertices());
   uint32 i = 0;
   
-  DynamicFSM::const_iterator mi_end = fsm.end();
-  for (DynamicFSM::const_iterator mi = fsm.begin(); mi != mi_end; ++mi) {
+  DynamicFSM::const_iterator mi_end(fsm.end());
+  for (DynamicFSM::const_iterator mi(fsm.begin()); mi != mi_end; ++mi) {
     DynamicFSM::vertex_descriptor m = *mi;
 
     // skip non-match vertices
@@ -98,15 +99,115 @@ void Compiler::labelGuardStates(DynamicFSM& fsm) {
     if (++i % 10000 == 0) {
       std::cerr << "handled " << i << " labeled vertices" << std::endl;
     }
+
     const unsigned int label = fsm[m]->Label;
     
     // walk guard label(s) back from this match state
     std::stack<DynamicFSM::vertex_descriptor,
                std::vector<DynamicFSM::vertex_descriptor> > next;
     
-    visited.assign(fsm.numVertices(), false);
-
     next.push(m);
+
+    while (!next.empty()) {
+      DynamicFSM::vertex_descriptor t = next.top();
+      next.pop();
+      
+      // check each parent of the current state
+      DynamicFSM::const_iterator ie(fsm.inVerticesBegin(t));
+      DynamicFSM::const_iterator ie_end(fsm.inVerticesEnd(t));
+      for ( ; ie != ie_end; ++ie) {
+        DynamicFSM::vertex_descriptor h = *ie;
+        
+        if (!fsm[h]) {
+          // Skip the initial state.
+          continue;
+        }
+        else if (fsm[h]->Label == UNALLOCATED) {
+          // Mark unmarked parents with our label and walk back to them.
+          fsm[h]->Label = label;
+          next.push(h);
+        }
+        else if (fsm[h]->Label == UNLABELABLE) {
+          // This parent is already marked as an ancestor of multiple match
+          // states; all paths from it back to the root are already marked
+          // as unlabelable, so we don't need to walk back from it.
+        }
+        else if (fsm[h]->Label == label) {
+          // This parent has our label, which means we've already walked
+          // back through it.
+        }
+        else {
+          // This parent has the label of some other match state. Mark it
+          // and all of its ancestors unlabelable.
+          std::stack<DynamicFSM::vertex_descriptor,
+               std::vector<DynamicFSM::vertex_descriptor> > unext;
+
+          unext.push(h);
+
+          while (!unext.empty()) {
+            DynamicFSM::vertex_descriptor u = unext.top();
+            unext.pop();
+
+            fsm[u]->Label = UNLABELABLE;
+
+            DynamicFSM::const_iterator ue(fsm.inVerticesBegin(u));
+            DynamicFSM::const_iterator ue_end(fsm.inVerticesEnd(u));
+            for ( ; ue != ue_end; ++ue) {
+              DynamicFSM::vertex_descriptor uh = *ue;
+              if (fsm[uh] && fsm[uh]->Label != UNLABELABLE) {
+                unext.push(uh);
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+ 
+  // Make a list of all tails of edges where the head is an ancestor of
+  // multiple match states, but the tail is an ancestor of only one.
+  std::vector<bool> visited(fsm.numVertices());
+
+  std::set<DynamicFSM::vertex_descriptor> heads;
+
+  std::stack<DynamicFSM::vertex_descriptor,
+             std::vector<DynamicFSM::vertex_descriptor> > next;
+
+  next.push(0);
+  visited[0] = true;
+
+  while (!next.empty()) {
+    DynamicFSM::vertex_descriptor h = next.top();
+    next.pop();
+
+    DynamicFSM::const_iterator vi(fsm.outVerticesBegin(h));
+    DynamicFSM::const_iterator vi_end(fsm.outVerticesEnd(h));
+    for ( ; vi != vi_end; ++vi) {
+      DynamicFSM::vertex_descriptor t = *vi;
+
+      if (visited[t] || !fsm[t]) continue; 
+
+      if (fsm[t]->Label == UNLABELABLE) {
+        next.push(t);
+      }
+      else {
+        heads.insert(t);
+      }
+
+      visited[t] = true;
+    }
+  }
+
+  // Unlabel every node not in heads
+  DynamicFSM::const_iterator vi_end(fsm.end());
+  for (DynamicFSM::const_iterator vi(fsm.begin()); vi != vi_end; ++vi) {
+    DynamicFSM::vertex_descriptor v = *vi;
+    if (fsm[v] && heads.find(v) == heads.end()) {
+      fsm[v]->Label = UNALLOCATED;
+    }
+  }
+
+/*
     while (!next.empty()) {
       DynamicFSM::vertex_descriptor t = next.top();
       next.pop();
@@ -176,4 +277,5 @@ void Compiler::labelGuardStates(DynamicFSM& fsm) {
       fsm[m]->Label = UNALLOCATED;
     }
   }
+*/
 }
