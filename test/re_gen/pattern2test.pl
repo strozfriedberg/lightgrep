773d2ae8ee97957b5891bf9a705ed182dea97c79
Joel Uckelman <uckelman@nomic.net>
2011-01-07 23:45:31 +0100

    Don't generate tests for patterns with zero-length matches.

--- test/re_gen/pattern2test.pl
+++ test/re_gen/pattern2test.pl
@@ -10,26 +10,40 @@ print TEXT $text;
 close TEXT;
 
 # print head stuff
-print <<HEAD
+print <<HEAD;
 #include <scope/test.h>
 
 #include "test_helper.h"
 
 HEAD
 
+my $patnum = 0;
+
 # read the patterns
 while (<>) {
   chomp;
 
-  # get matches from shitgrep
+  # get matches from shitgrep, 
+  system("./shitgrep -p '$_' $text_file 1>sg.stdout 2>sg.stderr");
+
+  open(SGERR, '<sg.stderr') or die "$!\n";
+  my $sgerr = join '', <SGERR>;
+  my $skip = $sgerr =~ /state \d+ is not allowed as a final state of the NFA/;
+  close SGERR;
+  next if ($skip);
+
+  open(SGOUT, '<sg.stdout') or die "$!\n";
+  my @sgout = <SGOUT>;
+  close SGOUT;
+
   my @matches = ();
-  foreach $m (`./shitgrep -p '$_' $text_file 2>/dev/null`) {
+  foreach $m (@sgout) {
     push(@matches, [(split('\t', $m))[0..2]]);
   }
 
   # print the scope test for this pattern and text
   print <<TEST;
-SCOPE_FIXTURE_CTOR(autoPatternTest$., STest, STest("$_")) {
+SCOPE_FIXTURE_CTOR(autoPatternTest$patnum, STest, STest("$_")) {
   const byte* text = (const byte*) "$text";
   fixture.search(text, text + @{[ length $text ]}, 0, fixture);
   SCOPE_ASSERT_EQUAL(@{[ $#matches + 1 ]}, fixture.Hits.size());
@@ -43,6 +57,8 @@ TEST
   }
 
   print "}\n\n";
+
+  ++$patnum;
 }
 
 # cleanup
