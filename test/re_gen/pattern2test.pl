33d0cc7182e687f78bd1419f725ea3d92c16ab98
Joel Uckelman <uckelman@nomic.net>
2011-01-08 02:59:23 +0100

    Generate tests for patterns with zero-length matches.

--- test/re_gen/pattern2test.pl
+++ test/re_gen/pattern2test.pl
@@ -22,41 +22,56 @@ my $patnum = 0;
 # read the patterns
 while (<>) {
   chomp;
+  $pat = $_;
 
-  # get matches from shitgrep, 
-  system("./shitgrep -p '$_' $text_file 1>sg.stdout 2>sg.stderr");
+  # get matches from shitgrep
+  system("./shitgrep -p '$pat' $text_file 1>sg.stdout 2>sg.stderr");
 
   open(SGERR, '<sg.stderr') or die "$!\n";
   my $sgerr = join '', <SGERR>;
-  my $skip = $sgerr =~ /state \d+ is not allowed as a final state of the NFA/;
+  my $zero = $sgerr =~ /state \d+ is not allowed as a final state of the NFA/;
   close SGERR;
-  next if ($skip);
+  
+  if ($zero) {
+    # test this pattern for zero-length matches
+    print <<TEST;
+SCOPE_TEST(autoPatternTest$patnum) {
+  Parser p;
+  SyntaxTree tree;
+  SCOPE_ASSERT(parse("$pat", false, tree, p));
+  SCOPE_ASSERT(!p.good());
+}
 
-  open(SGOUT, '<sg.stdout') or die "$!\n";
-  my @sgout = <SGOUT>;
-  close SGOUT;
-
-  my @matches = ();
-  foreach $m (@sgout) {
-    push(@matches, [(split('\t', $m))[0..2]]);
+TEST
   }
+  else {
+    # this pattern has no zero-length matches
+    open(SGOUT, '<sg.stdout') or die "$!\n";
+    my @sgout = <SGOUT>;
+    close SGOUT;
 
-  # print the scope test for this pattern and text
-  print <<TEST;
-SCOPE_FIXTURE_CTOR(autoPatternTest$patnum, STest, STest("$_")) {
+    my @matches = ();
+    foreach $m (@sgout) {
+      push(@matches, [(split('\t', $m))[0..2]]);
+    }
+
+    # print the scope test for this pattern and text
+    print <<TEST;
+SCOPE_FIXTURE_CTOR(autoPatternTest$patnum, STest, STest("$pat")) {
   const byte* text = (const byte*) "$text";
   fixture.search(text, text + @{[ length $text ]}, 0, fixture);
   SCOPE_ASSERT_EQUAL(@{[ $#matches + 1 ]}, fixture.Hits.size());
 TEST
 
-  for ($i = 0; $i <= $#matches; ++$i) {
-    printf(
-      "  SCOPE_ASSERT_EQUAL(SearchHit(%d, %d, %d), fixture.Hits[%d]);\n",
-      $matches[$i]->[0], $matches[$i]->[1], $matches[$i]->[2], $i
-    );
+    for ($i = 0; $i <= $#matches; ++$i) {
+      printf(
+        "  SCOPE_ASSERT_EQUAL(SearchHit(%d, %d, %d), fixture.Hits[%d]);\n",
+        $matches[$i]->[0], $matches[$i]->[1], $matches[$i]->[2], $i
+      );
+    }
+  
+    print "}\n\n";
   }
-
-  print "}\n\n";
 
   ++$patnum;
 }
