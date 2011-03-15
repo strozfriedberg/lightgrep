393982b7dbc086e5460fed5d3b7f2ed9e2816d17
Joel Uckelman <uckelman@nomic.net>
2011-03-15 17:39:05 +0100

    Added failing test for (a|b??)a.

--- test/test_searches.cpp
+++ test/test_searches.cpp
@@ -201,6 +201,13 @@ SCOPE_FIXTURE_CTOR(aQQOrBQQcSearch, STest, STest("(a??|b??)c")) {
   SCOPE_ASSERT_EQUAL(SearchHit(0, 1, 0), fixture.Hits[0]);
 }
 
+SCOPE_FIXTURE_CTOR(aOrBQQaSearch, STest, STest("(a|b??)a")) {
+  const byte* text = (const byte*) "aa";
+  fixture.search(text, text + 2, 0, fixture);
+  SCOPE_ASSERT_EQUAL(1, fixture.Hits.size());
+  SCOPE_ASSERT_EQUAL(SearchHit(0, 2, 0), fixture.Hits[0]);
+}
+
 SCOPE_FIXTURE_CTOR(startsWithTest, STest, STest(3, (const char*[]){"ab..ef", "c[a-z][a-z]", "[aA][bc][bc]"})) {
   const byte* text = (const byte*)"abcdefghijklmnop";
   fixture.startsWith(text, text + 16, 0, fixture);
