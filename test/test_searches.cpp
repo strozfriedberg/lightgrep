c828e24c5a77315f0c86540ca79f83c955259a5e
Joel Uckelman <uckelman@nomic.net>
2011-03-28 18:19:23 +0200

    Added failing tests for a+?b? and a+?b* on 'aab'.

--- test/test_searches.cpp
+++ test/test_searches.cpp
@@ -233,12 +233,28 @@ SCOPE_FIXTURE_CTOR(aaQQSearch, STest, STest("aa??")) {
   SCOPE_ASSERT_EQUAL(SearchHit(1, 1, 0), fixture.Hits[1]);
 }
 
-SCOPE_FIXTURE_CTOR(aaSQsearch, STest, STest("aa*?")) {
+SCOPE_FIXTURE_CTOR(aaSQSearch, STest, STest("aa*?")) {
   const byte* text = (const byte*) "aa";
   fixture.search(text, text + 2, 0, fixture);
   SCOPE_ASSERT_EQUAL(2, fixture.Hits.size());
   SCOPE_ASSERT_EQUAL(SearchHit(0, 1, 0), fixture.Hits[0]);
   SCOPE_ASSERT_EQUAL(SearchHit(1, 1, 0), fixture.Hits[1]);
+}
+
+SCOPE_FIXTURE_CTOR(aPQbQSearch, STest, STest("a+?b?")) {
+  const byte* text = (const byte*) "aab";
+  fixture.search(text, text + 3, 0, fixture);
+  SCOPE_ASSERT_EQUAL(2, fixture.Hits.size());
+  SCOPE_ASSERT_EQUAL(SearchHit(0, 1, 0), fixture.Hits[0]);
+  SCOPE_ASSERT_EQUAL(SearchHit(1, 2, 0), fixture.Hits[1]);
+}
+
+SCOPE_FIXTURE_CTOR(aPQbSSearch, STest, STest("a+?b*")) {
+  const byte* text = (const byte*) "aab";
+  fixture.search(text, text + 3, 0, fixture);
+  SCOPE_ASSERT_EQUAL(2, fixture.Hits.size());
+  SCOPE_ASSERT_EQUAL(SearchHit(0, 1, 0), fixture.Hits[0]);
+  SCOPE_ASSERT_EQUAL(SearchHit(1, 2, 0), fixture.Hits[1]);
 }
 
 SCOPE_FIXTURE_CTOR(startsWithTest, STest, STest(3, (const char*[]){"ab..ef", "c[a-z][a-z]", "[aA][bc][bc]"})) {
