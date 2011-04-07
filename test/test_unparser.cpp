4a6bd1a4492974cfd6351bc37f13faae461c0aaa
Joel Uckelman <uckelman@nomic.net>
2011-04-07 12:22:36 +0200

    Added an "unparser" for constructing patterns from parse trees.

---
+++ test/test_unparser.cpp
@@ -0,0 +1,60 @@
+#include <scope/test.h>
+
+#include "parser.h"
+#include "parsetree.h"
+#include "unparser.h"
+
+SCOPE_TEST(parseUnparseTest) {
+  ParseTree tree;
+
+  SCOPE_ASSERT(parse("a", false, tree));
+  SCOPE_ASSERT_EQUAL("a", unparse(tree));
+
+  SCOPE_ASSERT(parse("a?", false, tree));
+  SCOPE_ASSERT_EQUAL("a?", unparse(tree));
+
+  SCOPE_ASSERT(parse("a*", false, tree));
+  SCOPE_ASSERT_EQUAL("a*", unparse(tree));
+
+  SCOPE_ASSERT(parse("a+", false, tree));
+  SCOPE_ASSERT_EQUAL("a+", unparse(tree));
+
+  SCOPE_ASSERT(parse("a??", false, tree));
+  SCOPE_ASSERT_EQUAL("a??", unparse(tree));
+
+  SCOPE_ASSERT(parse("a*?", false, tree));
+  SCOPE_ASSERT_EQUAL("a*?", unparse(tree));
+
+  SCOPE_ASSERT(parse("a+?", false, tree));
+  SCOPE_ASSERT_EQUAL("a+?", unparse(tree));
+
+  SCOPE_ASSERT(parse("ab", false, tree));
+  SCOPE_ASSERT_EQUAL("ab", unparse(tree));
+
+  SCOPE_ASSERT(parse("a*b", false, tree));
+  SCOPE_ASSERT_EQUAL("a*b", unparse(tree));
+
+  SCOPE_ASSERT(parse("ab*", false, tree));
+  SCOPE_ASSERT_EQUAL("ab*", unparse(tree));
+
+  SCOPE_ASSERT(parse("a*b*", false, tree));
+  SCOPE_ASSERT_EQUAL("a*b*", unparse(tree));
+
+  SCOPE_ASSERT(parse("a|b", false, tree));
+  SCOPE_ASSERT_EQUAL("a|b", unparse(tree));
+
+  SCOPE_ASSERT(parse("a*|b", false, tree));
+  SCOPE_ASSERT_EQUAL("a*|b", unparse(tree));
+
+  SCOPE_ASSERT(parse("a|b*", false, tree));
+  SCOPE_ASSERT_EQUAL("a|b*", unparse(tree));
+  
+  SCOPE_ASSERT(parse("a*|b*", false, tree));
+  SCOPE_ASSERT_EQUAL("a*|b*", unparse(tree));
+
+  SCOPE_ASSERT(parse("(a)", false, tree));
+  SCOPE_ASSERT_EQUAL("a", unparse(tree));
+
+  SCOPE_ASSERT(parse("(ab)*", false, tree));
+  SCOPE_ASSERT_EQUAL("(ab)*", unparse(tree));
+}
