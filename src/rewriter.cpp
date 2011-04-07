175e278698076f402e061c02ac5fa7b30da410d2
Joel Uckelman <uckelman@nomic.net>
2011-04-07 19:13:46 +0200

    Replaced twisty prune_tree() with reduce_trailing_nongreedy().

--- src/rewriter.cpp
+++ src/rewriter.cpp
@@ -1,20 +1,39 @@
 
 #include "rewriter.h"
 
+#include <iostream>
 #include <stack>
 
 #include <boost/lexical_cast.hpp>
 
-void splice_out_parent(Node* node, std::stack<Node*>& branch) {
-  Node* parent = branch.top();
-  branch.pop();
-  Node* gparent = branch.top();
-
-  if (gparent->Left == parent) {
-    gparent->Left = node;
+void print_tree(std::ostream& out, const Node& n) {
+  if (n.Left) {
+    // this node has a left child
+    print_tree(out, *n.Left);
   }
-  else {
-    gparent->Right = node;
+
+  if (n.Right) {
+    // this node has a right child
+    print_tree(out, *n.Right);
+  }
+
+  out << n << '\n';
+}
+
+void print_branch(std::ostream& out, std::stack<Node*>& branch) {
+  std::stack<Node*> tmp;
+
+  while (!branch.empty()) {
+    tmp.push(branch.top());
+    branch.pop();
+  }
+
+  Node* n;
+  while (!tmp.empty()) {
+    n = tmp.top();
+    out << *n << '\n';
+    branch.push(n);
+    tmp.pop();
   }
 }
 
@@ -45,16 +64,46 @@ bool has_zero_length_match(const Node *n) {
   default:
     // WTF?
     throw std::logic_error(boost::lexical_cast<std::string>(n->Type));
-    return false;
   }
 }
 
-void reduce_trailing_zero_length(Node *root) {
-  Node* n = root;
+bool prefers_zero_length_match(const Node* n) {
+  switch (n->Type) {
+  case Node::REGEXP:
+  case Node::PLUS:
+  case Node::PLUS_NG:
+  case Node::STAR:
+  case Node::QUESTION:
+    return prefers_zero_length_match(n->Left);
 
+  case Node::STAR_NG:
+  case Node::QUESTION_NG:
+    return true;
+
+  case Node::ALTERNATION:
+    return prefers_zero_length_match(n->Left) ||
+           prefers_zero_length_match(n->Right);
+
+  case Node::CONCATENATION:
+    return prefers_zero_length_match(n->Left) &&
+           prefers_zero_length_match(n->Right);
+
+  case Node::DOT:
+  case Node::CHAR_CLASS:
+  case Node::LITERAL:
+    return false;
+
+  default:
+    // WTF?
+    throw std::logic_error(boost::lexical_cast<std::string>(n->Type));
+  }
+}
+
+bool reduce_trailing_nongreedy_then_empty(Node *root) {
   // Look for S+?T along trailing branches, where T admits zero-length
   // matches.
 
+  Node* n = root;
   while (n) {
     switch (n->Type) {
     case Node::REGEXP:
@@ -71,7 +120,7 @@ void reduce_trailing_zero_length(Node *root) {
     case Node::CONCATENATION:
       if (n->Left->Type == Node::PLUS_NG && has_zero_length_match(n->Right)) {
         n->Left = n->Left->Left;
-        return;
+        return true;
       }
       break;
 
@@ -82,152 +131,138 @@ void reduce_trailing_zero_length(Node *root) {
       break;
     default:
       // WTF?
-      break;
+      throw std::logic_error(boost::lexical_cast<std::string>(n->Type));
     }
 
     n = n->Right ? n->Right : n->Left;
   }
+
+  return false;
 }
 
-void prune_tree(Node *root, std::stack<Node*> branch) {
-  Node* n = root;
-
-  // follow the rightmost branch
-  while (n) {
+void prune_subtree(Node *n, std::stack<Node*>& branch) {
+  if (n->Type == Node::REGEXP) {
+    n->Left = 0;
     branch.push(n);
+    return;
+  }
 
-    switch (n->Type) {
-    case Node::REGEXP:
-    case Node::CONCATENATION:
-    case Node::PLUS:
-    case Node::STAR:
-    case Node::QUESTION:
-      // keep going
-      break;
-    case Node::ALTERNATION:
-      prune_tree(n->Right, branch);
+  Node* p = branch.top();
 
-      if (n->Right == 0) {
-        if (n->Left == 0) {
-          // roll up back to the root
-          while (!branch.empty()) {
-            n = branch.top();
-            branch.pop();
-          }
+  switch (p->Type) {
+  case Node::REGEXP:
+    // the whole pattern has been pruned!
+    p->Left = 0;
+    break;
 
-          n->Left = 0; 
-        }
-        else {
-          splice_out_parent(n->Left, branch);
-          n = branch.top();
-        }
+  case Node::ALTERNATION:
+  case Node::CONCATENATION:
+    // replace the parent node with the sibling node
+    {
+      Node* sibling = p->Left == n ? p->Right : p->Left;
+
+      branch.pop();
+      Node* gp = branch.top();
+
+      if (gp->Left == p) {
+        gp->Left = sibling;
       }
       else {
-        prune_tree(n->Left, branch);
-        if (n->Left == 0) {
-          splice_out_parent(n->Right, branch);
-          n = branch.top();
-        }
-        else {
-          return;
-        }
+        gp->Right = sibling;
       }
-      break;
-    case Node::PLUS_NG:
-      // prune out +?, then continue from parent of +?
-      splice_out_parent(n->Left, branch);
-      n = branch.top();
-      break;
-    case Node::STAR_NG:
-    case Node::QUESTION_NG:
-      // prune everything below ??, *? and all repetitions immediately above
-      { 
-        Node* op = n;
-        branch.pop();
-        Node* parent = branch.top(); 
-
-        while (op) {
-          switch (parent->Type) {
-          case Node::REGEXP:
-            // delete subtree rooted at op
-            op = 0;
-            
-            parent->Left = 0; 
-            n = parent;
-            break;
-
-          case Node::ALTERNATION:
-            {
-              // delete subtree rooted at op
-              if (parent->Left == op) {
-                parent->Left = 0;
-              }
-              else {
-                parent->Right = 0;
-              }            
-
-              // check whether we have alternations back to the root
-              Node* x = branch.top();
-              while (x->Type == Node::ALTERNATION) {
-                branch.pop();
-                x = branch.top();
-              }
-
-              if (x->Type == Node::REGEXP) {
-                parent->Left = parent->Right = 0;
-              }
-
-              return;
-            }
-
-          case Node::CONCATENATION:
-            {
-              // delete subtree rooted at op
-              op = 0;
-      
-              splice_out_parent(parent->Left, branch);
-              n = branch.top();         
-            }
-            break;
-
-          case Node::PLUS:
-          case Node::STAR:
-          case Node::QUESTION:
-            // back up one node
-            op = parent;
-            branch.pop();
-            parent = branch.top();
-            break;
-
-          // already removed
-          case Node::PLUS_NG:
-          // can't be above us
-          case Node::STAR_NG:
-          case Node::QUESTION_NG:
-          // no children
-          case Node::DOT:
-          case Node::CHAR_CLASS:
-          case Node::LITERAL:
-          default:
-            // impossible
-            break;
-          }
-        }
-      }
-      break;
-    case Node::DOT:
-    case Node::CHAR_CLASS:
-    case Node::LITERAL:
-      // finished
-      break;
-    default:
-      break;
     }
+    break;
 
-    n = n->Right ? n->Right : n->Left;
+  case Node::PLUS:
+  case Node::STAR:
+  case Node::QUESTION:
+  case Node::PLUS_NG:
+  case Node::STAR_NG:
+  case Node::QUESTION_NG:
+    // prune the parent node
+    branch.pop();
+    prune_subtree(p, branch);
+    break;
+
+  default:
+    // WTF?
+    throw std::logic_error(boost::lexical_cast<std::string>(n->Type));
   }
 }
 
-void prune_tree(Node *root) {
-  prune_tree(root, std::stack<Node*>());
+bool reduce_trailing_nongreedy(Node* n, std::stack<Node*>& branch) {
+  switch (n->Type) {
+  case Node::REGEXP:
+    if (!n->Left) {
+      return false;
+    }
+  case Node::PLUS:
+  case Node::STAR:
+  case Node::QUESTION:
+    branch.push(n);
+    return reduce_trailing_nongreedy(n->Left, branch);
+
+  case Node::CONCATENATION:
+    branch.push(n);
+    return reduce_trailing_nongreedy(n->Right, branch);
+
+  case Node::ALTERNATION:
+    {
+      branch.push(n);
+      std::stack<Node*> orig_branch(branch);
+
+      const bool lreduce = reduce_trailing_nongreedy(n->Left, branch);
+
+      if (n->Left) {
+        // The left alternative wasn't pruned away, so we have to traverse
+        // the subtree of the right alternative ourselves.
+        return reduce_trailing_nongreedy(n->Right, orig_branch) || lreduce;
+      }
+      else {
+        // The left alternative was pruned away, so the right alternative
+        // was moved up the tree and has already been traversed.
+        return lreduce;
+      }
+    }
+
+  case Node::PLUS_NG:
+    // strip out +?
+    {
+      Node* p = branch.top();
+
+      if (p->Left == n) {
+        p->Left = n->Left;
+      }
+      else {
+        p->Right = n->Left;
+      }
+
+      reduce_trailing_nongreedy(n->Left, branch);
+      return true;
+    }
+
+  case Node::STAR_NG:
+  case Node::QUESTION_NG:
+    // prune this subtree
+    prune_subtree(n, branch);
+    n = branch.top();
+    branch.pop();
+    reduce_trailing_nongreedy(n, branch);
+    return true;
+
+  case Node::DOT:
+  case Node::CHAR_CLASS:
+  case Node::LITERAL:
+    // branch finished
+    return false;
+
+  default:
+    // WTF?
+    throw std::logic_error(boost::lexical_cast<std::string>(n->Type));
+  }
+}
+
+bool reduce_trailing_nongreedy(Node* root) {
+  std::stack<Node*> branch;
+  return reduce_trailing_nongreedy(root, branch);
 }
