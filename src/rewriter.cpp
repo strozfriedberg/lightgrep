12ed49216b4eaf96fea5cf3b9853c63544edb149
Joel Uckelman <uckelman@nomic.net>
2011-04-20 17:33:14 +0200

    Rewrote reduce_empty_subtrees to actually work.

--- src/rewriter.cpp
+++ src/rewriter.cpp
@@ -37,10 +37,22 @@ void print_branch(std::ostream& out, std::stack<Node*>& branch) {
   }
 }
 
+void splice_out_parent(Node* gp, const Node* p, Node* c) {
+  if (gp->Left == p) {
+    gp->Left = c;
+  }
+  else if (gp->Right == p) {
+    gp->Right = c;
+  }
+  else {
+    throw std::logic_error("wtf");
+  }
+}
+
 bool has_zero_length_match(const Node *n) {
   switch (n->Type) {
   case Node::REGEXP:
-    return has_zero_length_match(n->Left);
+    return !n->Left || has_zero_length_match(n->Left);
 
   case Node::ALTERNATION:
     return has_zero_length_match(n->Left) || has_zero_length_match(n->Right);
@@ -66,7 +78,7 @@ bool has_zero_length_match(const Node *n) {
 bool prefers_zero_length_match(const Node* n) {
   switch (n->Type) {
   case Node::REGEXP:
-    return prefers_zero_length_match(n->Left);
+    return !n->Left || prefers_zero_length_match(n->Left);
 
   case Node::ALTERNATION:
     // Left has priority, so we don't need to check right.
@@ -96,7 +108,7 @@ bool prefers_zero_length_match(const Node* n) {
 bool has_only_zero_length_match(const Node* n) {
   switch (n->Type) {
   case Node::REGEXP:
-    return has_only_zero_length_match(n->Left);
+    return !n->Left || has_only_zero_length_match(n->Left);
 
   case Node::ALTERNATION:
     // Left has priority, so we don't need to check right.
@@ -125,108 +137,87 @@ bool reduce_empty_subtrees(Node* n, std::stack<Node*>& branch) {
   
   // ST{0}Q = ST{0}?Q = T{0}QS = T{0}?Q = S
   // R(S{0}Q|T) = (S{0}Q|T)R = R
-  // (R|S{0}Q|T) = R?
+  // (S|T{0}Q) = S?
  
   bool ret = false;
   branch.push(n);
 
-  switch (n->Type) {
-  case Node::REGEXP:
-    if (!n->Left) {
-      return ret;
-    }
-
-    if (has_only_zero_length_match(n->Left)) {
-      // prune away the whole tree 
+  if (has_only_zero_length_match(n)) {
+    switch (n->Type) {
+    case Node::REGEXP:
+      // prune the whole tree
       n->Left = 0;
-      ret = true;
+      break;
+ 
+    case Node::ALTERNATION:
+    case Node::CONCATENATION:
+    case Node::REPETITION:
+    case Node::REPETITION_NG:
+      // replace this subtree with a dummy    
+      n->Type = Node::REPETITION;
+      n->Min = n->Max = 0;
+      n->Right = 0;
+
+      // this is safe---we know that n must have a left child if it is
+      // not the root and has a zero length match
+      n->Left->Type = Node::LITERAL;
+      n->Left->Left = n->Left->Right = 0;
+      n->Left->Val = 'x';
+      break;
+     
+    default:
+      // WTF?
+      throw std::logic_error(boost::lexical_cast<std::string>(n->Type));
     }
-    else {
+    ret = true;
+  }
+  else {
+    switch (n->Type) {
+    case Node::REGEXP:
+    case Node::REPETITION:
+    case Node::REPETITION_NG:
       ret = reduce_empty_subtrees(n->Left, branch);
-    }
-    branch.pop();
-    break;
+      break;
 
-  case Node::ALTERNATION:
-    // We don't check the left alternative for zero-length matches only
-    // here because if it did, then the whole alternation would, so we
-    // would already have pruned it away.
-
-    if (has_only_zero_length_match(n->Right)) {
-      Node* l = n->Left;
-      Node* old = n->Right;
-
-      // prune away right alternative
-      prune_subtree(n->Right, branch);
-      n = branch.top();
-      branch.pop();
-
-      // insert a ? as the parent of the left alternative,
-      // reusing a pruned node
-      old->Type = Node::REPETITION;
-      old->Min = 0;
-      old->Max = 1;
-      old->Left = l;
-      old->Right = 0;
-
-      if (l == n->Left) {
-        n->Left = old;
-      }
-      else {
-        n->Right = old;
-      }
-
-      reduce_empty_subtrees(old, branch);
-      ret = true;
-    }
-    else {
+    case Node::ALTERNATION:
+    case Node::CONCATENATION:
       ret = reduce_empty_subtrees(n->Left, branch);
       ret |= reduce_empty_subtrees(n->Right, branch);
-      branch.pop();
-    }
-    break;
+      break;
 
-  case Node::CONCATENATION:
+    case Node::DOT:
+    case Node::CHAR_CLASS:
+    case Node::LITERAL:
+      // branch finished
+      ret = false;
+      break;
+
+    default:
+      // WTF?
+      throw std::logic_error(boost::lexical_cast<std::string>(n->Type));
+    }
+  }
+
+  branch.pop();
+
+  // fix concatenations and alternations with dead children on the way up
+  if (n->Type == Node::CONCATENATION) {
+    // convert ST{0} and T{0}S into S
     if (has_only_zero_length_match(n->Left)) {
-      // prune away left conjunct
-      prune_subtree(n->Left, branch);
-      n = branch.top();
-      branch.pop();
-      reduce_empty_subtrees(n, branch);
-      ret = true;
+      splice_out_parent(branch.top(), n, n->Right);
     }
     else if (has_only_zero_length_match(n->Right)) {
-      // prune away right conjunct
-      prune_subtree(n->Right, branch);
-      n = branch.top();
-      branch.pop(); 
-      reduce_empty_subtrees(n, branch);
-      ret = true;
+      splice_out_parent(branch.top(), n, n->Left);
     }
-    else {
-      ret = reduce_empty_subtrees(n->Left, branch);
-      ret |= reduce_empty_subtrees(n->Right, branch);
-      branch.pop();
+  }
+  else if (n->Type == Node::ALTERNATION) {
+    if (has_only_zero_length_match(n->Right)) {
+      // convert S|T{0} into S?
+      n->Type = Node::REPETITION;
+      n->Min = 0;
+      n->Max = 1;
+      n->Right = 0;
     }
-    break;
-
-  case Node::REPETITION:
-  case Node::REPETITION_NG:
-    ret = reduce_empty_subtrees(n->Left, branch);
-    branch.pop();
-    break;
-
-  case Node::DOT:
-  case Node::CHAR_CLASS:
-  case Node::LITERAL:
-    // branch finished
-    ret = false;
-    branch.pop();
-    break;
-
-  default:
-    // WTF?
-    throw std::logic_error(boost::lexical_cast<std::string>(n->Type));
   }
 
   return ret;
@@ -411,50 +402,4 @@ bool reduce_trailing_nongreedy_then_empty(Node* n, std::stack<Node*>& branch) {
 bool reduce_trailing_nongreedy_then_empty(Node* root) {
   std::stack<Node*> branch;
   return reduce_trailing_nongreedy_then_empty(root, branch);
-}
-
-void prune_subtree(Node *n, std::stack<Node*>& branch) {
-  if (n->Type == Node::REGEXP) {
-    n->Left = 0;
-    branch.push(n);
-    return;
-  }
-
-  Node* p = branch.top();
-
-  switch (p->Type) {
-  case Node::REGEXP:
-    // the whole pattern has been pruned!
-    p->Left = 0;
-    break;
-
-  case Node::ALTERNATION:
-  case Node::CONCATENATION:
-    // replace the parent node with the sibling node
-    {
-      Node* sibling = p->Left == n ? p->Right : p->Left;
-
-      branch.pop();
-      Node* gp = branch.top();
-
-      if (gp->Left == p) {
-        gp->Left = sibling;
-      }
-      else {
-        gp->Right = sibling;
-      }
-    }
-    break;
-
-  case Node::REPETITION:
-  case Node::REPETITION_NG:
-    // prune the parent node
-    branch.pop();
-    prune_subtree(p, branch);
-    break;
-
-  default:
-    // WTF?
-    throw std::logic_error(boost::lexical_cast<std::string>(n->Type));
-  }
 }
