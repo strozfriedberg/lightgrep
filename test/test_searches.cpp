e8a2fb468d4496846b7133a6c67a61c5e5c8794b
Joel Uckelman <uckelman@nomic.net>
2011-07-11 15:33:24 +0200

    Added failing test for a|aa? and ab|a|b on "aa".

--- test/test_searches.cpp
+++ test/test_searches.cpp
@@ -323,6 +323,16 @@ SCOPE_FIXTURE_CTOR(aaaOraaOraaDotSearch, STest, STest("aaa|aa|aa.")) {
   SCOPE_ASSERT_EQUAL(SearchHit(4, 2, 0), fixture.Hits[1]);
 }
 
+SCOPE_FIXTURE_CTOR(aOraaQ_abOraOrbSearch, STest, STest(2, (const char*[]){"a|aa?", "ab|a|b"})) {
+  const byte* text = (const byte*) "aaabaac";
+  fixture.search(text, text + 2, 0, fixture);
+  SCOPE_ASSERT_EQUAL(4, fixture.Hits.size());
+  SCOPE_ASSERT_EQUAL(SearchHit(0, 1, 0), fixture.Hits[0]);
+  SCOPE_ASSERT_EQUAL(SearchHit(0, 1, 1), fixture.Hits[1]);
+  SCOPE_ASSERT_EQUAL(SearchHit(1, 1, 0), fixture.Hits[2]);
+  SCOPE_ASSERT_EQUAL(SearchHit(1, 1, 1), fixture.Hits[3]);
+}
+
 SCOPE_FIXTURE_CTOR(startsWithTest, STest, STest(3, (const char*[]){"ab..ef", "c[a-z][a-z]", "[aA][bc][bc]"})) {
   const byte* text = (const byte*)"abcdefghijklmnop";
   fixture.startsWith(text, text + 16, 0, fixture);
