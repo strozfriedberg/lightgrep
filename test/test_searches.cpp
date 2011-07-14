b88ea8d7b472ae8d2bf86cf3787f2fdf54dd2512
Joel Uckelman <uckelman@nomic.net>
2011-07-14 17:20:51 +0200

    Added failing test for a+|.|aa. and aa.+..a* on text aaabaac.

--- test/test_searches.cpp
+++ test/test_searches.cpp
@@ -343,6 +343,17 @@ SCOPE_FIXTURE_CTOR(aOraa_aOrbSearch, STest, STest(2, (const char*[]){"a|aa", "a|
   SCOPE_ASSERT_EQUAL(SearchHit(1, 1, 1), fixture.Hits[3]);
 }
 
+SCOPE_FIXTURE_CTOR(aPOrDotOraaDot_aaDotPDotDotaQSearch, STest, STest(2, (const char *[]){ "a+|.|aa.", "aa.+..a*" })) {
+  const byte* text = (const byte*) "aaabaac";
+  fixture.search(text, text + 7, 0, fixture);
+  SCOPE_ASSERT_EQUAL(5, fixture.Hits.size());
+  SCOPE_ASSERT_EQUAL(SearchHit(0, 3, 0), fixture.Hits[0]);
+  SCOPE_ASSERT_EQUAL(SearchHit(0, 7, 1), fixture.Hits[1]);
+  SCOPE_ASSERT_EQUAL(SearchHit(3, 1, 0), fixture.Hits[2]);
+  SCOPE_ASSERT_EQUAL(SearchHit(4, 2, 0), fixture.Hits[3]);
+  SCOPE_ASSERT_EQUAL(SearchHit(6, 1, 0), fixture.Hits[4]);
+}
+
 SCOPE_FIXTURE_CTOR(startsWithTest, STest, STest(3, (const char*[]){"ab..ef", "c[a-z][a-z]", "[aA][bc][bc]"})) {
   const byte* text = (const byte*)"abcdefghijklmnop";
   fixture.startsWith(text, text + 16, 0, fixture);
