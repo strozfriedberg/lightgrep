a99acb65ad4288ee184af57f9da69d65952e8058
Joel Uckelman <uckelman@nomic.net>
2011-07-22 01:05:04 +0200

    NONE != NOLABEL. Labels are uint32, should be compared with NOLABEL.

--- test/test_searches.cpp
+++ test/test_searches.cpp
@@ -373,6 +373,14 @@ SCOPE_FIXTURE_CTOR(aDotaQQOraOrDotPQ_aQaOrDotPaQQSearch, STest, STest(2, (const 
   SCOPE_ASSERT_EQUAL(SearchHit(3, 1, 1), fixture.Hits[4]);
 }
 
+SCOPE_FIXTURE_CTOR(aaa_aaaOraSearch, STest, STest(2, (const char *[]){ "aaa", "aaa|a" })) {
+  const byte* text = (const byte*) "aaa";
+  fixture.search(text, text + 3, 0, fixture);
+  SCOPE_ASSERT_EQUAL(2, fixture.Hits.size());
+  SCOPE_ASSERT_EQUAL(SearchHit(0, 3, 0), fixture.Hits[0]);
+  SCOPE_ASSERT_EQUAL(SearchHit(0, 3, 1), fixture.Hits[1]);
+}
+
 SCOPE_FIXTURE_CTOR(startsWithTest, STest, STest(3, (const char*[]){"ab..ef", "c[a-z][a-z]", "[aA][bc][bc]"})) {
   const byte* text = (const byte*)"abcdefghijklmnop";
   fixture.startsWith(text, text + 16, 0, fixture);
