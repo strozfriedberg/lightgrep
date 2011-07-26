54f82626ec8d5b2b679d093114f36c5da98934d6
Joel Uckelman <uckelman@nomic.net>
2011-07-26 18:38:09 +0200

    Made NOLABEL and NONE static const members of Thread.

--- test/test_thread.cpp
+++ test/test_thread.cpp
@@ -6,9 +6,9 @@ SCOPE_TEST(defaultThreadConstructor) {
   Thread t;
   Instruction *nullPtr = 0;
   SCOPE_ASSERT_EQUAL(nullPtr, t.PC);
-  SCOPE_ASSERT_EQUAL(std::numeric_limits<uint32>::max(), t.Label);
+  SCOPE_ASSERT_EQUAL(Thread::NOLABEL, t.Label);
   SCOPE_ASSERT_EQUAL(0, t.Start);
-  SCOPE_ASSERT_EQUAL(std::numeric_limits<uint64>::max(), t.End);
+  SCOPE_ASSERT_EQUAL(Thread::NONE, t.End);
 }
 
 /*
@@ -29,9 +29,9 @@ SCOPE_TEST(threadJump) {
   Instruction* basePtr = reinterpret_cast<Instruction*>(8);
   t.jump(basePtr, 5);
   SCOPE_ASSERT_EQUAL(reinterpret_cast<Instruction*>(28), t.PC);
-  SCOPE_ASSERT_EQUAL(std::numeric_limits<uint32>::max(), t.Label);
+  SCOPE_ASSERT_EQUAL(Thread::NOLABEL, t.Label);
   SCOPE_ASSERT_EQUAL(0, t.Start);
-  SCOPE_ASSERT_EQUAL(std::numeric_limits<uint64>::max(), t.End);
+  SCOPE_ASSERT_EQUAL(Thread::NONE, t.End);
 }
 
 SCOPE_TEST(threadFork) {
@@ -41,5 +41,5 @@ SCOPE_TEST(threadFork) {
   SCOPE_ASSERT_EQUAL(reinterpret_cast<Instruction*>(16), child.PC);
   SCOPE_ASSERT_EQUAL(5, child.Label);
   SCOPE_ASSERT_EQUAL(123, child.Start);
-  SCOPE_ASSERT_EQUAL(std::numeric_limits<uint64>::max(), child.End);
+  SCOPE_ASSERT_EQUAL(Thread::NONE, child.End);
 }
