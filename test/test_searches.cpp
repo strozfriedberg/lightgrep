f45897820867568b39a19aacae4204d5175b7123
Joel Uckelman <uckelman@nomic.net>
2011-08-11 23:32:57 +0200

    Added test.

--- test/test_searches.cpp
+++ test/test_searches.cpp
@@ -344,6 +344,20 @@ SCOPE_FIXTURE_CTOR(aSQdotPQdotQaSearch, STest, STest("a*?.+?.?a")) {
   SCOPE_ASSERT_EQUAL(SearchHit(0, 3, 0), fixture.Hits[0]);
 }
 
+SCOPE_FIXTURE_CTOR(aaaaOrDotOrDotaDot_aOrDotaaOrDotOraDotSearch, STest, STest(2, (const char *[]){ "aaaa|.|.a.", "a|.(a)a|.|a." })) {
+  const byte* text = (const byte*) "aaab";
+  fixture.search(text, text + 4, 0, fixture);
+  SCOPE_ASSERT_EQUAL(8, fixture.Hits.size());
+  SCOPE_ASSERT_EQUAL(SearchHit(0, 1, 1), fixture.Hits[0]);
+  SCOPE_ASSERT_EQUAL(SearchHit(1, 1, 1), fixture.Hits[1]);
+  SCOPE_ASSERT_EQUAL(SearchHit(2, 1, 1), fixture.Hits[2]);
+  SCOPE_ASSERT_EQUAL(SearchHit(0, 1, 0), fixture.Hits[3]);
+  SCOPE_ASSERT_EQUAL(SearchHit(1, 1, 0), fixture.Hits[4]);
+  SCOPE_ASSERT_EQUAL(SearchHit(2, 1, 0), fixture.Hits[5]);
+  SCOPE_ASSERT_EQUAL(SearchHit(3, 1, 0), fixture.Hits[6]);
+  SCOPE_ASSERT_EQUAL(SearchHit(3, 1, 1), fixture.Hits[7]);
+}
+
 SCOPE_FIXTURE_CTOR(aOraaQ_aOrbSearch, STest, STest(2, (const char*[]){"a|aa?", "a|b"})) {
   const byte* text = (const byte*) "aa";
   fixture.search(text, text + 2, 0, fixture);
