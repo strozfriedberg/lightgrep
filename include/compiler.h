681162569c9c52e2ae6d0dd68380cf5d201d5898
Joel Uckelman <uckelman@nomic.net>
2011-08-11 19:43:39 +0200

    Extracted merge condition to Compiler::canMerge().

--- include/compiler.h
+++ include/compiler.h
@@ -21,6 +21,8 @@ public:
   
   StatePair processChild(const Graph& src, Graph& dst, uint32 si, Graph::vertex srcHead, Graph::vertex dstHead);
 
+  bool canMerge(const Graph& dst, const Graph& src, Graph::vertex srcTail, Graph::vertex dstTail, const ByteSet& srcBits, const ByteSet& dstBits, uint32 srcLabel, uint32 dstLabel) const;
+
 private:
   std::map<Graph::vertex, std::vector<Graph::vertex> > Dst2Src;
   std::vector<Graph::vertex> Src2Dst;
