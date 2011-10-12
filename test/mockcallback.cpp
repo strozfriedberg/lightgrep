a1b41e4e0b5bcbe9cc7829a458eb4ab80f3684a3
Joel Uckelman <uckelman@nomic.net>
2011-10-12 17:10:34 +0200

    Refactoring for C API.

--- test/mockcallback.cpp
+++ test/mockcallback.cpp
@@ -1,6 +1,9 @@
 
+#include "searchhit.h"
 #include "mockcallback.h"
 
-void mockcallback(void* userData, const LG_SearchHit* const hit) {
-  reinterpret_cast< std::vector<SearchHit>* >(userData)->push_back(*reinterpret_cast<const SearchHit* const>(hit));
+#include <vector>
+
+void mockCallback(void* userData, const LG_SearchHit* const hit) {
+  reinterpret_cast<std::vector<SearchHit>*>(userData)->push_back(*reinterpret_cast<const SearchHit* const>(hit));
 }
