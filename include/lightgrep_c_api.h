b86b662408c4878630adc125df9cbd04915189f2
Joel Uckelman <uckelman@nomic.net>
2011-10-13 15:49:24 +0200

    Added FIXME.

--- include/lightgrep_c_api.h
+++ include/lightgrep_c_api.h
@@ -95,6 +95,7 @@ extern "C" {
                       uint64 startOffset,
                       void* userData,
                       LG_HITCALLBACK_FN callbackFn);
+// FIXME: swap order of callback and data?
 
   // Reset the context to its initial state.
   // Call this before searching a new file.
