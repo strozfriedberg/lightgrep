78557600e475b4048ad2250b95cbc7ed6e72364e
Jon Stewart <jon@lightboxtechnologies.com>
2012-01-04 16:24:42 -0500

    Enable the twain test, and don't record the file segments.

--- pytest/trace_runner.py
+++ pytest/trace_runner.py
@@ -4,17 +4,17 @@ from analyze_trace import TraceStats
 
 def runProc(name, keys, corpus):
   print("%s %s %s" % (name, keys, corpus))
-  args = ['./lightgrep.exe', '--no-output', '--begin-debug=0']
-  args.append(keys)
-  args.append(corpus)
+  args = ['./lightgrep.exe', '--no-output', '--begin-debug=0', keys, corpus]
   lg = subprocess.Popen(args=args, bufsize=-1, stderr=subprocess.PIPE)
   stats = TraceStats()
+  stats.RecordSegments = False
   stats.read(lg.stderr)
   lg.wait()
   stats.output()
 
 if __name__ == '__main__':
-  tests = [('norvig-small', 'pytest/keys/114743.txt', 'pytest/corpora/norvig1mb.txt')]
+  tests = [('norvig', 'pytest/keys/114743.txt', 'pytest/corpora/norvig6mb.txt'),
+            ('twain', 'pytest/keys/twain.txt', 'pytest/corpora/marktwainworks.txt')]
   procs = [Process(target=runProc, args=t) for t in tests]
   for p in procs:
     p.start()
