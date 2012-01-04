0acc3a8808ec343224becd9f40d8e16afd7205b6
Jon Stewart <jonathan.l.stewart@acm.org>
2012-01-04 00:46:40 -0500

    Read through the threads and collect more statistics.

--- pytest/analyze_trace.py
+++ pytest/analyze_trace.py
@@ -12,6 +12,7 @@ class TraceStats:
     self.ThreadsPerFrameHist = collections.Counter()
     self.InstructionHist = collections.Counter()
     self.InstructionsPerFrameHist = collections.Counter()
+    self.ThreadLifetimes = collections.Counter()
     self.NumThreadsSegments = []
     self.NumBytes = 0
 
@@ -35,6 +36,15 @@ class TraceStats:
             self.NumThreadsSegments.append((lastNumThreads, lastStart, offset))
             lastStart = offset
             lastNumThreads = threadLevel
+          numInstrs = 0
+          for t in rec["list"]:
+            state = t["state"]
+            if state & 2:
+              self.InstructionHist[t["PC"]] += 1
+              numInstrs += 1
+            elif state & 8 > 0:
+              self.ThreadLifetimes[offset - t["Start"]] += 1
+          self.InstructionsPerFrameHist[numInstrs] += 1
       except:
         pass
     self.NumThreadsSegments.append((lastNumThreads, lastStart, bytes))
@@ -42,10 +52,26 @@ class TraceStats:
   
   def output(self):
     print("Num bytes: " + str(self.NumBytes))
+
     print("Max threads on a frame: " + str(self.MaxThreadsOnFrame))
     print("Threads on a frame histogram:")
     for el in self.ThreadsPerFrameHist.most_common():
       print("  %s, %s, %s" % (el[0], el[1], float(el[1] * 100) / self.NumBytes))
+
+    print("Instructions on a frame histogram:")
+    for el in self.InstructionsPerFrameHist.most_common():
+      print("  %s, %s, %s" % (el[0], el[1], float(el[1] * 100) / self.NumBytes))
+
+    print("Thread lifetimes:")
+    numThreads = sum(self.ThreadLifetimes.values())
+    for el in self.ThreadLifetimes.most_common():
+      print("  %s, %s, %s" % (el[0], el[1], float(el[1] * 100) / numThreads))
+
+    totalInstrs = sum(self.InstructionHist.values())
+    print("Instructions executed histogram:")
+    for el in self.InstructionHist.most_common():
+      print("  %s, %s, %s" % (el[0], el[1], float(el[1] * 100) / totalInstrs))
+    
     print("File segments (%s)" % (str(len(self.NumThreadsSegments))))
     for seg in self.NumThreadsSegments:
       print("  %s, %s, %s, %s" % (seg[0], seg[2] - seg[1], seg[1], seg[2]))
