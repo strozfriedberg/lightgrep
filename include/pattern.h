e30746fc9a687d209b3ce582383607961134a0d9
Jon Stewart <jonathan.l.stewart@acm.org>
2012-01-23 20:59:29 -0500

    Consistently use the option as 'CaseInsensitive' throughout. Parse a pattern line with tab-delimited fields (and comma separated encodings).

--- include/pattern.h
+++ include/pattern.h
@@ -7,7 +7,15 @@ public:
   std::string Expression;
   bool        FixedString,
               CaseInsensitive;
+  std::string Encoding;
 
-  Pattern(const std::string& expr = "", bool fixed = false, bool nocase = false):
-    Expression(expr), FixedString(fixed), CaseInsensitive(nocase) {}
+  Pattern(const std::string& expr = "", bool fixed = false, bool insensitive = false, const std::string& enc = ""):
+    Expression(expr), FixedString(fixed), CaseInsensitive(insensitive), Encoding(enc) {}
+
+  bool operator==(const Pattern& p) const {
+    return FixedString == p.FixedString && CaseInsensitive == p.CaseInsensitive &&
+      Expression == p.Expression && Encoding == p.Encoding;
+  }
 };
+
+std::ostream& operator<<(std::ostream&, const Pattern&);
