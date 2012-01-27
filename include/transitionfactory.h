1e562ceeb0c286544cb04ce3b244a3c36d5ffd99
Joel Uckelman <uckelman@nomic.net>
2012-01-27 05:33:26 +0100

    Added a function for requesting each Transition type, which saves us from constructing one each time.

--- include/transitionfactory.h
+++ include/transitionfactory.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <limits>
 #include <set>
 
 #include "basic.h"
@@ -9,6 +8,8 @@
 
 class TransitionFactory {
 public:
+  TransitionFactory(): Lit(0), Either(0, 0), Range(0, 0), CharClass(ByteSet()) {}
+
   ~TransitionFactory() {
     const std::set<Transition*>::iterator end(Exemplars.end());
     for (std::set<Transition*>::iterator i(Exemplars.begin()); i != end; ++i) {
@@ -16,6 +17,29 @@ public:
     }
   }
 
+  Transition* getLit(byte lit) {
+    Lit.Lit = lit;
+    return get(&Lit);
+  }
+
+  Transition* getEither(byte lit1, byte lit2) {
+    Either.Lit1 = lit1;
+    Either.Lit2 = lit2;
+    return get(&Either);
+  }
+
+  Transition* getRange(byte first, byte last) {
+    Range.First = first;
+    Range.Last = last;
+    return get(&Range);
+  }
+
+  Transition* getCharClass(const ByteSet& bytes) {
+    CharClass.Allowed = bytes;
+    return get(&CharClass);
+  }
+
+private:
   Transition* get(Transition* t) {
     const std::set<Transition*>::const_iterator i = Exemplars.find(t);
     if (i == Exemplars.end()) {
@@ -26,6 +50,11 @@ public:
     }
   }
 
-private:
   std::set<Transition*,TransitionComparator> Exemplars;
+
+  // Local states so we don't have to create one on each lookup
+  LitState Lit;
+  EitherState Either;
+  RangeState Range;
+  CharClassState CharClass;
 };
