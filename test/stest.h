73b83fa69a7779a85c9d1ce55f35de735a04fad8
Joel Uckelman <uckelman@nomic.net>
2012-02-02 01:22:53 +0100

    Removed STest ctor with count, since we can use std::initializer_list now.

--- test/stest.h
+++ test/stest.h
@@ -25,14 +25,6 @@ struct STest {
 
   STest(const std::vector<std::string>& keys);
 
-  STest(uint32 num, const char** keys) {
-    std::vector<Pattern> kws(num);
-    for (unsigned int i = 0; i < num; ++i) {
-      kws[i] = Pattern(keys[i]);
-    }
-    init(kws);
-  }
-
   void init(const std::vector<Pattern>& kws);
 
   void search(const byte* begin, const byte* end, uint64 offset) {
