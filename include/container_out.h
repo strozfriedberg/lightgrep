799ddba6f864a8c404a2825ca9debd5a93f1182c
Joel Uckelman <uckelman@nomic.net>
2012-02-07 02:01:07 +0100

    Added a version of operator<< for containers taking parameters as std::set does.

--- include/container_out.h
+++ include/container_out.h
@@ -6,7 +6,15 @@
 #include <ostream>
 
 template <typename T,
-          template<typename E, typename A=std::allocator<E>> class C>
+          template <typename E, typename A = std::allocator<E>> class C>
+std::ostream& operator<<(std::ostream& out, const C<T>& con) {
+  out << '[';
+  std::copy(con.begin(), con.end(), ostream_join_iterator<T>(out, ","));
+  return out << ']';
+}
+
+template <typename T,
+          template <typename K, typename C = std::less<K>, typename A = std::allocator<K>> class C>
 std::ostream& operator<<(std::ostream& out, const C<T>& con) {
   out << '[';
   std::copy(con.begin(), con.end(), ostream_join_iterator<T>(out, ","));
