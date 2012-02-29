83050fcf74cb491439669401adb7503b2e5caa1e
Joel Uckelman <uckelman@nomic.net>
2012-02-29 05:49:47 +0100

    Added transform_utf8_to_unicode. Parser now expects UTF-8 strings, which are converted to Unicode codepoints before lexing. Completes Feature #276.

--- include/unicode.h
+++ include/unicode.h
@@ -5,15 +5,15 @@
 
 #include <boost/iterator/iterator_facade.hpp>
 
-#define CONTINUATION(cp, i, end) \
+#define CONTINUATION(cp, i, end, used) \
   if (++i == end) { \
     cp = -1; \
-    return -1; \
+    return used; \
   } \
   b = *i & 0xFF; \
   if (b < 0x80 || 0xBF < b) { \
     cp = -1; \
-    return -1; \
+    return used; \
   } \
   cp = (cp << 6) | (b & 0x3F)
 
@@ -22,7 +22,7 @@ int utf8_to_unicode(int& cp, Iterator i, const Iterator& end) {
 
   if (i == end) {
     cp = -1;
-    return -1;
+    return 0;
   }
 
   cp = *i & 0xFF;
@@ -38,53 +38,62 @@ int utf8_to_unicode(int& cp, Iterator i, const Iterator& end) {
     // overlong
     if (cp < 0xC2) {
       cp = -1;
-      return -1;
+      return 1;
     }
     
     cp &= 0x1F;
-    CONTINUATION(cp, i, end);
+    CONTINUATION(cp, i, end, 2);
 
     return 2;
   }
   // 3-byte sequence
   else if (cp < 0xF0) {
     cp &= 0x0F;
-    CONTINUATION(cp, i, end);
-    CONTINUATION(cp, i, end);
+    CONTINUATION(cp, i, end, 2);
+    CONTINUATION(cp, i, end, 3);
 
     // check for overlong and UTF-16 surrogates
-    if ((0x7FF < cp && cp < 0xD800) || cp > 0xDFFF) {
-      return 3;
-    }
-    else {
+    if ((cp < 0x800 || 0xD7FF < cp) && cp < 0xE000) {
       cp = -1;
-      return -1;
     }
+
+    return 3;
   }
   // 4-byte sequence
   else if (cp < 0xF5) {
     cp &= 0x07;
-    CONTINUATION(cp, i, end);
-    CONTINUATION(cp, i, end);
-    CONTINUATION(cp, i, end);
+    CONTINUATION(cp, i, end, 2);
+    CONTINUATION(cp, i, end, 3);
+    CONTINUATION(cp, i, end, 4);
 
     // check for overlong and too high
     if (cp < 0x10000 || cp > 0x10FFFF) {
       cp = -1;
-      return -1;
     }
-    else {
-      return 4;
-    }
+
+    return 4;
   }
   else {
     // bad start byte
     cp = -1;
-    return -1;
+    return 1;
   }
 } 
 
 #undef CONTINUATION
+
+template <class InputIterator, class OutputIterator>
+OutputIterator transform_utf8_to_unicode(InputIterator ibegin,
+                                         InputIterator iend,
+                                         OutputIterator obegin)
+{
+  int cp;
+  while (ibegin != iend) {
+    ibegin += utf8_to_unicode(cp, ibegin, iend);
+    *obegin++ = cp;
+  }
+  return obegin;
+}
 
 template <typename C>
 struct IsLeadByte {
