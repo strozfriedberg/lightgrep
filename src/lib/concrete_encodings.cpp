843c90120b30d0e0eb546676c240828fb21c116b
Joel Uckelman <uckelman@nomic.net>
2012-03-06 02:22:23 +0100

    Added UTF-8 encoder.

--- src/lib/concrete_encodings.cpp
+++ src/lib/concrete_encodings.cpp
@@ -13,7 +13,46 @@ uint32 ASCII::write(int cp, byte buf[]) const {
   return 0;
 }
 
+uint32 UTF8::maxByteLength() const {
+  return 4;
+}
 
+uint32 UTF8::write(int cp, byte buf[]) const {
+  if (cp < 0) {
+    // too small
+    return 0;
+  }
+  else if (cp < 0x80) {
+    // one byte
+    buf[0] = (byte) cp;
+    return 1;
+  }
+  else if (cp < 0x800) {
+    // two bytes
+    buf[0] = 0xC0000000 | ((cp >> 6) & 0x1F);
+    buf[1] = 0x80000000 | ( cp       & 0x3F);
+    return 2;
+  }
+  else if (cp < 0x10000) {
+    // three bytes
+    buf[0] = 0xE0000000 | ((cp >> 12) & 0x0F);
+    buf[1] = 0x80000000 | ((cp >>  6) & 0x3F);
+    buf[2] = 0x80000000 | ( cp        & 0x3F);
+    return 3;
+  }
+  else if (cp < 0x110000) {
+    // four bytes
+    buf[0] = 0xF0000000 | ((cp >> 18) & 0x07);
+    buf[1] = 0x80000000 | ((cp >> 12) & 0x3F);
+    buf[2] = 0x80000000 | ((cp >>  6) & 0x3F);
+    buf[3] = 0x80000000 | ( cp        & 0x3F);
+    return 4;
+  }
+  else {
+    // too large
+    return 0;
+  }
+}
 
 uint32 UCS16::maxByteLength() const {
   return 2;
