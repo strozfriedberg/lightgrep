2e6667eb7351df1ffc0145dd110defae4122d93a
Joel Uckelman <uckelman@nomic.net>
2012-03-23 20:00:37 +0100

    Removed Fragment to its own header.

---
+++ include/fragment.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "automata.h"
+#include "basic.h"
+#include "parsenode.h"
+
+#include <limits>
+#include <vector>
+
+typedef std::vector<NFA::VertexDescriptor> InListT;
+typedef std::vector<std::pair<NFA::VertexDescriptor, uint32>> OutListT;
+
+static const uint32 NOSKIP = std::numeric_limits<uint32>::max();
+
+struct Fragment {
+  Fragment(): Skippable(NOSKIP) {}
+
+  Fragment(NFA::VertexDescriptor in, const ParseNode& n):
+    InList(1, in), N(n), Skippable(NOSKIP) {}
+
+  /*
+   * InList is the list of vertices in this fragment which have incoming
+   * edges from outside the fragment. OutList is the is the list of vertices
+   * in this fragment which have edges leaving the fragment.
+   */
+  InListT InList;
+  OutListT OutList;
+  ParseNode N;
+
+  uint32 Skippable;
+
+  void initFull(NFA::VertexDescriptor in, const ParseNode& n) {
+    N = n;
+    Skippable = NOSKIP;
+    InList.clear();
+    InList.push_back(in);
+    OutList.clear();
+    OutList.emplace_back(in, 0);
+  }
+
+  void reset(const ParseNode& n) {
+    N = n;
+    Skippable = NOSKIP;
+    InList.clear();
+    OutList.clear();
+  }
+
+  void assign(Fragment& f) {
+    InList.swap(f.InList);
+    OutList.swap(f.OutList);
+    N = f.N;
+    Skippable = f.Skippable;
+  }
+};
--- include/nfabuilder.h
+++ include/nfabuilder.h
@@ -3,60 +3,15 @@
 #include "automata.h"
 #include "basic.h"
 #include "encoding.h"
+#include "fragment.h"
 #include "parsenode.h"
 #include "parsetree.h"
 #include "transitionfactory.h"
 
-#include <limits>
 #include <stack>
 
 #include <boost/scoped_array.hpp>
 #include <memory>
-
-typedef std::vector<NFA::VertexDescriptor> InListT;
-typedef std::vector<std::pair<NFA::VertexDescriptor, uint32>> OutListT;
-
-static const uint32 NOSKIP = std::numeric_limits<uint32>::max();
-
-struct Fragment {
-  Fragment(): Skippable(NOSKIP) {}
-  Fragment(NFA::VertexDescriptor in, const ParseNode& n):
-    InList(1, in), N(n), Skippable(NOSKIP) {}
-
-  /*
-   * InList is the list of vertices in this fragment which have incoming
-   * edges from outside the fragment. OutList is the is the list of vertices
-   * in this fragment which have edges leaving the fragment.
-   */
-  InListT InList;
-  OutListT OutList;
-  ParseNode N;
-
-  uint32 Skippable;
-
-  void initFull(NFA::VertexDescriptor in, const ParseNode& n) {
-    N = n;
-    Skippable = NOSKIP;
-    InList.clear();
-    InList.push_back(in);
-    OutList.clear();
-    OutList.emplace_back(OutListT::value_type(in, 0));
-  }
-
-  void reset(const ParseNode& n) {
-    N = n;
-    Skippable = NOSKIP;
-    InList.clear();
-    OutList.clear();
-  }
-
-  void assign(Fragment& f) {
-    InList.swap(f.InList);
-    OutList.swap(f.OutList);
-    N = f.N;
-    Skippable = f.Skippable;
-  }
-};
 
 class NFABuilder {
 public:
