2f5ac1763ef29f07d91b88310949851bf719646b
Jon Stewart <jon@lightboxtechnologies.com>
2012-04-01 09:36:07 -0400

    Send random data streams for searching to lightgrep server.

--- pytest/send_data_to_lg.py
+++ pytest/send_data_to_lg.py
@@ -3,14 +3,66 @@
 import struct
 import socket
 import random
+from multiprocessing import Process
 
-sock = socket.create_connection(('127.0.0.1', 12777))
-for i in range(0, 1000):
-  size = random.randint(1, 2048)
-  hdr = struct.pack('QQ', i, size)
-  sock.send(hdr)
-  print("Sending id %s with %s bytes" % (i, size))
-  for j in range(0, size):
-    sock.send(struct.pack('B', random.randint(0, 255)))
+class LGClient:
+  # struct FileHeader {
+  #   byte   Cmd,
+  #          Type;
+  #   uint64 ID,
+  #          StartOffset,
+  #          Length;
+  # };
 
-sock.close()
+  def __init__(self, prefix):
+    self.prefix = prefix
+    self.totalStreams = random.randint(1, 10000)
+    self.hdr = struct.Struct('<BBQQQ')
+    self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+  def connect(self):
+    self.sock.connect(('127.0.0.1', 12777))
+    print("connected to lg as " + str(self.sock.getsockname()))
+
+  def sendSearchHeader(self, id, len):
+    self.sock.sendall(self.hdr.pack(0, 0, id, 0, len))
+
+  def hangup(self):
+    self.sock.sendall(self.hdr.pack(2, 0, 0, 0, 0))
+    self.sock.recv(1)
+    self.sock.close()
+
+  def shutdown(self):
+    self.sock.sendall(self.hdr.pack(3, 0, 0, 0, 0))
+    self.sock.close()
+
+  def sendStream(self, i):
+    id = self.prefix + i
+    len = random.randint(1, 2 << 30)
+    self.sendSearchHeader(id, len)
+    for i in xrange(len):
+      self.sock.sendall(chr(random.getrandbits(8)))
+
+  def run(self):
+    self.connect()
+    for i in xrange(self.totalStreams):
+      self.sendStream(i)
+    self.hangup()
+
+def start(prefix):
+  print("running client with prefix " + str(prefix))
+  client = LGClient(prefix)
+  client.run()
+
+if __name__ == '__main__':
+  print("starting")
+  procs = [Process(target=start, args=[(i*10000)]) for i in xrange(5)]
+  for p in procs:
+    print("spawning proc")
+    p.start()
+  for p in procs:
+    p.join()
+  killer = LGClient(0)
+  killer.connect()
+  killer.shutdown()
+  print("done")
