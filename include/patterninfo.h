17824ae50e7837f31d8f421fc2963ee1abbfa68d
Jon Stewart <jonathan.l.stewart@acm.org>
2012-04-06 21:04:39 +0200

    Added fieldl NumUserPatterns to PatternInfo and changed Options::getKeys() to return an instance of PatternInfo.

--- include/patterninfo.h
+++ include/patterninfo.h
@@ -6,9 +6,15 @@
 
 #include "pattern.h"
 
-struct PatternInfo {
+class PatternInfo {
+public:
+  PatternInfo(): NumUserPatterns(0) {}
+
   virtual ~PatternInfo() {}
 
+  bool empty() const { return 0 == NumUserPatterns; }
+
+  uint32 NumUserPatterns;
   std::vector<Pattern> Patterns;
   std::vector<std::pair<uint32, uint32>> Table;
 };
