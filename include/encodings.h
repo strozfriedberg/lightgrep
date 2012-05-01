4b877207854f184e9b654a517ee39f91f9a9bb31
Joel Uckelman <uckelman@nomic.net>
2012-05-01 05:49:51 +0200

    Cleaned up LG_SUPPORTED_ENCODINGS. Now we list ASCII, UTF-8, UTF-16LE, UTF-16BE, UTF-32LE, UTF-32BE.

--- include/encodings.h
+++ include/encodings.h
@@ -7,20 +7,22 @@
 extern "C" {
 #endif
 
-static const int LG_ENC_ASCII = 0;
-static const int LG_ENC_LATIN_1 = 1;
-static const int LG_ENC_UTF_8 = 2;
-static const int LG_ENC_UTF_16 = 3;
+// FIXME: why not an enum?
+static const int LG_ENC_ASCII    = 0;
+static const int LG_ENC_UTF_8    = 1;
+static const int LG_ENC_UTF_16LE = 2;
+static const int LG_ENC_UTF_16BE = 3;
+static const int LG_ENC_UTF_32LE = 4;
+static const int LG_ENC_UTF_32BE = 5;
 
 static const char* LG_SUPPORTED_ENCODINGS[] = {
-
-// The Encoding specified in LG_KeyOptions MUST match one of the below strings (in ASCII)
-
-  "US-ASCII",           // 0    aka ANSI_X3.4-1968, ASCII (as a special case, you can use any of the three)
-  "ISO_8859-1:1987",    // 1    8859-1, aka Latin-1
-  "UTF-8",              // 2
-  "UTF-16"              // 3
-
+// The encoding specified in LG_KeyOptions MUST be one of these ASCII strings:
+  "ASCII",    // 0
+  "UTF-8",    // 1
+  "UTF-16LE", // 2
+  "UTF-16BE", // 3
+  "UTF-32LE", // 4
+  "UTF-32BE"  // 5
 };
 
 #ifdef __cplusplus
