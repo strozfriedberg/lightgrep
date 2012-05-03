2eab59a091a995b1c8855c18aa2a58a588953412
Joel Uckelman <uckelman@nomic.net>
2012-05-03 02:31:26 +0200

    Delegated character class subgraph construction to Encoder::write. The implementation in the base class produces correct, but overlarge subgraphs.

--- include/encoder.h
+++ include/encoder.h
@@ -1,11 +1,15 @@
 #pragma once
 
+#include "automata.h"
 #include "basic.h"
+#include "fragment.h"
 
 class Encoder {
 public:
   virtual uint32 maxByteLength() const = 0;
   virtual uint32 write(int cp, byte buf[]) const = 0;
+//  virtual void write(const UnicodeSet& uset, NFA& g, Fragment& f) const = 0;
+  virtual void write(const UnicodeSet&, NFA&, Fragment&) const;
 
   virtual ~Encoder() {}
 };
