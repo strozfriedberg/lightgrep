9d336d99397264d78accef366bf641806f88f42b
Joel Uckelman <uckelman@nomic.net>
2012-05-05 04:51:04 +0200

    Name the arguments for write().

--- include/encoder.h
+++ include/encoder.h
@@ -9,7 +9,7 @@ public:
   virtual uint32 maxByteLength() const = 0;
   virtual uint32 write(int cp, byte buf[]) const = 0;
 //  virtual void write(const UnicodeSet& uset, NFA& g, Fragment& f) const = 0;
-  virtual void write(const UnicodeSet&, NFA&, Fragment&) const;
+  virtual void write(const UnicodeSet& uset, NFA& g, Fragment& frag) const;
 
   virtual ~Encoder() {}
 };
