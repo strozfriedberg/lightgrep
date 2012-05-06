dc36c3a5eb8f974adcb058cc87348fccd9b45cd2
Joel Uckelman <uckelman@nomic.net>
2012-05-07 01:59:37 +0200

    This brings the subgraph for \p{Any} from an unspeakable number of vertices to only 22. This is not terrible, but still not quite right, as, e.g., [E1,E7] [80,BF] [80,BF] could still be merged with [EE,EF] [80,BF] [80,BF], but isn't, and matching tails are also not yet collapsed.

--- src/lib/encoder.cpp
+++ src/lib/encoder.cpp
@@ -1,7 +1,12 @@
 #include "encoder.h"
 
 #include <algorithm>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
 #include <memory>
+#include <utility>
+#include <vector>
 
 uint32 findInitialSegment(NFA::VertexDescriptor& head, const byte* buf, const size_t blen, const NFA& g, const Fragment& frag) {
   ByteSet bs;
@@ -95,10 +100,171 @@ uint32 findTerminalSegment(NFA::VertexDescriptor& tail, const byte* buf, const s
   return 0;
 }
 
+std::ostream& operator<<(std::ostream& out, const ByteSet& bs) {
+  out << '[';
+  uint32 low = 256;
+  bool first = true;
+  for (uint32 i = 0; i < 256; ++i) {
+    if (bs.test(i)) {
+      if (low > 255) {
+        if (!first) {
+          out << ',';
+        }
+        out << std::setfill('0') << std::setw(2) << i;
+        low = i;
+        first = false;
+      }
+    }
+    else if (low < 256) {
+      if ((i-1) > low) {
+        out << '-' << std::setfill('0') << std::setw(2) << (i-1);
+      }
+      low = 256;
+    }
+  }
+  out << ']';
+  return out;
+}
+
+std::ostream& operator<<(std::ostream& out, const std::vector<ByteSet>& v) {
+  std::copy(v.begin(), v.end(), std::ostream_iterator<ByteSet>(out, " "));
+  return out;
+}
+
+std::ostream& operator<<(std::ostream& out, const std::vector<std::vector<ByteSet>>& v) {
+  out << std::hex;
+  std::copy(
+    v.begin(), v.end(), std::ostream_iterator<std::vector<ByteSet>>(out, "\n")
+  );
+  out << std::dec;
+  return out;
+}
+
+bool mismatch_except_at(std::vector<ByteSet>::size_type n,
+                        const std::vector<ByteSet>& a,
+                        const std::vector<ByteSet>& b)
+{
+  // a mismatches b (except at position n) if:
+  //  * a and b are different lengths, or
+  //  * the leading segments of a and b (if any) before n differ, or
+  //  * the trailing segments of a and b (if any) after n differ
+  return
+    a.size() != b.size() ||
+    (a.size() > n+1 && !std::equal(a.begin(), a.end()-n-1, b.begin())) ||
+    (n > 0 && !std::equal(a.end()-n, a.end(), b.end()-n));
+}
+
 void Encoder::write(const UnicodeSet& uset, NFA& g, Fragment& frag) const {
   frag.InList.clear();
   frag.OutList.clear();
 
+  std::unique_ptr<byte[]> buf(new byte[maxByteLength()]);
+
+  std::vector<std::vector<ByteSet>> va, vb, vo;
+
+  // collect the encodings
+  for (const UnicodeSet::range& r : uset) {
+    const uint32 l = r.first, h = r.second;
+    for (uint32 cp = l; cp < h; ++cp) {
+      const uint32 len = write(cp, buf.get());
+      if (len == 0) {
+        // cp is invalid, skip it
+        continue;
+      }
+
+      va.emplace_back(len);
+      for (uint32 i = 0; i < len; ++i) {
+        va.back()[i].set(buf[i]);
+      }
+    }
+  }
+
+  // sort the encodings by length, then lexicographically
+  std::sort(va.begin(), va.end(),
+    [](const std::vector<ByteSet>& a, const std::vector<ByteSet>& b) {
+      return a.size() < b.size() || (a.size() == b.size() &&
+        std::lexicographical_compare(a.begin(), a.end(), b.begin(), b.end()));
+    }
+  );
+
+  // collapse ranges
+  const uint32 mlen = maxByteLength();
+  for (uint32 n = 0; n < mlen; ++n) {
+    for (auto i = va.begin(); i != va.end(); ) {
+      // find the limit of encodings matching i everywhere except position n
+      auto j = std::find_if(i, va.end(),
+        [=](const std::vector<ByteSet>& b) {
+          return mismatch_except_at(n, *i, b);
+        }
+      );
+
+      // collapse the range from i to j
+      std::vector<ByteSet>& v = *i;
+      for (++i; i != j; ++i) {
+        v[v.size()-n-1] |= (*i)[i->size()-n-1];
+      }
+
+      (n == v.size() - 1 ? vo : vb).push_back(std::move(v));
+    }
+
+    va.swap(vb);
+    vb.clear();
+  }
+
+//std::cerr << vo << std::endl;
+
+  ByteSet bs;
+
+  // create a graph from the collapsed ranges
+  for (const std::vector<ByteSet>& enc : vo) {
+    NFA::VertexDescriptor head, tail;
+
+    // leading vertex
+    tail = g.addVertex();
+    g[tail].Trans = g.TransFac->getSmallest(enc[0]);
+    frag.InList.push_back(tail);
+
+    const uint32 elen = enc.size();
+    if (elen > 1) {
+      // middle vertices
+      for (uint32 i = 1; i < elen-1; ++i) {
+        head = tail;
+        tail = g.addVertex();
+        g[tail].Trans = g.TransFac->getSmallest(enc[i]);
+        g.addEdge(head, tail);
+      }
+
+      // trailing vertex
+      head = tail;
+
+      // find a match for the last byte
+      const auto oi = std::find_if(
+        frag.OutList.begin(), frag.OutList.end(),
+        [&](const std::pair<NFA::VertexDescriptor,uint32>& p) {
+          bs.reset();
+          g[p.first].Trans->getBits(bs);
+          return bs == enc[elen-1];
+        }
+      );
+
+      if (oi == frag.OutList.end()) {
+        // no match
+        tail = g.addVertex();
+        g[tail].Trans = g.TransFac->getSmallest(enc[elen-1]);
+        frag.OutList.emplace_back(tail, 0);
+      }
+      else {
+        tail = oi->first;
+      }
+
+      g.addEdge(head, tail);
+    }
+    else {
+      frag.OutList.emplace_back(tail, 0);
+    }
+  }
+
+/*
   std::unique_ptr<byte[]> buf(new byte[maxByteLength() + 20]);
   ByteSet bs;
 
@@ -218,4 +384,5 @@ void Encoder::write(const UnicodeSet& uset, NFA& g, Fragment& frag) const {
       }
     }
   }
+*/
 }
