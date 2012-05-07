b45279f9886df48ec732d7eb4ecc905bc0ad3b44
Joel Uckelman <uckelman@nomic.net>
2012-05-07 06:22:16 +0200

    Inching closer to encoding chain suffix merging...

--- src/lib/encoder.cpp
+++ src/lib/encoder.cpp
@@ -10,52 +10,6 @@
 
 #include <boost/iterator/filter_iterator.hpp>
 
-uint32 findInitialSegment(NFA::VertexDescriptor& head, const byte* buf, const size_t blen, const NFA& g, const Fragment& frag) {
-  ByteSet bs;
-
-  // find a match for byte 0
-  const auto ii = std::find_if(
-    frag.InList.begin(),
-    frag.InList.end(),
-    [&](NFA::VertexDescriptor v) {
-      bs.reset();
-      g[v].Trans->getBits(bs);
-      return bs.test(buf[0]) && bs.count() == 1;
-    }
-  );
-
-  if (ii == frag.InList.end()) {
-    // matching intial segment is empty
-    return 0;
-  }
-
-  head = *ii;
-
-  // find a match for subsequent bytes
-  NFA::VertexDescriptor tail = 0;
-  for (size_t b = 1; b < blen; ++b) {
-    const uint32 odeg = g.outDegree(head);
-    for (uint32 e = 0; e < odeg; ++e) {
-      tail = g.outVertex(head, e);
-      bs.reset();
-      g[tail].Trans->getBits(bs);
-      if (bs.test(buf[b]) && bs.count() == 1) {
-        break;
-      }
-      tail = 0;
-    }
-
-    if (tail) {
-      head = tail;
-    }
-    else {
-      return b;
-    }
-  }
-
-  return blen;
-}
-
 uint32 findTerminalSegment(NFA::VertexDescriptor& tail, const byte* buf, const size_t blen, const NFA& g, const Fragment& frag) {
   ByteSet bs;
 
@@ -148,8 +102,8 @@ bool mismatch_except_at(std::vector<ByteSet>::size_type n,
 {
   // a mismatches b (except at position n) if:
   //  * a and b are different lengths, or
-  //  * the leading segments of a and b (if any) before n differ, or
-  //  * the trailing segments of a and b (if any) after n differ
+  //  * the prefixes of a and b before n differ, or
+  //  * the suffixes of a and b after n differ
   return
     a.size() != b.size() ||
     (a.size() > n+1 && !std::equal(a.begin(), a.end()-n-1, b.begin())) ||
@@ -238,170 +192,103 @@ void Encoder::write(const UnicodeSet& uset, NFA& g, Fragment& frag) const {
   for (const std::vector<ByteSet>& enc : vo) {
     NFA::VertexDescriptor head, tail;
 
-    // leading vertex
-    tail = g.addVertex();
-    g[tail].Trans = g.TransFac->getSmallest(enc[0]);
+    //
+    // find a matching suffix
+    //
+
+    int32 b = enc.size()-1;
+
+    // find a match for the last transition
+    const auto oi = std::find_if(
+      frag.OutList.begin(), frag.OutList.end(),
+      [&](const std::pair<NFA::VertexDescriptor,uint32>& p) {
+        bs.reset();
+        g[p.first].Trans->getBits(bs);
+        return bs == enc[b];
+      }
+    );
+
+    if (oi != frag.OutList.end()) {
+      // match, use this tail
+      tail = oi->first;
+    }
+    else {
+      // build a new tail
+      tail = g.addVertex();
+      g[tail].Trans = g.TransFac->getSmallest(enc[b]);
+      frag.OutList.emplace_back(tail, 0);
+    }
+
+    // build previous vertices
+    for (--b; b >= 0; --b) {
+      head = g.addVertex();
+      g[head].Trans = g.TransFac->getSmallest(enc[b]);
+      g.addEdge(head, tail);
+      tail = head;
+    }
+
     frag.InList.push_back(tail);
 
-    const uint32 elen = enc.size();
-    if (elen > 1) {
-      // middle vertices
-      for (uint32 i = 1; i < elen-1; ++i) {
+/*
+
+      // walk backwards until a mismatch
+      uint32 b = enc.size() - 2;
+      do {
+        head = 0;
+        const uint32 ideg = g.inDegree(tail);
+        for (uint32 i = 0; i < ideg; ++i) {
+          head = g.inVertex(tail, i);
+          bs.reset();
+          g[head].Trans->getBits(bs);
+          if (bs == enc[b]) {
+            tail = head;
+            --b;
+            break;
+          }
+          head = 0;
+        }
+      } while (head);
+
+      const NFA::VertexDescriptor tbeg = tail;
+
+      // build nodes through position b
+
+      // leading vertex
+      tail = g.addVertex();
+      g[tail].Trans = g.TransFac->getSmallest(enc[0]);
+      frag.InList.push_back(tail);
+
+      // successive vertices
+      const uint32 elen = enc.size();
+      for (uint32 i = 1; i <= b; ++i) {
         head = tail;
         tail = g.addVertex();
         g[tail].Trans = g.TransFac->getSmallest(enc[i]);
         g.addEdge(head, tail);
       }
 
-      // trailing vertex
-      head = tail;
-
-      // find a match for the last byte
-      const auto oi = std::find_if(
-        frag.OutList.begin(), frag.OutList.end(),
-        [&](const std::pair<NFA::VertexDescriptor,uint32>& p) {
-          bs.reset();
-          g[p.first].Trans->getBits(bs);
-          return bs == enc[elen-1];
-        }
-      );
-
-      if (oi == frag.OutList.end()) {
-        // no match
-        tail = g.addVertex();
-        g[tail].Trans = g.TransFac->getSmallest(enc[elen-1]);
-        frag.OutList.emplace_back(tail, 0);
-      }
-      else {
-        tail = oi->first;
-      }
-
-      g.addEdge(head, tail);
+      // connect to tbeg
+      g.addEdge(head, tbeg);
     }
     else {
-      frag.OutList.emplace_back(tail, 0);
-    }
-  }
-
-/*
-  std::unique_ptr<byte[]> buf(new byte[maxByteLength() + 20]);
-  ByteSet bs;
-
-  for (const UnicodeSet::range& r : uset) {
-    const uint32 l = r.first, h = r.second;
-    for (uint32 cp = l; cp < h; ++cp) {
-      const uint32 len = write(cp, buf.get());
-      if (len == 0) {
-        // cp is invalid, skip it
-        continue;
-      }
-
-      NFA::VertexDescriptor iend = 0;
-
-      // find a matching leading segment
-      uint32 ipos = findInitialSegment(iend, buf.get(), len, g, frag);
-
-      NFA::VertexDescriptor head = iend, tail;
-
-      if (ipos == len - 1) {
-        if (iend) {
-          if (g.outDegree(head) > 0) {
-            tail = g.outVertex(head, 0);
-            bs.reset();
-            g[tail].Trans->getBits(bs);
-            bs.set(buf[ipos]);
-
-            ByteSet obs;
-            const auto oi = std::find_if(
-              frag.OutList.begin(),
-              frag.OutList.end(),
-              [&](const std::pair<NFA::VertexDescriptor,uint32>& p) {
-                obs.reset();
-                g[p.first].Trans->getBits(obs);
-                return bs == obs;
-              }
-            );
-
-            if (oi == frag.OutList.end()) {
-              g[tail].Trans = g.TransFac->getByteSet(bs);
-            }
-            else {
-              tail = oi->first;
-              g.addEdge(head, tail);
-
-              tail = g.outVertex(head, 0);
-              if (g.inDegree(tail) > 1) {
-                g.removeEdge(g.outEdge(head, 0));
-              }
-              else {
-                std::pair<NFA::VertexDescriptor,uint32> p(tail, 0);
-                frag.OutList.erase(
-                  std::remove(frag.OutList.begin(), frag.OutList.end(), p)
-                );
-                g.removeVertex(tail);
-              }
-            }
-          }
-          else {
-            tail = g.addVertex();
-            g[tail].Trans = g.TransFac->getLit(buf[ipos]);
-            g.addEdge(head, tail);
-            frag.OutList.emplace_back(tail, 0);
-          }
-        }
-        else {
-          const auto ii = std::find_if(
-            frag.InList.begin(),
-            frag.InList.end(),
-            [&](NFA::VertexDescriptor v) {
-              return g.outDegree(v) == 0;
-            }
-          );
-
-          if (ii == frag.InList.end()) {
-            tail = g.addVertex();
-            g[tail].Trans = g.TransFac->getLit(buf[ipos]);
-            frag.InList.push_back(tail);
-            frag.OutList.emplace_back(tail, 0);
-          }
-          else {
-            tail = *ii;
-            bs.reset();
-            g[tail].Trans->getBits(bs);
-            bs.set(buf[ipos]);
-            g[tail].Trans = g.TransFac->getByteSet(bs);
-          }
-        }
-      }
-      else {
-        // find a matching trailing segment
-        NFA::VertexDescriptor tbeg = 0;
-        const uint32 tpos = findTerminalSegment(tbeg, buf.get(), len, g, frag);
-
-        for ( ; ipos < tpos; ++ipos) {
-          tail = g.addVertex();
-          g[tail].Trans = g.TransFac->getLit(buf[ipos]);
-
-          if (ipos == 0) {
-            frag.InList.push_back(tail);
-          }
-
-          if (ipos == len - 1) {
-            frag.OutList.emplace_back(tail, 0);
-          }
-
-          if (head) {
-            g.addEdge(head, tail);
-          }
-
-          head = tail;
-        }
-
-        if (tpos < len) {
-          g.addEdge(head, tbeg);
-        }
-      }
-    }
-  }
 */
+/*
+      // leading vertex
+      tail = g.addVertex();
+      g[tail].Trans = g.TransFac->getSmallest(enc[0]);
+      frag.InList.push_back(tail);
+
+      // successive vertices
+      const uint32 elen = enc.size();
+      for (uint32 i = 1; i < elen; ++i) {
+        head = tail;
+        tail = g.addVertex();
+        g[tail].Trans = g.TransFac->getSmallest(enc[i]);
+        g.addEdge(head, tail);
+      }
+
+      frag.OutList.emplace_back(tail, 0);
+*/
+//    }
+  }
 }
