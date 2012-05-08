5d1db5ecc5794464e7ff52b2b5b12a173b41469a
Joel Uckelman <uckelman@nomic.net>
2012-05-08 21:19:57 +0200

    Renamed collectEncodings to be another overload of write.

--- include/encoder.h
+++ include/encoder.h
@@ -2,13 +2,20 @@
 
 #include "automata.h"
 #include "basic.h"
+#include "byteset.h"
 #include "fragment.h"
+
+#include <vector>
 
 class Encoder {
 public:
+  virtual ~Encoder() {}
+
   virtual uint32 maxByteLength() const = 0;
+
   virtual uint32 write(int cp, byte buf[]) const = 0;
+
   virtual void write(const UnicodeSet& uset, NFA& g, Fragment& frag) const;
 
-  virtual ~Encoder() {}
+  virtual void write(std::vector<std::vector<ByteSet>>& v, const UnicodeSet& uset) const;
 };
