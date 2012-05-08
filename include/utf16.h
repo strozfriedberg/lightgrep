9933961e7877468dbd8c383a2ceca767643e137d
Joel Uckelman <uckelman@nomic.net>
2012-05-08 21:22:26 +0200

    Reunited UTF-16 encoder so the same code handles LE and BE. Range collapsing is now merely slow, instead of glacial.

--- include/utf16.h
+++ include/utf16.h
@@ -45,12 +45,81 @@ public:
     }
   }
 
+  virtual void write(std::vector<std::vector<ByteSet>>& v,
+                     const UnicodeSet& uset) const
+  {
+    byte cur[4];
+    byte prev[4];
+
+    uint32 clen, plen = 0;
+
+    for (const UnicodeSet::range& r : uset) {
+      const uint32 l = r.first, h = r.second;
+      for (uint32 cp = l; cp < h; ++cp) {
+        clen = write(cp, cur);
+        if (clen == 0) {
+          // cp is invalid, skip it
+          continue;
+        }
+
+/*
+        if (cur[LE ? 0 : 3] == 0 && cp + 255 < h) {
+          // write the whole block in one go
+          v.emplace_back(clen);
+          for (uint32 i = 0; i < clen; ++i) {
+            v.back()[i].set(cur[i]);
+          }
+
+          v.back()[(clen == 2 ? 0 : 2) + !LE].reset().flip();
+          cp += 255;
+          std::swap(prev, cur);
+          plen = clen;
+          continue;
+        }
+*/
+
+        if (clen == 2) {
+          if (plen == 2 && cur[LE ? 1 : 0] == prev[LE ? 1 : 0]) {
+            // join the previous cp if we agree on the relevant byte
+            v.back()[LE ? 0 : 1].set(cur[LE ? 0 : 1]);
+          }
+          else {
+            // otherwise add a new encoding to the list
+            v.emplace_back(clen);
+            for (uint32 i = 0; i < clen; ++i) {
+              v.back()[i].set(cur[i]);
+            }
+
+            std::swap(prev, cur);
+            plen = clen;
+          }
+        }
+        else { // clen == 4
+          if (plen == 4 &&
+              cur[0] == prev[0] &&
+              cur[1] == prev[1] &&
+              cur[LE ? 3 : 2] == prev[LE ? 3 : 2])
+          {
+            // join the previous cp if we are the same except on byte 2
+            v.back()[LE ? 2 : 3].set(cur[LE ? 2 : 3]);
+          }
+          else {
+            // otherwise add a new encoding to the list
+            v.emplace_back(clen);
+            for (uint32 i = 0; i < clen; ++i) {
+              v.back()[i].set(cur[i]);
+            }
+
+            std::swap(prev, cur);
+            plen = clen;
+          }
+        }
+      }
+    }
+  }
+
   using Encoder::write;
 };
 
-class UTF16LE: public UTF16Base<true> {
-protected:
-  virtual void collectEncodings(std::vector<std::vector<ByteSet>>& va, const UnicodeSet& uset) const;
-};
-
+typedef UTF16Base<true>  UTF16LE;
 typedef UTF16Base<false> UTF16BE;
