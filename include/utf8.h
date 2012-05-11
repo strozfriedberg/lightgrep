bb7a88117491a89148766439a48bc347519ce0fd
Joel Uckelman <uckelman@nomic.net>
2012-05-12 01:04:25 +0200

    Remove the UTF-16 surrogates from the valid range of code points for all full-Unicode encoders.

--- include/utf8.h
+++ include/utf8.h
@@ -4,7 +4,7 @@
 
 class UTF8: public Encoder {
 public:
-  UTF8(): valid(0, 0x110000) {}
+  UTF8(): valid{{0, 0xD800}, {0xE000, 0x110000}} {}
 
   virtual uint32 maxByteLength() const { return 4; }
 
