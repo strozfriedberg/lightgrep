482a724c671a44a392c14f679cd5953aabf2d5ef
Joel Uckelman <uckelman@nomic.net>
2012-05-12 03:08:42 +0200

    Added a ranged set() operation to ByteSet. Only works for 'true' presently.

--- include/byteset.h
+++ include/byteset.h
@@ -19,6 +19,36 @@ public:
     return memcmp(this, &other, sizeof(ByteSet));
   }
 
+  ByteSet& set(size_t beg, size_t end, bool value) {
+    for ( ; beg < end && beg % 8 > 0; ++beg) {
+      set(beg, value);
+    }
+
+    if ((end-beg)/8 > 0) {
+      unsigned char* buf = reinterpret_cast<unsigned char*>(this);
+      std::fill(buf + beg/8, buf + end/8, value ? 0xFF : 0);
+      beg = (end/8)*8;
+    }
+
+    for ( ; beg < end; ++beg) {
+      set(beg, value);
+    }
+
+/*
+    unsigned char* buf = reinterpret_cast<unsigned char*>(this);
+    if (beg % 8 > 0) {
+      *(buf + beg/8) |= (1 << (8-beg%8)) - 1;
+    }
+    std::fill(buf + (beg+1)/8, buf + (end/8), value ? 0xFF : 0);
+    if (end % 8 > 0) {
+      *(buf + end/8) |= (1 << (end%8)) - 1;
+    }
+*/
+    return *this;
+  }
+
+  using std::bitset<256>::set;
+
   bool operator<(const ByteSet& other) const {
     return compare(other) < 0;
   }
