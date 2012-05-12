e3ff57d4793a481a5f4011660ebb392249790014
Joel Uckelman <uckelman@nomic.net>
2012-05-13 01:15:24 +0200

    Refactored Encoders. Character class subgraph building is returned to NFABuilder; Encoders no longer have any dependencies on NFA or Fragment. The new version of Encoders::write produces a vector of encoding ranges. Encoder::collectRanges makes an intial pass at gathering encoding ranges from a set of code points, which Encoders::write collapses.

--- include/ascii.h
+++ include/ascii.h
@@ -4,15 +4,16 @@
 
 class ASCII: public Encoder {
 public:
-  ASCII(): valid(0, 0x100) {}
+  ASCII(): Valid(0, 0x100) {}
 
   virtual uint32 maxByteLength() const { return 1; }
 
-  virtual const UnicodeSet& validCodePoints() const { return valid; };
+  virtual const UnicodeSet& validCodePoints() const { return Valid; };
 
   virtual uint32 write(int cp, byte buf[]) const;
-  virtual void write(const UnicodeSet& uset, NFA& g, Fragment& frag) const;
+
+  virtual void write(const UnicodeSet& user, std::vector<std::vector<ByteSet>>& v) const;
 
 private:
-  const UnicodeSet valid;
+  const UnicodeSet Valid;
 };
