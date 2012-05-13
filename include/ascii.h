093cab873b4e50f19686a511e0cfd14ab2b37612
Joel Uckelman <uckelman@nomic.net>
2012-05-13 23:00:15 +0200

    Moved member for storing valid code points from concrete encoders to EncoderBase.

--- include/ascii.h
+++ include/ascii.h
@@ -4,16 +4,11 @@
 
 class ASCII: public EncoderBase {
 public:
-  ASCII(): Valid(0, 0x100) {}
+  ASCII(): EncoderBase(UnicodeSet{{0, 0x100}}) {}
 
   virtual uint32 maxByteLength() const { return 1; }
-
-  virtual const UnicodeSet& validCodePoints() const { return Valid; };
 
   virtual uint32 write(int cp, byte buf[]) const;
 
   virtual void write(const UnicodeSet& user, std::vector<std::vector<ByteSet>>& v) const;
-
-private:
-  const UnicodeSet Valid;
 };
