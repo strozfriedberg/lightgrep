da8faba157b1e9d33c6094274976d28d5b7e0f56
Joel Uckelman <uckelman@nomic.net>
2012-05-13 23:02:28 +0200

    Ctor for UTFBase should not be public.

--- include/utfbase.h
+++ include/utfbase.h
@@ -4,11 +4,11 @@
 
 class UTFBase: public EncoderBase {
 public:
-  UTFBase(): EncoderBase({{0, 0xD800}, {0xE000, 0x110000}}) {}
-
   using EncoderBase::write;
 
 protected:
+  UTFBase(): EncoderBase({{0, 0xD800}, {0xE000, 0x110000}}) {}
+
   virtual void writeRangeBlock(std::vector<ByteSet>& v, uint32& l, uint32 h, uint32 len, uint32 blimit) const = 0;
 
   virtual void writeRange(std::vector<std::vector<ByteSet>>& va, UnicodeSet::const_iterator& i, const UnicodeSet::const_iterator& iend, uint32& l, uint32& h, byte* cur, uint32 len, uint32 blimit) const;
