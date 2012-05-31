79385f64315fc94da6def288b771414a429bee1e
Joel Uckelman <uckelman@nomic.net>
2012-05-31 15:25:53 +0200

    Built stronger preference for ASCII letters into match generator.

--- src/lib/matchgen.cpp
+++ src/lib/matchgen.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <random>
+#include <tuple>
 
 bool checkForRoadLessTaken(const NFA& g, const std::vector<uint32>& seen,
                           const uint32 maxLoops, const NFA::VertexDescriptor v)
@@ -25,29 +26,19 @@ void addRange(std::vector<byte>& bytes, std::initializer_list<std::pair<byte,byt
   }
 }
 
-NFA::VertexDescriptor chooseRandomTarget(
-  const NFA& g, const std::vector<uint32>& seen,
-  NFA::VertexDescriptor v, uint32 maxLoops, std::default_random_engine& rng)
-{
-  std::uniform_int_distribution<uint32> uout(0, g.outDegree(v) - 1);
-  NFA::VertexDescriptor w;
-  do {
-    w = g.outVertex(v, uout(rng));
-  } while (seen[w] > maxLoops);
-  return w;
-}
-
-byte chooseByte(const ByteSet& allowed, std::default_random_engine& rng) {
+template <class RNG>
+byte chooseByte(const ByteSet& allowed, RNG& rng) {
   std::vector<byte> bytes;
+  std::bernoulli_distribution more(0.25);
 
   // can we select alphanumeric?
   addRange(bytes, {{'0', '9'}, {'A', 'Z'}, {'a', 'z'}}, allowed);
 
-  if (bytes.empty()) {
+  if (bytes.empty() || more(rng)) {
     // can we select other printable characters?
     addRange(bytes, {{'!', '/'}, {':', '@'}, {'[', '`'}, {'{', '~'}}, allowed);
 
-    if (bytes.empty()) {
+    if (bytes.empty() || more(rng)) {
       // no visible characters in this range
       addRange(bytes, {{0, ' '}, {0x7F, 0xFF}}, allowed);
     }
@@ -55,6 +46,42 @@ byte chooseByte(const ByteSet& allowed, std::default_random_engine& rng) {
 
   std::uniform_int_distribution<uint32> uout(0, bytes.size() - 1);
   return bytes[uout(rng)];
+}
+
+template <class RNG>
+std::tuple<NFA::VertexDescriptor,byte> chooseRandomTarget(
+  const NFA& g, const std::vector<uint32>& seen,
+  const NFA::VertexDescriptor v, const uint32 maxLoops, RNG& rng)
+{
+  ByteSet bs;
+
+  // collect all of the bytes from undervisited edges leaving v
+  NFA::VertexDescriptor w;
+  const uint32 odeg = g.outDegree(v);
+  for (uint32 i = 0; i < odeg; ++i) {
+    w = g.outVertex(v, i);
+    if (seen[w] <= maxLoops) {
+      g[w].Trans->orBytes(bs);
+    }
+  }
+
+  // pick a byte
+  const byte b = chooseByte(bs, rng);
+
+  // then return the first undervisited neighbor of v reachable on that byte
+  bs.reset();
+  for (uint32 i = 0; i < odeg; ++i) {
+    w = g.outVertex(v, i);
+    if (seen[w] <= maxLoops && g[w].Trans->getBytes(bs).test(b)) {
+      return std::make_tuple(w, b);
+    }
+  }
+
+  // this cannot happen
+  THROW_WITH_OUTPUT(
+    std::logic_error,
+    "byte value " << std::hex << (int) b << " not found!"
+  );
 }
 
 void matchgen(const NFA& g, std::set<std::string>& matches,
@@ -74,31 +101,28 @@ void matchgen(const NFA& g, std::set<std::string>& matches,
 
     seen.assign(g.verticesSize(), 0);
 
-    bool done = false;
-    do {
+    for (;;) {
       // check that there is at least one out vertex not seen too often
       if (!checkForRoadLessTaken(g, seen, maxLoops, v)) {
         break;
       }
-      v = chooseRandomTarget(g, seen, v, maxLoops, rng);
+
+      // select a random neighbor and byte
+      byte b;
+      std::tie(v, b) = chooseRandomTarget(g, seen, v, maxLoops, rng);
       ++seen[v];
-
-      // select a random transition to that vertex
-      allowed.reset(); 
-      g[v].Trans->getBytes(allowed);
-
-      match += chooseByte(allowed, rng);
+      match += b;
 
       if (g[v].IsMatch) {
         // check whether we could extend this match
         if (!checkForRoadLessTaken(g, seen, maxLoops, v)
-          || umatch(rng)) // or can, but don't want to
+            || umatch(rng)) // or can, but don't want to
         {
           // we can't extend the match, report it
           matches.insert(match);
-          done = true;
+          break;
         }
       }
-    } while (!done);
+    }
   }
 }
