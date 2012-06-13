eafe09165a153474b070e6d0dbbf91b8264c895a
Joel Uckelman <uckelman@nomic.net>
2012-06-13 17:49:44 +0200

    Fixed both versions of ASCII::write() to have the correct range, [0x00,0x80).

--- src/lib/ascii.cpp
+++ src/lib/ascii.cpp
@@ -4,7 +4,7 @@ uint32 ASCII::write(int cp, byte buf[]) const {
   if (cp < 0) {
     return 0;
   }
-  else if (cp < 0x100) {
+  else if (cp < 0x80) {
     buf[0] = cp;
     return 1;
   }
@@ -16,11 +16,11 @@ uint32 ASCII::write(int cp, byte buf[]) const {
 void ASCII::write(const UnicodeSet& uset, std::vector<std::vector<ByteSet>>& v) const {
   v.emplace_back(1);
   for (const UnicodeSet::range& r : uset) {
-    if (r.first > 0xFF) {
+    if (r.first > 0x7F) {
       break;
     }
-    else if (r.second > 0xFF) {
-      v[0][0].set(r.first, 0x100, true);
+    else if (r.second > 0x7F) {
+      v[0][0].set(r.first, 0x80, true);
       break;
     }
     else {
