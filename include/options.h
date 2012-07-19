776daaf4e86b90003612337674328a1aa0e5e7e8
Jon Stewart <jon@lightboxtechnologies.com>
2012-07-19 14:00:39 -0400

    Added statsfile command-line option and reworked server to use.

--- include/options.h
+++ include/options.h
@@ -35,7 +35,8 @@ public:
               Output,
               ServerAddr,
               ServerLog,
-              ProgramFile;
+              ProgramFile,
+              StatsFileName;
 
   std::vector<std::string> Inputs,
                            CmdLinePatterns,
