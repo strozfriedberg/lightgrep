bea55d0bc755980d45f6b6cd96ae80d71fb18bdc
Joel Uckelman <uckelman@nomic.net>
2012-07-27 15:38:59 +0200

    Added ICUEncoder(const std::string&) ctor for convenience.

--- include/icuencoder.h
+++ include/icuencoder.h
@@ -3,11 +3,14 @@
 #include "caching_encoder.h"
 #include "encoderbase.h"
 
+#include <string>
+
 #include <unicode/ucnv.h>
 
 class ICUEncoder: public EncoderBase {
 public:
   ICUEncoder(const char* const name);
+  ICUEncoder(const std::string& name);
   virtual ~ICUEncoder();
 
   virtual uint32 maxByteLength() const;
@@ -27,5 +30,9 @@ private:
 
 class CachingICUEncoder: public CachingEncoder<ICUEncoder> {
 public:
-  CachingICUEncoder(const char* const name): CachingEncoder<ICUEncoder>({}, name) {}
+  CachingICUEncoder(const char* const name):
+    CachingEncoder<ICUEncoder>({}, name) {}
+
+  CachingICUEncoder(const std::string& name):
+    CachingEncoder<ICUEncoder>({}, name) {}
 };
