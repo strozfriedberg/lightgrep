f220d239e5d41fcae4c37abbefe04f7b5c6759ef
Joel Uckelman <uckelman@nomic.net>
2012-07-30 14:32:04 +0200

    Use only the C API for ICU. This uglies up the functions which used to use icu::UnicodeSet, but lets us statically link to libicuuc.

--- test/test_icuutil.cpp
+++ test/test_icuutil.cpp
@@ -1,37 +1,41 @@
 #include <scope/test.h>
 
+#include <memory>
+
 #include "icuutil.h"
 
 SCOPE_TEST(testUnicodeSetICUToLGDstEmpty) {
-  icu::UnicodeSet src(0x27, 0x3F);
-  ::UnicodeSet exp{{0x27,0x40}}, act;
+  std::unique_ptr<USet, void(*)(USet*)> src(uset_open(0x27, 0x3F), uset_close);
+  UnicodeSet exp{{0x27,0x40}}, act;
 
-  convUnicodeSet(act, src);
+  convUnicodeSet(act, src.get());
   SCOPE_ASSERT_EQUAL(exp, act);
 }
 
 SCOPE_TEST(testUnicodeSetICUToLGDstNonEmpty) {
   // test that convUnicodeSet clears dst
-  icu::UnicodeSet src(0x27, 0x3F);
-  ::UnicodeSet exp{{0x27,0x40}}, act{0xBEEF};
+  std::unique_ptr<USet, void(*)(USet*)> src(uset_open(0x27, 0x3F), uset_close);
+  UnicodeSet exp{{0x27,0x40}}, act{0xBEEF};
 
-  convUnicodeSet(act, src);
+  convUnicodeSet(act, src.get());
   SCOPE_ASSERT_EQUAL(exp, act);
 }
 
 SCOPE_TEST(testUnicodeSetLGToICUDstEmpty) {
-  ::UnicodeSet src{{0x27,0x40}};
-  icu::UnicodeSet exp(0x27, 0x3F), act;
+  UnicodeSet src{{0x27,0x40}};
+  std::unique_ptr<USet, void(*)(USet*)> exp(uset_open(0x27, 0x3F), uset_close);
+  std::unique_ptr<USet, void(*)(USet*)> act(uset_openEmpty(), uset_close);
 
-  convUnicodeSet(act, src);
-  SCOPE_ASSERT(exp == act);
+  convUnicodeSet(act.get(), src);
+  SCOPE_ASSERT(uset_equals(exp.get(), act.get()));
 }
 
 SCOPE_TEST(testUnicodeSetLGToICUDstNonEmpty) {
   // test that convUnicodeSet clears dst
-  ::UnicodeSet src{{0x27,0x40}};
-  icu::UnicodeSet exp(0x27, 0x3F), act(0xBEEF, 0xBEEF);
+  UnicodeSet src{{0x27,0x40}};
+  std::unique_ptr<USet, void(*)(USet*)> exp(uset_open(0x27, 0x3F), uset_close);
+  std::unique_ptr<USet, void(*)(USet*)> act(uset_open(0xBEEF, 0xBEEF), uset_close);
 
-  convUnicodeSet(act, src);
-  SCOPE_ASSERT(exp == act);
+  convUnicodeSet(act.get(), src);
+  SCOPE_ASSERT(uset_equals(exp.get(), act.get()));
 }
