b0f9af05a4d0c6ea033933ee09fac54e65b78d92
Joel Uckelman <uckelman@nomic.net>
2012-08-11 11:56:26 -0400

    Changed my boostType to empty now that I'm using self-compiled boost.

--- ldsetup.sh
+++ ldsetup.sh
@@ -2,6 +2,6 @@
 
 export LD_LIBRARY_PATH=/home/uckelman/projects/lightbox/lightgrep/lib
 
-alias scb='scons -j8 boostType="-mt"'
-alias scc='scons -c boostType="-mt"'
+alias scb='scons -j8 boostType=""'
+alias scc='scons -c boostType=""'
 
