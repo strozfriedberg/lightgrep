3b828a4ec3b63925a5d228e855bc6bb4961cddf6
Joel Uckelman <uckelman@nomic.net>
2012-10-31 13:06:08 +0100

    Removed files which belong in liblightgrep.

--- include/concrete_encoders.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "ascii.h"
-#include "icuencoder.h"
-#include "utf8.h"
-#include "utf16.h"
-#include "utf32.h"
-
--- test/test_icuencoder.cpp
+++
@@ -1,3 +0,0 @@
-#include <scope/test.h>
-
-#include "icuencoder.h"
