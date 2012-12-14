6fcc1f3af5b3a405b39b45f8b8261a8dd38a4e64
Joel Uckelman <uckelman@nomic.net>
2012-12-14 17:19:38 +0100

    Adjustments for liblightgrep changes 66139364 and 6e9ca1a8e.

--- test/test_options.cpp
+++ test/test_options.cpp
@@ -9,10 +9,10 @@ SCOPE_TEST(parsePatterns) {
   Options opts;
   std::string line("Expression\t1\t1\tUTF-8,UTF-16");
   std::vector<Pattern> pats;
-  SCOPE_ASSERT(opts.parseLine(0, line, pats));
+  SCOPE_ASSERT(opts.parseLine(line, pats));
   SCOPE_ASSERT_EQUAL(2u, pats.size());
-  SCOPE_ASSERT_EQUAL(Pattern("Expression", true, true, 0, "UTF-8"), pats[0]);
-  SCOPE_ASSERT_EQUAL(Pattern("Expression", true, true, 0, "UTF-16"), pats[1]);
+  SCOPE_ASSERT_EQUAL(Pattern("Expression", true, true, "UTF-8"), pats[0]);
+  SCOPE_ASSERT_EQUAL(Pattern("Expression", true, true, "UTF-16"), pats[1]);
 }
 
 SCOPE_TEST(parsePatternNoOpts) {
@@ -23,7 +23,7 @@ SCOPE_TEST(parsePatternNoOpts) {
 
   std::string line("Expression");
   std::vector<Pattern> pats;
-  SCOPE_ASSERT(opts.parseLine(0, line, pats));
+  SCOPE_ASSERT(opts.parseLine(line, pats));
   SCOPE_ASSERT_EQUAL(1u, pats.size());
-  SCOPE_ASSERT_EQUAL(Pattern("Expression", false, false, 0, "UTF-8"), pats[0]);
+  SCOPE_ASSERT_EQUAL(Pattern("Expression", false, false, "UTF-8"), pats[0]);
 }
