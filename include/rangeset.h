0c5871ac018210cd17c825cc75cef9dd05668790
Joel Uckelman <uckelman@nomic.net>
2013-03-06 13:54:25 +0100

    Correctly handle self-or-assignment (|=) for RangeSet.

--- include/rangeset.h
+++ include/rangeset.h
@@ -138,9 +138,11 @@ public:
   }
 
   RangeSet<T,N>& operator|=(const RangeSet<T,N>& r) {
-    const const_internal_iterator end(r.vec.end());
-    for (const_internal_iterator i(r.vec.begin()); i != end; i += 2) {
-      insert(*i, *(i+1));
+    if (this != &r) {
+      const const_internal_iterator end(r.vec.end());
+      for (const_internal_iterator i(r.vec.begin()); i != end; i += 2) {
+        insert(*i, *(i+1));
+      }
     }
     return *this;
   }
