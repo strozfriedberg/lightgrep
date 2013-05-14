d80562af85249bac51374e1007f4ead820635375
Joel Uckelman <uckelman@nomic.net>
2013-05-14 19:20:36 +0200

    Added toString() and hashCode() to SearchHit.

--- src/java/java/src/com/lightboxtechnologies/lightgrep/SearchHit.java
+++ src/java/java/src/com/lightboxtechnologies/lightgrep/SearchHit.java
@@ -23,4 +23,13 @@ public class SearchHit {
     final SearchHit h = (SearchHit) o;
     return Start == h.Start && End == h.End && KeywordIndex == h.KeywordIndex;
   }
+
+  public int hashCode() {
+    return ((17*37 + KeywordIndex)*37 +
+      ((int) (Start ^ (Start >> 32))))*37 + ((int) (End ^ (End >> 32)));
+  }
+
+  public String toString() {
+    return KeywordIndex + "@[" + Start + "," + End + ")";
+  }
 }
