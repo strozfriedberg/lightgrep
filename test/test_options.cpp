e41888534436f9bf11d3b3f40a03d517d68e82e2
Joel Uckelman <uckelman@nomic.net>
2013-06-14 19:57:37 +0200

    Missed uppdating the test to work with Key instead of Pattern. Fixed.

--- test/test_options.cpp
+++ test/test_options.cpp
@@ -3,13 +3,14 @@
 #include <fstream>
 #include <vector>
 
+#include "key.h"
 #include "options.h"
 
 SCOPE_TEST(parsePatterns) {
   Options opts;
   std::string line("Expression\t1\t1\tUTF-8,UTF-16");
-  std::vector<Pattern> pats;
-  SCOPE_ASSERT(opts.parseLine(line, pats));
+  std::vector<Key> pats;
+  SCOPE_ASSERT(opts.parseLine(line, 0, pats));
   SCOPE_ASSERT_EQUAL(2u, pats.size());
   SCOPE_ASSERT_EQUAL(Pattern("Expression", true, true, "UTF-8"), pats[0]);
   SCOPE_ASSERT_EQUAL(Pattern("Expression", true, true, "UTF-16"), pats[1]);
@@ -22,8 +23,8 @@ SCOPE_TEST(parsePatternNoOpts) {
   opts.Encodings = { "UTF-8" };
 
   std::string line("Expression");
-  std::vector<Pattern> pats;
-  SCOPE_ASSERT(opts.parseLine(line, pats));
+  std::vector<Key> pats;
+  SCOPE_ASSERT(opts.parseLine(line, 0, pats));
   SCOPE_ASSERT_EQUAL(1u, pats.size());
   SCOPE_ASSERT_EQUAL(Pattern("Expression", false, false, "UTF-8"), pats[0]);
 }
