b01e46a8dbccefc1ac6a7dc9bff99f11489b06ec
Joel Uckelman <uckelman@nomic.net>
2013-10-22 19:42:36 +0200

    All pattern parsing goes through lg_add_pattern_list now, to take advantage pattern file parsing.

--- src/cmd/options.cpp
+++ src/cmd/options.cpp
@@ -1,29 +1,10 @@
-// must include <fstream> before options.h, because of <iosfwd> usage
 #include <iostream>
+// must include <fstream> before options.h, because of <iosfwd> usage
 #include <fstream>
-#include <stdexcept>
-
-#include <boost/tokenizer.hpp>
 
 #include "options.h"
 #include "ostream_join_iterator.h"
 #include "util.h"
-
-bool Options::readKeyFile(const std::string& keyFilePath, std::vector<Key>& keys) const {
-  std::ifstream keyFile(keyFilePath.c_str(), std::ios::in);
-  if (keyFile) {
-    char line[8192];
-    for (uint32_t lineno = 0; keyFile; ++lineno) {
-      keyFile.getline(line, sizeof(line));
-      parseLine(line, lineno, keys);
-    }
-    return !keys.empty();
-  }
-  else {
-    std::cerr << "Could not open keywords file " <<  keyFilePath << std::endl;
-    return false;
-  }
-}
 
 std::ostream& Options::openOutput() const {
   if (Output == "-") {
@@ -95,82 +76,4 @@ std::vector<std::pair<std::string,std::string>> Options::getKeyFiles() const {
   }
 
   return ret;
-}
-
-std::vector<Key> Options::getKeys() const {
-  std::vector<Key> ret;
-  if (!CmdLinePatterns.empty()) {
-    uint32_t lineno = 0;
-    for (const std::string& p : CmdLinePatterns) {
-      parseLine(p, lineno++, ret);
-    }
-  }
-  else {
-    for (const std::string& kf : KeyFiles) {
-      readKeyFile(kf, ret);
-    }
-  }
-  return ret;
-}
-
-void setBool(const std::string& s, bool& b) {
-  if (0 == s.compare("1")) {
-    b = true;
-  }
-  else if (0 == s.compare("0")) {
-    b = false;
-  }
-  // don't set if unrecognized
-}
-
-bool Options::parseLine(const std::string& line, uint32_t lineno, std::vector<Key>& keys) const {
-  typedef boost::char_separator<char> char_separator;
-  typedef boost::tokenizer<char_separator> tokenizer;
-
-  if (!line.empty()) {
-    const tokenizer tokens(line, char_separator("\t"));
-    tokenizer::const_iterator curTok(tokens.begin());
-    const tokenizer::const_iterator endTok(tokens.end());
-    if (curTok != endTok) {
-      Key p(*curTok, LiteralMode, CaseInsensitive);
-      p.UserIndex = lineno;
-
-      if (++curTok == endTok) {
-        // encoding names are in Encodings
-        if (Encodings.empty()) {
-          return false;
-        }
-
-        for (const std::string& enc : Encodings) {
-          p.Encoding = { enc };
-          keys.push_back(p);
-        }
-      }
-      else {
-        // encoding names are at the end of the line
-        setBool(*curTok, p.FixedString);
-        if (++curTok == endTok) {
-          return false;
-        }
-        setBool(*curTok, p.CaseInsensitive);
-
-        if (++curTok == endTok) {
-          return false;
-        }
-
-        const tokenizer encList(*curTok, char_separator(","));
-        if (encList.begin() == encList.end()) {
-          return false;
-        }
-
-        for (const std::string& enc : encList) {
-          p.Encoding = { enc };
-          keys.push_back(p);
-        }
-      }
-
-      return true;
-    }
-  }
-  return false;
 }
