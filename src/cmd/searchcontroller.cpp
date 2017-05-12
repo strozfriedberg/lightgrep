733e298ea957b48e6a15ce3a778f9ca2409ac0ff
Joel Uckelman <juckelman@strozfriedberg.co.uk>
2017-05-12 13:01:43 +0100

    Cleanup.

--- src/cmd/searchcontroller.cpp
+++ src/cmd/searchcontroller.cpp
@@ -3,7 +3,9 @@
 #include <cstdio>
 #include <iostream>
 
+// FIXME: Replace boost::thread with std::thread
 #include <boost/thread.hpp>
+// FIXME: Replace boost::timer with something from std::chrono
 #include <boost/timer.hpp>
 
 uint64_t readNext(FILE* file, char* buf, unsigned int blockSize) {
@@ -21,37 +23,35 @@ bool SearchController::searchFile(
            offset = 0;
 
   blkSize = readNext(file, Cur.get(), BlockSize);
-  if (!feof(file)) {
-    do {
-      // read the next block on a separate thread
-      boost::packaged_task<uint64_t> task(
-        std::bind(&readNext, file, Next.get(), BlockSize)
-      );
-      boost::unique_future<uint64_t> sizeFut = task.get_future();
-      boost::thread exec(std::move(task));
+  while (!std::feof(file)) {
+    // read the next block on a separate thread
+    boost::packaged_task<uint64_t> task(
+      std::bind(&readNext, file, Next.get(), BlockSize)
+    );
+    boost::unique_future<uint64_t> sizeFut = task.get_future();
+    boost::thread exec(std::move(task));
 
-      // search cur block
-      hinfo->setBuffer(Cur.get(), blkSize, offset);
+    // search cur block
+    hinfo->setBuffer(Cur.get(), blkSize, offset);
 
-      lg_search(
-        searcher.get(),
-        Cur.get(),
-        Cur.get() + blkSize,
-        offset, hinfo, callback
-      );
+    lg_search(
+      searcher.get(),
+      Cur.get(),
+      Cur.get() + blkSize,
+      offset, hinfo, callback
+    );
 
-      offset += blkSize;
-      if (offset % (1024 * 1024 * 1024) == 0) { // FIXME: should change this due to the block size being variable
-        const double lastTime = searchClock.elapsed();
-        uint64_t units = offset >> 20;
-        const double bw = units / lastTime;
-        units >>= 10;
-        std::cerr << units << " GB searched in " << lastTime
-                  << " seconds, " << bw << " MB/s avg" << std::endl;
-      }
-      blkSize = sizeFut.get(); // block on i/o thread completion
-      Cur.swap(Next);
-    } while (!feof(file)); // note file is shared btwn threads, but safely
+    offset += blkSize;
+    if (offset % (1024 * 1024 * 1024) == 0) { // FIXME: should change this due to the block size being variable
+      const double lastTime = searchClock.elapsed();
+      uint64_t units = offset >> 20;
+      const double bw = units / lastTime;
+      units >>= 10;
+      std::cerr << units << " GB searched in " << lastTime
+                << " seconds, " << bw << " MB/s avg" << std::endl;
+    }
+    blkSize = sizeFut.get(); // block on i/o thread completion
+    Cur.swap(Next);
   }
 
   // assert: all data has been read, offset + blkSize == file size,
