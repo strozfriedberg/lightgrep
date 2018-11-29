f3ae7c0c1d8fb4ed4870b48bec97702f736fb879
Zack Weger <ZWeger@StrozFriedberg.com>
2018-11-29 17:46:00 -0500

    Update scope. Remove unneeded references to pair_out.h

--- test/test_icuutil.cpp
+++ test/test_icuutil.cpp
@@ -16,11 +16,11 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include "pair_out.h"
 #include <scope/test.h>
 
 #include "rangeset.h"
 #include "icuutil.h"
-#include "pair_out.h"
 
 #include <unicode/uset.h>
 #include <memory>
