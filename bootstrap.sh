2e41faf8f2d3faccfeb1d056d388f2d0ab1efeb8
Jon Stewart <jstewart@strozfriedberg.com>
2018-12-11 22:36:03 -0500

    Somewhere handy to save how to build for AddressSanitizer. Now I can forget it.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
 autoreconf -fi
+
+# if you want to build for AddressSanitizer:
+# ./configure CXXFLAGS="-g -O0 -fsanitize=address -fno-omit-frame-pointer" LINKFLAGS="-fsanitize=address -fno-omit-frame-pointer"
