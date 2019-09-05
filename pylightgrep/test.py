252cd17a8d2d94946a7ffec39990fd5d15595cbd
Joel Uckelman <juckelman@strozfriedberg.co.uk>
2019-09-05 17:21:57 +0100

    Added tests for utility pointer functions.

--- pylightgrep/test.py
+++ pylightgrep/test.py
@@ -1,8 +1,42 @@
 #!/usr/bin/python3
 
+import array
+import ctypes
 import unittest
 
 import lightgrep
+
+
+class PointerTests(unittest.TestCase):
+    sizes = (0, 1, 7, 8, 1024)
+    buftypes = (
+        bytes,
+        bytearray,
+        lambda n: array.array('b', [0]*n),
+        lambda n: array.array('B', [0]*n)
+    )
+
+    def check_range(self, buf):
+        r_beg, r_end = lightgrep.buf_range(buf, ctypes.c_char)
+        self.assertNotEqual(r_beg, 0)
+        self.assertNotEqual(r_end, 0)
+        self.assertEqual(ctypes.addressof(r_end)-ctypes.addressof(r_beg), len(buf))
+
+        beg = lightgrep.buf_beg(buf, ctypes.c_char)
+        self.assertEqual(ctypes.addressof(beg), ctypes.addressof(r_beg))
+
+        end = lightgrep.buf_end(buf, ctypes.c_char)
+        self.assertEqual(ctypes.addressof(end), ctypes.addressof(r_end))
+
+    def test_buf_range(self):
+        for n in self.sizes:
+            for buftype in self.buftypes:
+                buf = buftype(n)
+                with self.subTest(n=n, buf=buf):
+                    self.check_range(buf)
+                with self.subTest(n=n, buf=memoryview(buf)):
+                    self.check_range(buf)
+
 
 
 
