8e4271df1a73b8a81877f833e23369b1913c2055
Joel Uckelman <juckelman@strozfriedberg.co.uk>
2019-09-10 14:58:15 +0100

    Adjusted lg_example.py to match current bindings.

--- pylightgrep/lg_example.py
+++ pylightgrep/lg_example.py
@@ -9,13 +9,13 @@ searchData = searchString.encode('utf-8')
 testData = testString.encode('utf-8')
 print("searchString: %s" % searchString)
 keys = [
-    ("hello", ["UTF-8", "ISO-8859-1"], KeyOpts(fixedString = True, caseInsensitive = False)),
-    ("world", ["UTF-8"], KeyOpts(fixedString = True, caseInsensitive = True)),
+    ("hello", ["UTF-8", "ISO-8859-1"], KeyOpts(fixedString=True, caseInsensitive=False)),
+    ("world", ["UTF-8"], KeyOpts(fixedString=True, caseInsensitive=True)),
     # bl0w
-    ("bl\\dw", ["UTF-8"], KeyOpts(fixedString = False, caseInsensitive = True)),
-    ("[^a-z]+", ["UTF-8"], KeyOpts(fixedString = False, caseInsensitive = True)),
+    ("bl\\dw", ["UTF-8"], KeyOpts(fixedString=False, caseInsensitive=True)),
+    ("[^a-z]+", ["UTF-8"], KeyOpts(fixedString=False, caseInsensitive=True)),
     # Backslash must be escaped once for Python, and again for Lightgrep
-    ("s\\\\((A|E)U\\-)?\\d{1,3}-\\d{1,4}[^a-zA-Z0-9]", ["UTF-8"], KeyOpts(fixedString = False, caseInsensitive = False))
+    ("s\\\\((A|E)U\\-)?\\d{1,3}-\\d{1,4}[^a-zA-Z0-9]", ["UTF-8"], KeyOpts(fixedString=False, caseInsensitive=False))
 ]
 # Using with to open a Lightgrep object and
 # perform a search, passing keys and callback
@@ -23,46 +23,52 @@ keys = [
 print("============================")
 print("Results using 'with'")
 withHits = HitAccumulator()
-with Lightgrep(keys, withHits.lgCallback) as lg:
-    # call .encode() on a string to get a bytes object back, then pass into bytearray
-    withHitCount = lg.searchBuffer(searchData, withHits)
-    print("%d hits found" % withHitCount)
-    for h in withHits.Hits:
-        print("hit at (%s, %s) on keyindex %s, pattern is '%s' with encoding chain '%s'" %
-            (str(h.get("start")), str(h.get("end")), str(h.get("keywordIndex")), h.get("pattern"), h.get("encChain")))
-        # hBytes = searchData[h.get("start"):h.get("end")]
-        # hText = hBytes.decode("utf-8)")
-        # print("    hit text: '%s'" % hText)
-    withHits.reset()
+with make_program_from_patterns(keys, ProgOpts()) as prog:
+    with Context(prog, CtxOpts()) as ctx:
+        # call .encode() on a string to get a bytes object back, then pass into bytearray
+        withHitCount = ctx.searchBuffer(searchData, withHits)
+        print("%d hits found" % withHitCount)
+        for h in withHits.Hits:
+            print("hit at (%s, %s) on keyindex %s, pattern is '%s' with encoding chain '%s'" %
+                (str(h.get("start")), str(h.get("end")), str(h.get("keywordIndex")), h.get("pattern"), h.get("encChain")))
+            # hBytes = searchData[h.get("start"):h.get("end")]
+            # hText = hBytes.decode("utf-8)")
+            # print("    hit text: '%s'" % hText)
+        withHits.reset()
+
 print("---------------------------")
 print("Results creating program and pattern map separately from context")
 # Creating the program and pattern map separately
 # from the context
-myLgProg = Lightgrep.createProgram(keys)
-myLg = Lightgrep()
-myHits = HitAccumulator()
-myLg.createContext(myLgProg, myHits.lgCallback)
-myHitCount = myLg.searchBuffer(searchData, myHits)
-print("%d hits found" % myHitCount)
-for h in myHits.Hits:
-    print("hit at (%s, %s) on keyindex %s, pattern is '%s' with encoding chain '%s'" %
-        (str(h.get("start")), str(h.get("end")), str(h.get("keywordIndex")), h.get("pattern"), h.get("encChain")))
-myHits.reset()
-print("---------------------------")
-print("Results reusing context with different data")
-myHitCount = myLg.searchBuffer(testData, myHits)
-print("%d hits found" % myHitCount)
-for h in myHits.Hits:
-    print("hit at (%s, %s) on keyindex %s, pattern is '%s' with encoding chain '%s'" %
-        (str(h.get("start")), str(h.get("end")), str(h.get("keywordIndex")), h.get("pattern"), h.get("encChain")))
-myHits.reset()
-print("---------------------------")
-print("Results reusing context again with and startswith()")
-myHitCount = myLg.searchBufferStartswith(searchData, myHits)
-print("%d hits found" % myHitCount)
-for h in myHits.Hits:
-    print("hit at (%s, %s) on keyindex %s, pattern is '%s' with encoding chain '%s'" %
-        (str(h.get("start")), str(h.get("end")), str(h.get("keywordIndex")), h.get("pattern"), h.get("encChain")))
-myHits.reset()
-myLg.closeContext() # If you need to reuse the program and pattern map, only call closeContext()
-myLg.close() # Call close() explicitly, since we didn't use 'with'
+with Program(0) as prog:
+    with Error() as err:
+        with Pattern() as pat:
+            with Fsm(0) as fsm:
+                fsm.add_patterns(prog, pat, keys, err)
+                prog.compile(fsm, ProgOpts())
+
+    myHits = HitAccumulator()
+    with Context(prog, CtxOpts()) as ctx:
+        myHitCount = ctx.searchBuffer(searchData, myHits)
+
+        print("%d hits found" % myHitCount)
+        for h in myHits.Hits:
+            print("hit at (%s, %s) on keyindex %s, pattern is '%s' with encoding chain '%s'" %
+                (str(h.get("start")), str(h.get("end")), str(h.get("keywordIndex")), h.get("pattern"), h.get("encChain")))
+        myHits.reset()
+        print("---------------------------")
+        print("Results reusing context with different data")
+        myHitCount = ctx.searchBuffer(testData, myHits)
+        print("%d hits found" % myHitCount)
+        for h in myHits.Hits:
+            print("hit at (%s, %s) on keyindex %s, pattern is '%s' with encoding chain '%s'" %
+                (str(h.get("start")), str(h.get("end")), str(h.get("keywordIndex")), h.get("pattern"), h.get("encChain")))
+        myHits.reset()
+        print("---------------------------")
+        print("Results reusing context again with and startswith()")
+        myHitCount = ctx.searchBufferStartswith(searchData, myHits)
+        print("%d hits found" % myHitCount)
+        for h in myHits.Hits:
+            print("hit at (%s, %s) on keyindex %s, pattern is '%s' with encoding chain '%s'" %
+                (str(h.get("start")), str(h.get("end")), str(h.get("keywordIndex")), h.get("pattern"), h.get("encChain")))
+        myHits.reset()
