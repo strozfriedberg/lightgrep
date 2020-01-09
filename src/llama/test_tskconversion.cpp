039faa535e2419fb1e4a186ab99dd8f6958d507c
Jon Stewart <jstewart@strozfriedberg.com>
2020-01-08 22:21:59 -0500

    Test an edge condition

--- src/llama/test_tskconversion.cpp
+++ src/llama/test_tskconversion.cpp
@@ -11,7 +11,7 @@ SCOPE_TEST(testTskConvertTimestamps) {
   meta.ctime = 234123870; // 1977-06-02 18:24:30
   meta.ctime_nano = 315227845;
   meta.mtime = 314159265; // 1979-12-16 02:27:45
-  meta.mtime_nano = 589793238;
+  meta.mtime_nano = 999999999;
 
   jsoncons::json ts;
 
@@ -21,5 +21,5 @@ SCOPE_TEST(testTskConvertTimestamps) {
   SCOPE_ASSERT_EQUAL("2020-01-07 02:40:22.1234567", ts["accessed"]);
   SCOPE_ASSERT_EQUAL("1970-01-01 08:42:17.1234564", ts["created"]);
   SCOPE_ASSERT_EQUAL("1977-06-02 18:24:30.315227845", ts["metadata"]);
-  SCOPE_ASSERT_EQUAL("1979-12-16 02:27:45.589793238", ts["modified"]);
+  SCOPE_ASSERT_EQUAL("1979-12-16 02:27:45.999999999", ts["modified"]);
 }
