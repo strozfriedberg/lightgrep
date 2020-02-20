aadbdd7cc6b8b36989e91edba476308272fa9119
Joel Uckelman <juckelman@strozfriedberg.co.uk>
2020-02-20 15:10:52 +0000

    Fixed warning: Removed useless const qualifier.

--- test/stest.cpp
+++ test/stest.cpp
@@ -22,7 +22,7 @@ namespace {
   void collector(void* userData, const LG_SearchHit* const hit) {
     STest* stest = static_cast<STest*>(userData);
 
-    stest->Hits.push_back(*static_cast<const SearchHit* const>(hit));
+    stest->Hits.push_back(*static_cast<const SearchHit*>(hit));
 
     const LG_PatternInfo* info = lg_pattern_info(
       stest->Prog.get(), hit->KeywordIndex
