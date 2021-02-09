31b849f39dacad09a9a463b14e3b1ff959a481d3
Joel Uckelman <juckelman@strozfriedberg.co.uk>
2021-02-09 15:03:41 +0000

    Refactored TskReader::addToBatch.

--- src/llama/tskreader.h
+++ src/llama/tskreader.h
@@ -94,6 +94,8 @@ private:
     Tsg = Tsk.makeTimestampGetter(fs_info->ftype);
     Tracker->setInodeRange(fs_info->first_inum, fs_info->last_inum + 1);
     Tracker->setBlockRange(fs_info->first_block * fs_info->block_size, (fs_info->last_block + 1) * fs_info->block_size);
+    CurFsOffset = fs_info->offset;
+    CurFsBlockSize = fs_info->block_size;
     return TSK_FILTER_CONT;
   }
 
@@ -109,6 +111,113 @@ private:
 
   void markDeletedRun(uint64_t inum, uint64_t begin, uint64_t end) {
     Tracker->markBlocksClaimed(inum, begin, end);
+  }
+
+  void handleRuns(
+    const TSK_FS_ATTR& a,
+    uint64_t fsOffset,
+    uint64_t blockSize,
+    uint64_t inum,
+    void (TskReader::*markDataRun)(uint64_t inum, uint64_t begin, uint64_t end),
+    jsoncons::json& jnrd_runs)
+  {
+    uint64_t skipBytes = a.nrd.skiplen;
+    const uint64_t mainSize = (a.flags & TSK_FS_ATTR_COMP) ? a.nrd.allocsize: a.nrd.initsize;
+
+    uint64_t fileOffset = 0;
+    uint64_t slackOffset = 0;
+
+    for (auto r = a.nrd.run; r; r = r->next) {
+      if (r->flags == TSK_FS_ATTR_RUN_FLAG_FILLER) {
+        // TODO: check on the exact semantics of this flag
+        continue;
+      }
+
+      jnrd_runs.push_back(Tsk.convertRun(*r));
+
+      // Determine absolute offsets for the run
+      uint64_t beg = fsOffset + r->addr * blockSize;
+      const uint64_t runEnd = beg + r->len * blockSize;
+      uint64_t end = runEnd;
+
+      // Ensure that we've advanced past the initial skip.
+
+      // The TSK structs are capable of representing an intial skip
+      // which could exceed the length of one or more runs. Can that
+      // ever happen? No idea.
+      if (skipBytes > 0) {
+        const uint64_t toSkip = std::min(end - beg, skipBytes);
+        beg += toSkip;
+        skipBytes -= toSkip;
+      }
+
+      if (beg < end) {
+        // we've reached data
+        bool trueSlack = false;
+        uint64_t bytesRemaining = mainSize - fileOffset;
+        if (beg + bytesRemaining < end) {
+          // end is the start of true slack
+          end = beg + bytesRemaining;
+          trueSlack = true;
+        }
+
+        if (beg < end) {
+          if (r->flags == TSK_FS_ATTR_RUN_FLAG_NONE) {
+            // just normal data; sparse blocks will be made available
+            // as unallocated (FIXME: still true?)
+            (this->*markDataRun)(inum, beg, end);
+          }
+          // advance offset (even if data run is sparse)
+          fileOffset += (end - beg);
+        }
+
+        if (trueSlack) {
+          // mark slack at end of allocated space
+          if (r->flags == TSK_FS_ATTR_RUN_FLAG_NONE) {
+            // but only if not sparse (sparse slack is a thing!)
+
+            // TODO: We're not marking slack right now because we
+            // don't know what should claim it.
+          }
+          slackOffset += (runEnd - end);
+        }
+      }
+
+      if (r == a.nrd.run_end) {
+        // This is hopefully unnecessary, but what if
+        // r->nrd.run_end.next isn't null?
+        // paranoia is a feature
+        // FIXME: Maybe we should throw here instead?
+        break;
+      }
+    }
+  }
+
+  void handleAttrs(
+    const TSK_FS_META& meta,
+    uint64_t fsOffset,
+    uint64_t blockSize,
+    uint64_t inum,
+    jsoncons::json& jattrs)
+  {
+    const bool deleted = meta.flags & TSK_FS_META_FLAG_UNALLOC;
+    const auto markDataRun = deleted ? &TskReader::markDeletedRun
+                                     : &TskReader::markAllocatedRun;
+
+    for (const TSK_FS_ATTR* a = meta.attr->head; a; a = a->next) {
+      if (!(a->flags & TSK_FS_ATTR_INUSE)) {
+        // This is an artifact of TSK's data structures, not a real run.
+        continue;
+      }
+
+      jsoncons::json jattr = Tsk.convertAttr(*a);
+
+      if (a->flags & TSK_FS_ATTR_NONRES) {
+        handleRuns(*a, fsOffset, blockSize, inum, markDataRun, jattr["nrd_runs"]);
+      }
+
+      jattrs.push_back(std::move(jattr));
+    }
   }
 
   bool addToBatch(TSK_FS_FILE* fs_file) {
@@ -133,7 +242,7 @@ private:
         std::cerr << Path.top() << " done\n";
         Path.pop();
       }
-      Path.push(fs_file->meta->addr);
+      Path.push(inum);
 
       std::cerr << fs_file->name->par_addr << " -> " << Path.top() << '\n';
 
@@ -144,107 +253,17 @@ private:
     // handle the meta
     //
     jsoncons::json jmeta = Tsk.convertMeta(meta, *Tsg);
-    jsoncons::json& jattrs = jmeta["attrs"];
 
     //
     // handle the attrs
     //
+    jsoncons::json& jattrs = jmeta["attrs"];
 
     // ridiculous bullshit to force attrs to be populated
     Tsk.populateAttrs(fs_file);
 
     if (meta.attr) {
-      const uint64_t blockSize = fs_file->fs_info->block_size;
-      const uint64_t fsOffset = fs_file->fs_info->offset;
-
-      const bool deleted = meta.flags & TSK_FS_META_FLAG_UNALLOC;
-      const auto markDataRun = deleted ? &TskReader::markDeletedRun
-                                       : &TskReader::markAllocatedRun;
-
-      for (const TSK_FS_ATTR* a = meta.attr->head; a; a = a->next) {
-        if (!(a->flags & TSK_FS_ATTR_INUSE)) {
-          continue;
-        }
-
-        jsoncons::json jattr = Tsk.convertAttr(*a);
-
-        if (a->flags & TSK_FS_ATTR_NONRES) {
-          jsoncons::json& jnrd_runs = jattr["nrd_runs"];
-
-          uint64_t skipBytes = a->nrd.skiplen;
-          const uint64_t mainSize = (a->flags & TSK_FS_ATTR_COMP) ? a->nrd.allocsize: a->nrd.initsize;
-
-          uint64_t fileOffset = 0;
-          uint64_t slackOffset = 0;
-
-          for (auto r = a->nrd.run; r; r = r->next) {
-            if (r->flags == TSK_FS_ATTR_RUN_FLAG_FILLER) {
-              // TODO: check on the exact semantics of this flag
-              continue;
-            }
-
-            jnrd_runs.push_back(Tsk.convertRun(*r));
-
-            // Determine absolute offsets for the run
-            uint64_t beg = fsOffset + r->addr * blockSize;
-            const uint64_t runEnd = beg + r->len * blockSize;
-            uint64_t end = runEnd;
-
-            // Ensure that we've advanced past the initial skip.
-
-            // The TSK structs are capable of representing an intial skip
-            // which could exceed the length of one or more runs. Can that
-            // ever happen? No idea.
-            if (skipBytes > 0) {
-              const uint64_t toSkip = std::min(end - beg, skipBytes);
-              beg += toSkip;
-              skipBytes -= toSkip;
-            }
-
-            if (beg < end) {
-              // we've reached data
-              bool trueSlack = false;
-              uint64_t bytesRemaining = mainSize - fileOffset;
-              if (beg + bytesRemaining < end) {
-                // end is the start of true slack
-                end = beg + bytesRemaining;
-                trueSlack = true;
-              }
-
-              if (beg < end) {
-                if (r->flags == TSK_FS_ATTR_RUN_FLAG_NONE) {
-                  // just normal data; sparse blocks will be made available
-                  // as unallocated (FIXME: still true?)
-                  (this->*markDataRun)(inum, beg, end);
-                }
-                // advance offset (even if data run is sparse)
-                fileOffset += (end - beg);
-              }
-
-              if (trueSlack) {
-                // mark slack at end of allocated space
-                if (r->flags == TSK_FS_ATTR_RUN_FLAG_NONE) {
-                  // but only if not sparse (sparse slack is a thing!)
-
-                  // TODO: We're not marking slack right now because we
-                  // don't know what should claim it.
-                }
-                slackOffset += (runEnd - end);
-              }
-            }
-
-            if (r == a->nrd.run_end) {
-              // This is hopefully unnecessary, but what if
-              // r->nrd.run_end.next isn't null?
-              // paranoia is a feature
-              // FIXME: Maybe we should throw here instead?
-              break;
-            }
-          }
-        }
-
-        jattrs.push_back(std::move(jattr));
-      }
+      handleAttrs(meta, CurFsOffset, CurFsBlockSize, inum, jattrs);
     }
 
     Input->push({std::move(jmeta), makeBlockSequence(fs_file)});
@@ -280,20 +299,13 @@ private:
   std::shared_ptr<InputHandler> Input;
   std::shared_ptr<OutputHandler> Output;
 
-  std::vector<bool> InodeEncountered;
-
-  uint64_t InumBegin,
-           InumEnd;
-
-  std::vector<bool> Allocated;
-
-  uint64_t BlockBegin,
-           BlockEnd;
-
   Provider Tsk;
   TskImgAssembler Ass;
   std::unique_ptr<TimestampGetter> Tsg;
   std::unique_ptr<InodeAndBlockTracker> Tracker;
 
   std::stack<TSK_INUM_T> Path;
+
+  uint64_t CurFsOffset;
+  uint64_t CurFsBlockSize;
 };
