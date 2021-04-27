46e3afdeadff8dc7c56d19e1ecfa54d97119bcf9
Joel Uckelman <juckelman@strozfriedberg.co.uk>
2021-04-27 14:08:19 +0100

    Typo.

--- include/lightgrep/api.h
+++ include/lightgrep/api.h
@@ -85,7 +85,7 @@ extern "C" {
 
   // Options for compiling patterns
   //
-  // DeterminizeDepth: the depth to which to determinze the NFA;
+  // DeterminizeDepth: the depth to which to determinize the NFA;
   //     0 -> no determinization, i.e., use the NFA;
   //   > 0 -> determinze to given depth, i.e., convert the NFA to partial DFA
   //   UINT32_MAX is effectively full determinization
