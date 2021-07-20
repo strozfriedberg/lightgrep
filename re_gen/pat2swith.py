1219bdfd7591c52dd6900694ec6065b51dcb5479
Joel Uckelman <juckelman@strozfriedberg.co.uk>
2021-07-20 14:38:22 +0100

    Reformatted Python scripts for modern Python.

--- re_gen/pat2swith.py
+++ re_gen/pat2swith.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 
 #
-# Expects either: 
+# Expects either:
 #
 #   * a tab-separated pattern set followed by a text on each stdin line, or
 #   * a tab-separated pattern set on each stdin line, and a text as argv[1].
-# 
+#
 # Writes unit tests to stdout.
 #
 
@@ -14,67 +14,68 @@ import sys
 
 import lgtestlib
 
+
 def main():
-  sg = os.path.dirname(__file__) + '/shitgrep'
+    sg = os.path.dirname(__file__) + '/shitgrep'
 
-  # get text from command line?
-  if len(sys.argv) < 2:
-    cl_text = False
-  elif len(sys.argv) == 2:
-    text = sys.argv[1]
-    cl_text = True
-  else:
-    raise Exception('too many arguments') 
+    # get text from command line?
+    if len(sys.argv) < 2:
+        cl_text = False
+    elif len(sys.argv) == 2:
+        text = sys.argv[1]
+        cl_text = True
+    else:
+        raise Exception('too many arguments')
 
-  # print head stuff
-  print '''#include <scope/test.h>
+    # print head stuff
+    print('''#include <scope/test.h>
 
 #include "stest.h"
-'''
+''')
 
-  setnum = 0
+    setnum = 0
 
-  for line in sys.stdin:
-    # read the test set
-    parts = line.rstrip('\n').split('\t')
-   
-    if cl_text:
-      pats = parts
-    else: 
-      pats = parts[0:-1]
-      text = parts[-1]
+    for line in sys.stdin:
+        # read the test set
+        parts = line.rstrip('\n').split('\t')
 
-    apats = ['^(' + p + ')' for p in pats]
+        if cl_text:
+            pats = parts
+        else:
+            pats = parts[0:-1]
+            text = parts[-1]
 
-    # get matches from shitgrep
-    matches = lgtestlib.run_shitgrep(sg, apats, text)
+        apats = ['^(' + p + ')' for p in pats]
 
-    if len(pats) == 1:
-      stest = 'R"({})"'.format(pats[0])
-    else:
-      stest = '{{ R"({})" }}'.format(')", R"('.join(pats))
+        # get matches from shitgrep
+        matches = lgtestlib.run_shitgrep(sg, apats, text)
 
-    print '''SCOPE_FIXTURE_CTOR(autoPatternStartsWithTest{setnum}, STest, STest({stest})) {{'''.format(setnum=setnum, stest=stest)
+        if len(pats) == 1:
+            stest = 'R"({})"'.format(pats[0])
+        else:
+            stest = '{{ R"({})" }}'.format(')", R"('.join(pats))
 
-    if matches is None:
-      # every pattern in this set has zero-length matches
-      print '  SCOPE_ASSERT(fixture.parsesButNotValid());'
-    else:
-      # this pattern set has no zero-length matches
-      print '''  const char text[] = "{text}";
+        print('''SCOPE_FIXTURE_CTOR(autoPatternStartsWithTest{setnum}, STest, STest({stest})) {{'''.format(setnum=setnum, stest=stest))
+
+        if matches is None:
+            # every pattern in this set has zero-length matches
+            print('  SCOPE_ASSERT(fixture.parsesButNotValid());')
+        else:
+            # this pattern set has no zero-length matches
+            print('''  const char text[] = "{text}";
   fixture.startsWith(text, text + {textlen}, 0);
-  SCOPE_ASSERT_EQUAL({matchcount}u, fixture.Hits.size());'''.format(text=text, textlen=len(text), matchcount=len(matches))
+  SCOPE_ASSERT_EQUAL({matchcount}u, fixture.Hits.size());'''.format(text=text, textlen=len(text), matchcount=len(matches)))
 
-      for i, m in enumerate(matches):
-        print '  SCOPE_ASSERT_EQUAL(SearchHit({}, {}, {}), fixture.Hits[{}]);'.format(m[0], m[1], m[2], i)
+            for i, m in enumerate(matches):
+                print('  SCOPE_ASSERT_EQUAL(SearchHit({}, {}, {}), fixture.Hits[{}]);'.format(m[0], m[1], m[2], i))
 
-    print '}\n'
+        print('}\n')
 
-    setnum += 1
+        setnum += 1
 
-  return 0
+    return 0
 
 
 if __name__ == "__main__":
-  sys.exit(main())
+    sys.exit(main())
 
