311c0e0cc97edb46ca47058c7cf57e00d2eb2542
Joel Uckelman <juckelman@strozfriedberg.co.uk>
2022-06-14 11:26:09 +0100

    t - Adjusted setProgramUserIndexIndexTooLargeTest() to compile a program before calling ProgramHandle.getPatternInfo().

--- src/java/java/test/com/lightboxtechnologies/lightgrep/LightgrepTest.java
+++ src/java/java/test/com/lightboxtechnologies/lightgrep/LightgrepTest.java
@@ -497,8 +497,21 @@ public class LightgrepTest {
 
   @Test(expected=IndexOutOfBoundsException.class)
   public void setProgramUserIndexIndexTooLargeTest() throws Exception {
-    try (final ProgramHandle hProg = new ProgramHandle(0)) {
-      hProg.setUserIndex(42, 1);
+    try (final FSMHandle hFsm = new FSMHandle(0, 0)) {
+      try (final PatternHandle hPattern = new PatternHandle()) {
+        final KeyOptions kopts = new KeyOptions();
+        kopts.FixedString = false;
+        kopts.CaseInsensitive = false;
+        kopts.UnicodeMode = false;
+
+        hPattern.parsePattern("foo", kopts);
+        hFsm.addPattern(hPattern, "UTF-8", 0);
+
+        try (final ProgramHandle hProg = new ProgramHandle(0)) {
+          hProg.compile(hFsm, new ProgramOptions());
+          hProg.setUserIndex(42, 1);
+        }
+      }
     }
   }
 
