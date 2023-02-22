9e3d5fd76ed3422178690453246bc3c7cb68f257
Joel Uckelman <juckelman@strozfriedberg.co.uk>
2023-02-22 15:51:47 +0000

    B - Temporarily skip tests on static Windows builds; currently installed Wine cannot run test.exe.

--- .world/build.sh
+++ .world/build.sh
@@ -2,9 +2,15 @@
 
 . .world/build_config.sh
 
+make_it
+
 if [ "$Target" = 'windows' ]; then
   MAKE_FLAGS+=' LOG_COMPILER=wine'
+  if [[ "$Linkage" == 'static' ]]; then
+    # Installed wine cannot run the test executable; skip it for now
+    # See https://bugzilla.redhat.com/show_bug.cgi?id=2108032
+    exit 0
+  fi
 fi
 
-make_it
 make_check_it
