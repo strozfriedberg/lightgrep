e971a06bc95852092a9c4a8f6cf577e48a985aa6
Joel Uckelman <juckelman@strozfriedberg.co.uk>
2023-02-22 16:57:19 +0000

    B - enc needs the log compiler set to run, derp.

--- .world/build.sh
+++ .world/build.sh
@@ -2,15 +2,16 @@
 
 . .world/build_config.sh
 
-make_it
-
 if [ "$Target" = 'windows' ]; then
   MAKE_FLAGS+=' LOG_COMPILER=wine'
-  if [[ "$Linkage" == 'static' ]]; then
-    # Installed wine cannot run the test executable; skip it for now
-    # See https://bugzilla.redhat.com/show_bug.cgi?id=2108032
-    exit 0
-  fi
+fi
+
+make_it
+
+if [[ "$Target" == 'windows' && "$Linkage" == 'static' ]]; then
+  # Installed wine cannot run the test executable; skip it for now
+  # See https://bugzilla.redhat.com/show_bug.cgi?id=2108032
+  exit 0
 fi
 
 make_check_it
