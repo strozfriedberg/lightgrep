03615526254214ea296029d683a178e13581cdf0
JarWeinstein <jared.weinstein@aon.com>
2023-06-15 19:16:05 -0400

    t - added additional testing for looping DFS

--- test/test_dfs.cpp
+++ test/test_dfs.cpp
@@ -138,3 +138,19 @@ TEST_CASE("testDFS") {
 
   REQUIRE(exp == act);
 }
+
+TEST_CASE("testDFSLoop") {
+  G g(5);
+  g.addEdge(0, 1);
+  g.addEdge(1, 0);
+  g.addEdge(0, 2);
+  g.addEdge(2, 3);
+  g.addEdge(3, 0);
+  g.addEdge(3, 4);
+  g.addEdge(1, 2);
+
+  const Lists act = depthFirstSearch(1, 4, g);
+  const Lists exp{{1, 0, 2, 3, 4}, {1, 2, 3, 4}};
+
+  REQUIRE(exp == act);
+}
