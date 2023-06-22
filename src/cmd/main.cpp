0212c6223bc90106d35970341eaeb3bd70a6f418
Julia Paluch <julia.paluch@strozfriedberg.com>
2023-06-22 16:55:55 -0400

    B - Only print prog size if verbose flag enabled

--- src/cmd/main.cpp
+++ src/cmd/main.cpp
@@ -516,8 +516,9 @@ void writeProgram(const Options& opts) {
 
   // break on through the C API to print the program
   ProgramPtr p(prog->Prog);
-  std::cerr << p->size() << " instructions\n"
-            << p->bufSize() << " program size in bytes" << std::endl;
+  if (opts.Verbose) {
+    std::cerr << p->bufSize() << " program size in bytes" << std::endl;
+  }
 
   std::ostream& out(opts.openOutput());
   if (opts.Binary) {
