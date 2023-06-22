de0188edd78214d528a3de3196f0465e727e56df
JarWeinstein <jared.weinstein@aon.com>
2023-06-22 19:54:41 -0400

    t - additional testing for dominantPath

--- test/test_dfs.cpp
+++ test/test_dfs.cpp
@@ -298,3 +298,49 @@ TEST_CASE("testDFSDominator") {
 
   REQUIRE(exp == act);
 }
+
+TEST_CASE("testDFSDominator2") {
+  G g(12);
+
+  add_edges(&g, {
+    {0, 1},
+    {1, 2},
+    {2, 3},
+    {2, 11},
+    {3, 4},
+    {4, 5},
+    {5, 6},
+    {6, 7},
+    {7, 8},
+    {7, 10},
+    {8, 9},
+    {10, 9},
+    {11, 5}
+  });
+
+  const List act = dominantPath(0, 9, g);
+  const List exp {0, 1, 2};
+
+  REQUIRE(exp == act);
+}
+
+TEST_CASE("testDFSDominator3") {
+  G g(7);
+
+  add_edges(&g, {
+    {0, 1},
+    {1, 2},
+    {1, 5},
+    {2, 3},
+    {3, 4},
+    {5, 6},
+    {6, 2}
+  });
+
+  const List act = dominantPath(0, 4, g);
+  const List exp {2, 3, 4};
+
+  REQUIRE(exp == act);
+}
+
+
