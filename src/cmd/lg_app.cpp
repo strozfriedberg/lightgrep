f005a63ff461bf62dbe38ea3e5d8ddcfafec752a
Julia Paluch <julia.paluch@strozfriedberg.com>
2023-10-16 12:49:56 -0400

    R - create class to hold fsm, prog, and errs, to be named later

--- src/cmd/lg_app.cpp
+++ src/cmd/lg_app.cpp
@@ -23,11 +23,25 @@ namespace {
   }
 }
 
+class NoName {
+public:
+  std::unique_ptr<FSMHandle, void(*)(FSMHandle*)> fsm;
+  std::unique_ptr<ProgramHandle, void(*)(ProgramHandle*)> prog;
+  std::unique_ptr<LG_Error, void(*)(LG_Error*)> err;
+
+  NoName(
+    std::unique_ptr<FSMHandle, void(*)(FSMHandle*)>,
+    std::unique_ptr<ProgramHandle, void(*)(ProgramHandle*)> prog,
+    std::unique_ptr<LG_Error, void(*)(LG_Error*)> err
+    ) : fsm(std::move(fsm)), prog(std::move(prog)), err(std::move(err)) {};
+};
+  
+
 std::tuple<
   std::unique_ptr<FSMHandle, void(*)(FSMHandle*)>,
   std::unique_ptr<ProgramHandle, void(*)(ProgramHandle*)>,
   std::unique_ptr<LG_Error, void(*)(LG_Error*)>
->
+> 
 parsePatterns(const Options& opts)
 {
   // read the patterns and parse them
