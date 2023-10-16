b4b8646fcf47ac415a4c6b2fa652b6e427fa5d8b
Julia Paluch <julia.paluch@strozfriedberg.com>
2023-10-16 14:57:28 -0400

    b - whoops. pass in an fsm to the contructor

--- include/lg_app.h
+++ include/lg_app.h
@@ -13,7 +13,7 @@ public:
   std::unique_ptr<LG_Error, void(*)(LG_Error*)> err;
 
   NoName(
-    std::unique_ptr<FSMHandle, void(*)(FSMHandle*)>,
+    std::unique_ptr<FSMHandle, void(*)(FSMHandle*)> fsm,
     std::unique_ptr<ProgramHandle, void(*)(ProgramHandle*)> prog,
     std::unique_ptr<LG_Error, void(*)(LG_Error*)> err
     ) : fsm(std::move(fsm)), prog(std::move(prog)), err(std::move(err)) {};
