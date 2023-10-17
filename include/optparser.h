decd3cdb44941d220273d03000c6c01ae5ea90a4
Julia Paluch <julia.paluch@strozfriedberg.com>
2023-10-17 14:01:33 -0400

    a - use po namespace consistently

--- include/optparser.h
+++ include/optparser.h
@@ -3,5 +3,5 @@
 #include "options.h"
 
 void parse_opts(int argc, const char* const * argv,
-                boost::program_options::options_description& desc,
+                po::options_description& desc,
                 Options& opts);
