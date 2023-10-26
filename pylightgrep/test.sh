1e7f042ac9ed798b1f2911489151ca0e365703d3
Joel Uckelman <juckelman@strozfriedberg.co.uk>
2023-10-26 15:27:35 +0100

    B - Run python3, not python.

--- pylightgrep/test.sh
+++ pylightgrep/test.sh
@@ -3,6 +3,6 @@
 if [ -e src/lib/.libs/liblightgrep.so ]; then
   LIBS=$(realpath src/lib/.libs)
   pushd pylightgrep
-  LD_LIBRARY_PATH=$LIBS:$LD_LIBRARY_PATH python -m unittest -v
+  LD_LIBRARY_PATH=$LIBS:$LD_LIBRARY_PATH python3 -m unittest -v
   popd
 fi
