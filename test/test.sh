f0e8e68a4cd5b19a0b5628b98c51b015132aadb9
Joel Uckelman <joel.uckelman@aon.com>
2024-08-19 12:08:51 +0100

    d - Added comment.

--- test/test.sh
+++ test/test.sh
@@ -6,6 +6,14 @@ if hash parallel ; then
   # RANDOM is only 15 bits; this gets us 32
   seed=$(( ($RANDOM << 17) | ($RANDOM << 2) | ($RANDOM & 0x03) ))
 
+  # Catch2 supports test sharding. We run tests as $jobs shards, and
+  # randomize test order to catch non-isolation of tests and make it
+  # likely that the slowest tests are spread across the shards. So that
+  # each shard agrees with the others about how the tetst are divided,
+  # each shard needs to know its own index and the RNG seed. Despite the
+  # test order being random each time, the test order is fully
+  # reproducible in case of a fault because Catch2 prints the seed used.
+
   parallel -t test/test --shard-count $jobs --shard-index {} --order rand --rng-seed $seed ::: $(seq 0 $(($jobs - 1)))
 
 else
