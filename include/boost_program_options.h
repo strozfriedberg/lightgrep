8bce2abd6a338c601cff33cd4bbff2fdef7cea99
Jon Stewart <jstewart@strozfriedberg.com>
2025-04-04 18:26:29 -0400

    b - fiddle a bit more with the gcc/clang warning schism

--- include/boost_program_options.h
+++ include/boost_program_options.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdeprecated-declarations"
 #if defined(__clang__)
+  #pragma clang diagnostic ignored "-Wdeprecated-declarations"
   #pragma clang diagnostic ignored "-Wdeprecated-builtins"
 #endif
 #include <boost/program_options.hpp>
