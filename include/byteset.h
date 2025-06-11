1d2f0c43348d9ef3de98ff9ddd7e00d7917b872f
Julia Paluch <julia.paluch@strozfriedberg.com>
2025-06-11 16:28:18 -0400

    b - count trailing zeroes instead of using popcount and subtracting one

--- include/byteset.h
+++ include/byteset.h
@@ -83,14 +83,14 @@ public:
 
   void fast_for_each(std::function<void(uint64_t)> callback) const {
     // This is evil.
-    uint64_t* words = reinterpret_cast<uint64_t*>(this);
+    const uint64_t* words = reinterpret_cast<const uint64_t*>(this);
     for (int i = 0; i < 4; i++) {
       uint64_t word = words[i];
       while (word != 0) {
         // isolate least significant set bit
         uint64_t lsb = word & -word;
         // get the index of the set bit
-        uint64_t index = (i << 6) + __builtin_popcountll(word - 1);
+        uint64_t index = (i << 6) + __builtin_ctzll(word);
         callback(index);
         // xor og word with lsb to get new word with the previous lsb unset
         word ^= lsb;
